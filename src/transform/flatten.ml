aba3c7c32ceaa1d64927c671363fe1d198f8dce8
Francois Bobot <bobot@lri.fr>
2010-03-05 09:17:22 +0000

    Pourquoi Theory.Context n'est pas un module accessible alors que apres open Theory Context l'est?

--- src/transform/flatten.ml
+++ src/transform/flatten.ml
@@ -2,7 +2,7 @@ open Theory
 
 let elt a =
   let rec aux first_level acc d = match first_level, d.d_node with
-    | _,Duse t -> Context.ctxt_fold (aux false) (d::acc) t.th_ctxt
+    | _,Duse t -> Context.ctxt_fold (aux false) acc t.th_ctxt
     | false,Dprop (Pgoal,_,_) -> acc
     | false,Dprop (Plemma,i,f) -> (create_prop Paxiom (Ident.id_dup i) f)::acc
     | _ -> d::acc 
