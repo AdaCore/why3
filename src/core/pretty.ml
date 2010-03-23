e0d8224f082b9b746b7fcb6bc8785cdb2b33111e
Jean-Christophe Filliâtre <filliatr@lri.fr>
2010-03-23 15:02:59 +0000

    print_inst plus robust

--- src/core/pretty.ml
+++ src/core/pretty.ml
@@ -328,7 +328,8 @@ let print_inst fmt (id1,id2) =
   else if Hid.mem phash id2 then
     let n = id_unique pprinter id1 in
     fprintf fmt "prop %s = %a" n print_pr (Hid.find phash id2)
-  else assert false
+  else 
+    fprintf fmt "ident %s = %s" id1.id_long id2.id_long
 
 let print_decl fmt d = match d.d_node with
   | Dtype tl  -> print_list newline print_type_decl fmt tl
