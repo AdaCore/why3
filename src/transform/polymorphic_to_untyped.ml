553177f11d32c53a6db0f3581aacc05c751ee1ef
Simon Cruanes <simon.cruanes.2007@m4x.org>
2010-05-12 11:42:11 +0000

    prototype of a transformation from polymorphic logic to (almost) untyped logic. It does nothing, but should not disturb the compilation/use of the rest of the system.

---
+++ src/transform/polymorphic_to_untyped.ml
@@ -0,0 +1,32 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  Copyright (C) 2010-                                                   *)
+(*    Francois Bobot                                                      *)
+(*    Jean-Christophe Filliatre                                           *)
+(*    Johannes Kanig                                                      *)
+(*    Andrei Paskevich                                                    *)
+(*                                                                        *)
+(*  This software is free software; you can redistribute it and/or        *)
+(*  modify it under the terms of the GNU Library General Public           *)
+(*  License version 2.1, with the special exception on linking            *)
+(*  described in file LICENSE.                                            *)
+(*                                                                        *)
+(*  This software is distributed in the hope that it will be useful,      *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of        *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  *)
+(*                                                                        *)
+(**************************************************************************)
+
+open Util
+open Ident
+open Ty
+open Term
+open Decl
+open Task
+
+
+let eliminate_polymorphism = Register.store (fun () -> Trans.identity)
+
+let polymorphic_to_untyped = Register.store (fun () -> Trans.identity)
+
+let () = Register.register_transform "polymorphic_to_untyped" polymorphic_to_untyped
