2b9133aa416762b20ee2623da676cc1c40201aff
François Bobot <francois@bobot.eu>
2010-12-01 22:05:11 +0100

    Pp : no newline inside the result of Pp.string_of

--- src/util/pp.ml
+++ src/util/pp.ml
@@ -151,5 +151,9 @@ let rec print_list_opt sep print fmt = function
 let string_of p x =
   let b = Buffer.create 100 in
   let fmt = formatter_of_buffer b in
+  let out,flush,_newline,spaces =
+    pp_get_all_formatter_output_functions fmt () in
+  pp_set_all_formatter_output_functions fmt
+    ~out ~flush ~newline:(fun () -> spaces 1) ~spaces;
   fprintf fmt "%a@?" p x;
   Buffer.contents b
