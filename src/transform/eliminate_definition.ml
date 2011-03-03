a98734ca50d4cff1d33e3d17071c7d57467f5e01
Andrei Paskevich <andrei@tertium.org>
2011-03-03 11:13:57 +0100

    do not produce triggers for definitions (they are either trivial or wrong)

--- src/transform/eliminate_definition.ml
+++ src/transform/eliminate_definition.ml
@@ -86,13 +86,13 @@ let add_ld func pred axl d = match d with
         | Term t when func ->
             let nm = ls.ls_name.id_string ^ "_def" in
             let hd = t_app ls (List.map t_var vl) t.t_ty in
-            let ax = f_forall_close vl [[Term hd]] (t_insert hd t) in
+            let ax = f_forall_close vl [] (t_insert hd t) in
             let pr = create_prsymbol (id_derive nm ls.ls_name) in
             create_prop_decl Paxiom pr ax :: axl, (ls, None)
         | Fmla f when pred ->
             let nm = ls.ls_name.id_string ^ "_def" in
             let hd = f_app ls (List.map t_var vl) in
-            let ax = f_forall_close vl [[Fmla hd]] (f_insert hd f) in
+            let ax = f_forall_close vl [] (f_insert hd f) in
             let pr = create_prsymbol (id_derive nm ls.ls_name) in
             create_prop_decl Paxiom pr ax :: axl, (ls, None)
         | _ -> axl, d
