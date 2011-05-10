AdaCore/why3

commit 2923f2166dff504bfa5c1a5afe60d044c17cd905

Change abstraction into a real transformation.

src/transform/abstraction.ml
@@ -23,15 +23,16 @@ open Term
 open Decl
 open Task
 
-let term_table = Hterm_alpha.create 257
-let fmla_table = Hfmla_alpha.create 257
-let extra_decls = ref []
+let abstraction (keep : lsymbol -> bool) =
+  let term_table = Hterm_alpha.create 257 in
+  let fmla_table = Hfmla_alpha.create 257 in
+  let extra_decls = ref [] in
 
-let rec abstract_term keep t : term =
-  match t.t_node with
+  let rec abstract_term t : term =
+    match t.t_node with
     | Tconst _ | Tapp(_,[]) -> t
     | Tapp(ls,_) when keep ls ->
-        t_map (abstract_term keep) (abstract_fmla keep) t
+        t_map abstract_term abstract_fmla t
     | _ ->
         begin try Hterm_alpha.find term_table t with Not_found ->
           let ls = create_fsymbol (id_fresh "abstr") [] t.t_ty in
@@ -41,13 +42,13 @@ let rec abstract_term keep t : term =
           tabs
         end
 
-and abstract_fmla keep f =
-  match f.f_node with
+  and abstract_fmla f =
+    match f.f_node with
     | Ftrue | Ffalse -> f
     | Fnot _ | Fbinop _ ->
-        f_map (abstract_term keep) (abstract_fmla keep) f
+        f_map abstract_term abstract_fmla f
     | Fapp(ls,_) when keep ls ->
-        f_map (abstract_term keep) (abstract_fmla keep) f
+        f_map abstract_term abstract_fmla f
     | _ ->
         begin try Hfmla_alpha.find fmla_table f with Not_found ->
           let ls = create_psymbol (id_fresh "abstr") [] in
@@ -55,20 +56,12 @@ and abstract_fmla keep f =
           extra_decls := ls :: !extra_decls;
           Hfmla_alpha.add fmla_table f fabs;
           fabs
-        end
+        end in
 
-
-let abstract_decl keep (d : decl) : decl list =
-  let d = decl_map (abstract_term keep) (abstract_fmla keep) d in
-  let l =
+  let abstract_decl (d : decl) : decl list =
+    let d = decl_map abstract_term abstract_fmla d in
     List.fold_left
       (fun acc ls -> create_logic_decl [ls,None] :: acc)
-      [d]
-      !extra_decls
-  in
-  extra_decls := []; l
+      [d] !extra_decls in
 
-let abstraction (keep : lsymbol -> bool) (t: task) : task =
-  Hfmla_alpha.clear fmla_table;
-  Hterm_alpha.clear term_table;
-  Trans.apply (Trans.decl (abstract_decl keep) None) t
+  Trans.decl abstract_decl None

src/printer/gappa.ml
@@ -332,10 +332,9 @@ let print_task env pr thpr ?old:_ fmt task =
         | Tapp(ls,_) -> not (Sid.mem ls.ls_name info.info_symbols)
         | _ -> true) true f)
     ])) task in
-  let task =
-    Abstraction.abstraction
+  let task = Trans.apply (Abstraction.abstraction
       (fun ls -> Sid.mem ls.ls_name info.info_symbols)
-      task in
+    ) task in
   let task = Trans.apply (Trans.lookup_transform "introduce_premises" env) task in
 (*
   eprintf "Abstraction: @\n%a@." Pretty.print_task task;

src/transform/abstraction.mli
@@ -20,7 +20,7 @@
 
 
 
-val abstraction : (Term.lsymbol -> bool) -> Task.task -> Task.task
+val abstraction : (Term.lsymbol -> bool) -> Task.task Trans.trans
 (** [abstract keep t] applies variable abstraction of the task [t],
     that is replaces subterms or subformulas headed by a logic symbol
     f that do not satisfies [keep f] into a fresh variable.

commit d85384f3f061e79cd9dd667ed3c17d772179d65e

Fix breakage of abstraction introduced by commit 2923f21.
That commit caused the declaration list to no longer be reset after each declaration.
Fixing this issue speeds up Gappa printing by 800%.
Abstracted subterms and formulas are now systematically added to extra_decls, since their presence in the global tables does not guarantee they have already been defined.
This guarantee was lost when the tables became global to the transformation, but it was hidden by list extra_decls never being emptied.

Note: removing alpha-conversion does not bring any sensible speedup.

src/transform/abstraction.ml
@@ -34,13 +34,13 @@ let abstraction (keep : lsymbol -> bool) =
     | Tapp(ls,_) when keep ls ->
         t_map abstract_term abstract_fmla t
     | _ ->
-        begin try Hterm_alpha.find term_table t with Not_found ->
+        let (ls, tabs) = try Hterm_alpha.find term_table t with Not_found ->
           let ls = create_fsymbol (id_fresh "abstr") [] t.t_ty in
           let tabs = t_app ls [] t.t_ty in
-          extra_decls := ls :: !extra_decls;
-          Hterm_alpha.add term_table t tabs;
-          tabs
-        end
+          Hterm_alpha.add term_table t (ls, tabs);
+          ls, tabs in
+        extra_decls := ls :: !extra_decls;
+        tabs
 
   and abstract_fmla f =
     match f.f_node with
@@ -50,18 +50,19 @@ let abstraction (keep : lsymbol -> bool) =
     | Fapp(ls,_) when keep ls ->
         f_map abstract_term abstract_fmla f
     | _ ->
-        begin try Hfmla_alpha.find fmla_table f with Not_found ->
+        let (ls, fabs) = try Hfmla_alpha.find fmla_table f with Not_found ->
           let ls = create_psymbol (id_fresh "abstr") [] in
           let fabs = f_app ls [] in
-          extra_decls := ls :: !extra_decls;
-          Hfmla_alpha.add fmla_table f fabs;
-          fabs
-        end in
+          Hfmla_alpha.add fmla_table f (ls, fabs);
+          ls, fabs in
+        extra_decls := ls :: !extra_decls;
+        fabs in
 
   let abstract_decl (d : decl) : decl list =
     let d = decl_map abstract_term abstract_fmla d in
-    List.fold_left
+    let l = List.fold_left
       (fun acc ls -> create_logic_decl [ls,None] :: acc)
       [d] !extra_decls in
+    extra_decls := []; l in
 
   Trans.decl abstract_decl None