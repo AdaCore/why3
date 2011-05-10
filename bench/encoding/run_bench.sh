a960dc7cf3bde76803e3d0ef01d13b1c51c3fe67
François Bobot <francois@bobot.eu>
2011-05-10 14:01:55 +0200

    relativize path for the benchs of frocos

--- bench/encoding/run_bench.sh
+++ bench/encoding/run_bench.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
+WHY3BENCH="../../bin/why3bench -C why.conf"
+
 echo "With Z3"
 for bench in $(cat benchs); do
     echo $bench
-    why3bench -B z3_$bench.rc "$@"
+    $WHY3BENCH -B z3_$bench.rc $ARGS
     echo
 done
 
 echo "With CVC3"
 for bench in $(cat benchs); do
     echo $bench
-    why3bench -B cvc3_$bench.rc "$@"
+    $WHY3BENCH -B cvc3_$bench.rc $ARGS
     echo
 done
 
 echo "With Yices"
-for $bench in $(cat benchs_yices); do
-    echo bench
-    why3bench -B yices_$bench.rc "$@"
+for bench in $(cat benchs_yices); do
+    echo $bench
+    $WHY3BENCH -B yices_$bench.rc
     echo
 done
