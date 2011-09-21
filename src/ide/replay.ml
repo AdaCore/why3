d461ec79fe7f91e4d23f7914cdbef2b8044d8b95
Asma Tafat-Bouzid <atafat@lri.fr>
2011-09-21 10:53:30 +0200

    Latex statistics

--- src/ide/replay.ml
+++ src/ide/replay.ml
@@ -330,14 +330,18 @@ let prover_name a =
     | M.Undetected_prover s -> s
 
 let rec goal_latex_stat n fmt prov depth depth_max first g =
-  if(n ==1) then begin
+  if(n ==1) then 
+    begin
     if not first then
-      if(depth < depth_max) then
-	for i = 1 to depth do fprintf fmt "&" done
-      else
-	for i = 1 to depth - 1 do fprintf fmt "&" done
+      begin
+	  if(depth < depth_max) then
+	    for i = 1 to depth do fprintf fmt "&" done
+	  else
+	    for i = 1 to depth - 1 do fprintf fmt "&" done
+      end
     else
-     if depth > 0 then fprintf fmt "&"
+       if(depth < depth_max) then
+	if depth > 0 then fprintf fmt "&"
   end;
   if (depth <= 1) then 
     fprintf fmt "\\verb|%s| " (M.goal_expl g);
