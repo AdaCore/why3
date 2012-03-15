f1ac0ce0542745d9ed773e7c87da691d88bcfa88
Andrei Paskevich <andrei@lri.fr>
2012-03-15 16:29:19 +0100

    fix BadArity reporting

--- src/parser/typing.ml
+++ src/parser/typing.ml
@@ -380,7 +380,7 @@ and dpat_node loc uc env = function
 
 and dpat_args ls loc uc env el pl =
   let n = List.length el and m = List.length pl in
-  if n <> m then error ~loc (BadArity (ls,m,n));
+  if n <> m then error ~loc (BadArity (ls,n,m));
   let rec check_arg env = function
     | [], [] ->
         env, []
@@ -438,7 +438,7 @@ and dterm_node ~localize loc uc env = function
       (* 0-arity symbol (constant) *)
       let s, tyl, ty = specialize_fsymbol x uc in
       let n = List.length tyl in
-      if n > 0 then error ~loc (BadArity (s, 0, n));
+      if n > 0 then error ~loc (BadArity (s,n,0));
       Tapp (s, []), ty
   | PPapp (x, tl) when check_highord uc env x tl ->
       let tl = apply_highord loc x tl in
@@ -712,7 +712,7 @@ and dpat_list uc env ty p =
 
 and dtype_args ~localize ls loc uc env el tl =
   let n = List.length el and m = List.length tl in
-  if n <> m then error ~loc (BadArity (ls, m, n));
+  if n <> m then error ~loc (BadArity (ls,n,m));
   let rec check_arg = function
     | [], [] ->
         []
