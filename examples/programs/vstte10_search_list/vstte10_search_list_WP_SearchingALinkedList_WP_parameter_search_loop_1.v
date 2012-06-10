ab739e492745e54d8872e6982b8eaa982821eeb9
Andrei Paskevich <andrei@lri.fr>
2012-06-10 16:02:15 +0200

    update sessions

--- examples/programs/vstte10_search_list/vstte10_search_list_WP_SearchingALinkedList_WP_parameter_search_loop_1.v
+++ examples/programs/vstte10_search_list/vstte10_search_list_WP_SearchingALinkedList_WP_parameter_search_loop_1.v
@@ -2,18 +2,27 @@
 (* Beware! Only edit allowed sections below    *)
 Require Import ZArith.
 Require Import Rbase.
+Require int.Int.
+
+(* Why3 assumption *)
 Definition unit  := unit.
 
-Parameter mark : Type.
+Parameter qtmark : Type.
 
-Parameter at1: forall (a:Type), a -> mark -> a.
-
+Parameter at1: forall (a:Type), a -> qtmark -> a.
 Implicit Arguments at1.
 
 Parameter old: forall (a:Type), a -> a.
-
 Implicit Arguments old.
 
+(* Why3 assumption *)
+Definition implb(x:bool) (y:bool): bool := match (x,
+  y) with
+  | (true, false) => false
+  | (_, _) => true
+  end.
+
+(* Why3 assumption *)
 Inductive list (a:Type) :=
   | Nil : list a
   | Cons : a -> (list a) -> list a.
@@ -22,11 +31,12 @@ Implicit Arguments Nil.
 Unset Contextual Implicit.
 Implicit Arguments Cons.
 
+(* Why3 assumption *)
 Set Implicit Arguments.
 Fixpoint length (a:Type)(l:(list a)) {struct l}: Z :=
   match l with
-  | Nil  => 0%Z
-  | Cons _ r => (1%Z + (length r))%Z
+  | Nil => 0%Z
+  | (Cons _ r) => (1%Z + (length r))%Z
   end.
 Unset Implicit Arguments.
 
@@ -34,8 +44,9 @@ Axiom Length_nonnegative : forall (a:Type), forall (l:(list a)),
   (0%Z <= (length l))%Z.
 
 Axiom Length_nil : forall (a:Type), forall (l:(list a)),
-  ((length l) = 0%Z) <-> (l = (Nil:(list a))).
+  ((length l) = 0%Z) <-> (l = (Nil :(list a))).
 
+(* Why3 assumption *)
 Inductive option (a:Type) :=
   | None : option a
   | Some : a -> option a.
@@ -45,56 +56,70 @@ Unset Contextual Implicit.
 Implicit Arguments Some.
 
 Parameter nth: forall (a:Type), Z -> (list a) -> (option a).
-
 Implicit Arguments nth.
 
 Axiom nth_def : forall (a:Type), forall (n:Z) (l:(list a)),
   match l with
-  | Nil  => ((nth n l) = (None:(option a)))
-  | Cons x r => ((n = 0%Z) -> ((nth n l) = (Some x))) /\ ((~ (n = 0%Z)) ->
+  | Nil => ((nth n l) = (None :(option a)))
+  | (Cons x r) => ((n = 0%Z) -> ((nth n l) = (Some x))) /\ ((~ (n = 0%Z)) ->
       ((nth n l) = (nth (n - 1%Z)%Z r)))
   end.
 
+(* Why3 assumption *)
 Definition zero_at(l:(list Z)) (i:Z): Prop := ((nth i l) = (Some 0%Z)) /\
-  forall (j:Z), ((0%Z <= j)%Z /\ (j <  i)%Z) -> ~ ((nth j l) = (Some 0%Z)).
+  forall (j:Z), ((0%Z <= j)%Z /\ (j < i)%Z) -> ~ ((nth j l) = (Some 0%Z)).
 
+(* Why3 assumption *)
 Definition no_zero(l:(list Z)): Prop := forall (j:Z), ((0%Z <= j)%Z /\
-  (j <  (length l))%Z) -> ~ ((nth j l) = (Some 0%Z)).
+  (j < (length l))%Z) -> ~ ((nth j l) = (Some 0%Z)).
 
+(* Why3 assumption *)
 Inductive ref (a:Type) :=
   | mk_ref : a -> ref a.
 Implicit Arguments mk_ref.
 
-Definition contents (a:Type)(u:(ref a)): a :=
-  match u with
-  | mk_ref contents1 => contents1
+(* Why3 assumption *)
+Definition contents (a:Type)(v:(ref a)): a :=
+  match v with
+  | (mk_ref x) => x
   end.
 Implicit Arguments contents.
 
+(* Why3 assumption *)
 Definition hd (a:Type)(l:(list a)): (option a) :=
   match l with
-  | Nil  => (None:(option a))
-  | Cons h _ => (Some h)
+  | Nil => (None :(option a))
+  | (Cons h _) => (Some h)
   end.
 Implicit Arguments hd.
 
+(* Why3 assumption *)
 Definition tl (a:Type)(l:(list a)): (option (list a)) :=
   match l with
-  | Nil  => (None:(option (list a)))
-  | Cons _ t => (Some t)
+  | Nil => (None :(option (list a)))
+  | (Cons _ t) => (Some t)
   end.
 Implicit Arguments tl.
 
-(* YOU MAY EDIT THE CONTEXT BELOW *)
 
-(* DO NOT EDIT BELOW *)
 
-Theorem WP_parameter_search_loop : True.
+(* Why3 goal *)
+Theorem WP_parameter_search_loop : forall (l:(list Z)), forall (s:(list Z)),
+  forall (i:Z), ((0%Z <= i)%Z /\ (((i + (length s))%Z = (length l)) /\
+  ((forall (j:Z), (0%Z <= j)%Z -> ((nth j s) = (nth (i + j)%Z l))) /\
+  forall (j:Z), ((0%Z <= j)%Z /\ (j < i)%Z) -> ~ ((nth j
+  l) = (Some 0%Z))))) -> ((~ (s = (Nil :(list Z)))) -> ((~ (s = (Nil :(list
+  Z)))) -> forall (result:Z),
+  match s with
+  | Nil => False
+  | (Cons h _) => (result = h)
+  end -> ((result = 0%Z) -> ((((0%Z <= i)%Z /\ (i < (length l))%Z) /\
+  (zero_at l i)) \/ ((i = (length l)) /\ (no_zero l)))))).
 (* YOU MAY EDIT THE PROOF BELOW *)
 intuition.
 destruct s.
-discriminate H4.
-injection H4; intros; subst; clear H4.
+destruct H4.
+subst; subst.
 clear H0 H1.
 left.
 split.
@@ -110,6 +135,5 @@ intuition.
 rewrite H4 in H1.
 auto.
 Qed.
-(* DO NOT EDIT BELOW *)
 
 
