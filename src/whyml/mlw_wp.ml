7c366c9f17ea582b5f742630e8ef3dd5d54a5e31
Johannes Kanig <kanig@adacore.com>
2012-12-06 18:02:31 -0500

    KC19-025 asymmetric conjunction
    this avoids "cascading" errors, by including a VC in the context of following
    VCs

--- src/whyml/mlw_wp.ml
+++ src/whyml/mlw_wp.ml
@@ -1396,7 +1396,7 @@ and fast_wp_desc (env : wp_env) (s : Subst.t) (r : res_type) (e : expr)
           r
           call_post
           xpost in
-        let ok = t_and_simp wp1.ok (wp_implies wp1.post.ne pre) in
+        let ok = wp_and ~sym:false wp1.ok (wp_implies wp1.post.ne pre) in
         let ne = t_and_simp wp1.post.ne post.ne in
         let xne = iter_all_exns [xpost; wp1.exn] (fun ex ->
           let p2_effect = Sreg.union e1_regs call_regs in
@@ -1423,7 +1423,8 @@ and fast_wp_desc (env : wp_env) (s : Subst.t) (r : res_type) (e : expr)
          else e2 in
       let wp1 = fast_wp_expr env s (v, xresult) e1 in
       let wp2 = fast_wp_expr env wp1.post.s r e2 in
-      let ok = t_and_simp wp1.ok (t_implies_subst v wp1.post.ne wp2.ok) in
+      let ok =
+         wp_and ~sym:false wp1.ok (t_implies_subst v wp1.post.ne wp2.ok) in
       let ok = wp_label e ok in
       let e1_regs = regs_of_writes e1.e_effect in
       let e2_regs = regs_of_writes e2.e_effect in
