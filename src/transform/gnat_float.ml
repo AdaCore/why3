47410207a6b27d17fc03837309e852ec43e3a805
Johannes Kanig <kanig@adacore.com>
2013-02-06 17:23:34 +0100

    M130-022 introduce axioms for real constants
    For an occurence of a constant 63.2, introduce an axiom
    
    63.0 = from_int(63)
    
    and rewrite 63.2 as from_int(63) + .02
    
    gnat_float.ml:
       collect all constants and their integer part + the from_int term, and
       introduce such an axiom

--- src/transform/gnat_float.ml
+++ src/transform/gnat_float.ml
@@ -35,13 +35,57 @@ let print_num fmt c =
   | ConstReal rc -> Format.fprintf fmt "real:(%a)" print_real_const rc
 *)
 
+module Compare_Num =
+  struct
+    type t = Number.real_constant
+
+    let opt_compare s1 s2 =
+      match s1, s2 with
+      | None, None -> 0
+      | None, _ -> -1
+      | _, None -> 1
+      | Some s1, Some s2 -> Pervasives.compare s1 s2
+
+    let compare a b =
+      match a, b with
+      | RConstDec (s11, s12, s13), RConstDec (s21, s22, s23)
+      | RConstHex (s11, s12, s13), RConstHex (s21, s22, s23) ->
+          let c = Pervasives.compare s11 s21 in
+          if c = 0 then
+            let c = Pervasives.compare s12 s22 in
+            if c = 0 then opt_compare s13 s23
+            else c
+          else c
+      | RConstDec _, _ -> -1
+      | _, RConstDec _ -> 1
+  end
+
+module Mnum = Extmap.Map.Make (Compare_Num)
+
 let const_replace repl =
-  (* replace a real constant with the return value of function repl *)
-  let rec const_replace t =
-    let t = t_map const_replace t in
+  (* the function repl is expected to accept a real constant and return either
+     [None], if this constant is not "interesting", or
+     Some (trans, from_int, int_part), where
+       [trans] is the sum from_int (int_part) + (fractional part),
+       [from_int] is just the part "from_int (int_part)"
+       [int_part] is the int_part as a float.
+     For example, for 63.1, we obtain
+       trans = from_int 63 + 0.1
+       from_int = from_int 63
+       int_part = 63.0
+
+     const_replace now replaces every "interesting" real constant by [trans].
+     In addition, it fills a map with [int_part] |-> [from_int] *)
+  let rec const_replace (acc : term Mnum.t) t =
+    let acc, t = t_map_fold const_replace acc t in
     match t.t_node with
-    | Tconst (ConstReal rc) -> repl rc
-    | _ -> t
+    | Tconst (ConstReal rc) ->
+        begin match repl rc with
+        | Some (trans, from_int, int_part) ->
+            Mnum.add int_part from_int acc, trans
+        | None -> acc, t
+        end
+    | _ -> acc, t
   in
   const_replace
 
@@ -59,6 +103,13 @@ let extract_from_rc rc =
       else Some (int, frac)
   | _ -> None
 
+let add_axioms map task =
+  (* given a map [int_part] |-> [from_int_term], generate axioms of the form
+     int_part = from_int_term *)
+  Mnum.fold (fun re fr task ->
+    let sym = create_prsymbol (Ident.id_fresh "real const axiom") in
+    let term = t_equ (t_const (ConstReal re)) fr in
+    Task.add_decl task (create_prop_decl Paxiom sym term)) map task
 
 let float_lift th_from_int th_real =
   let from_int_ls =
@@ -75,36 +126,46 @@ let float_lift th_from_int th_real =
     t_app from_int_ls [t] (Some Ty.ty_real) in
   let rc_to_new_term rc =
     (* function that given a real constant x.y, builds the term
-       from_int (x) + 0.y *)
+       from_int (x) + 0.y, and returns [from_int x] and [x], see also the
+       documentation for [const_replace] *)
     match extract_from_rc rc with
     | Some (i, frac) ->
         let i_t = t_from_int (t_const (ConstInt (Number.int_const_dec i))) in
         let frac_t =
           t_const (ConstReal (Number.real_const_dec "0" frac None)) in
-        if i = "0" then frac_t
-        else if frac = "0" then i_t
-        else t_add i_t frac_t
-    | None -> t_const (ConstReal rc)
+        let trans =
+          if i = "0" then frac_t
+          else if frac = "0" then i_t
+          else t_add i_t frac_t
+        in
+        Some (trans, i_t, Number.real_const_dec i "0" None)
+    | None -> None
   in
   let replace = const_replace rc_to_new_term in
-  Trans.fold_map (fun thd ((),( task : task)) ->
+  Trans.fold_map (fun thd ((acc : term Mnum.t), task) ->
+    (* iterate over all declarations that contain terms, potentially replace
+       constants, and collect associations *)
     match thd.task_decl.td_node with
     | Decl { d_node = Dlogic ld } ->
-        let ld = List.map (fun (fs, defn) ->
+        let acc, ld = List.fold_left (fun (acc, ld) (fs, defn) ->
           let vls, t = open_ls_defn defn in
-          let t = replace t in
-          make_ls_defn fs vls t) ld in
+          let acc, t = replace acc t in
+          acc, make_ls_defn fs vls t :: ld) (acc,[]) ld in
         let task = Task.add_decl task (create_logic_decl ld) in
-        (), task
+        acc, task
     | Decl { d_node = Dprop (Paxiom, ps, term) } ->
-        let term = replace term in
+        let acc, term = replace acc term in
         let task = Task.add_decl task (create_prop_decl Paxiom ps term) in
-        (), task
+        acc, task
     | Decl { d_node = Dprop (Pgoal, ps, term) } ->
-        let term = replace term in
-        (), Task.add_decl task (create_prop_decl Pgoal ps term)
-    | _ -> (), add_tdecl task thd.task_decl
-  ) () None
+        (* We transform the goal term to get the constants in the goal; Then,
+           we add axioms for all collected constants, including in previous
+           axioms and definitions, and then we add the goal as well. *)
+        let acc, term = replace acc term in
+        let task = add_axioms acc task in
+        Mnum.empty, Task.add_decl task (create_prop_decl Pgoal ps term)
+    | _ -> acc, add_tdecl task thd.task_decl
+  ) Mnum.empty None
 
 let () =
   Trans.register_env_transform ~desc:"float literals" "float_literal"
