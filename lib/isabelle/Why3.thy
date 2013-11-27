f1546983203d6f74d52f532975cdbbc827c87622
Stefan Berghofer <stefan.berghofer@secunet.com>
2013-11-27 13:32:59 +0100

    Added more realizations

--- lib/isabelle/Why3.thy
+++ lib/isabelle/Why3.thy
@@ -1,5 +1,9 @@
 theory Why3
-imports Why3_Set Why3_List
+imports
+  Why3_Map
+  Why3_Set
+  Why3_List
+  Why3_Int
 begin
 
 end
--- lib/isabelle/Why3_List.thy
+++ lib/isabelle/Why3_List.thy
@@ -28,6 +28,9 @@ section {* Nth element of a list *}
 
 why3_open "Nth.xml"
 
+lemma nth_eq: "0 \<le> i \<Longrightarrow> nat i < length xs \<Longrightarrow> nth i xs = Some (xs ! nat i)"
+  by (induct xs arbitrary: i) (auto simp add: nat_diff_distrib)
+
 why3_end
 
 
@@ -69,6 +72,89 @@ next
     with `0 < i` show ?thesis by auto
   qed simp
 qed
+
+why3_end
+
+
+section {* Head and tail *}
+
+why3_open "HdTl.xml"
+
+why3_end
+
+
+section {* Relation between head, tail, and nth *}
+
+why3_open "NthHdTl.xml"
+
+why3_vc Nth_tl
+  using assms
+  by (simp add: tl_def split add: list.split_asm)
+
+why3_vc Nth0_head
+  by (simp add: hd_def split add: list.split)
+
+why3_end
+
+
+section {* Appending two lists *}
+
+why3_open "Append.xml"
+
+why3_vc infix_plpl_def by (simp split add: list.split)
+
+why3_vc Append_assoc by simp
+
+why3_vc Append_l_nil by simp
+
+why3_vc Append_length by simp
+
+why3_vc mem_append by simp
+
+why3_vc mem_decomp
+  using assms
+  by (simp add: in_set_conv_decomp)
+
+why3_end
+
+
+why3_open "NthLengthAppend.xml"
+
+why3_vc nth_append_1
+proof (cases "0 \<le> i")
+  case True
+  with assms have "nat i < length l1" by simp
+  with True show ?thesis
+    by (simp add: nth_eq nth_append)
+next
+  case False
+  then show ?thesis by (simp add: nth_none_1)
+qed
+
+why3_vc nth_append_2
+proof (cases "nat i < length (l1 @ l2)")
+  case True
+  with assms show ?thesis
+    by (auto simp add: nth_eq nth_append nat_diff_distrib)
+next
+  case False
+  with assms show ?thesis by (simp add: nth_none_2)
+qed
+
+why3_end
+
+
+section {* Reversing a list *}
+
+why3_open "Reverse.xml"
+
+why3_vc reverse_def by (simp split add: list.split)
+
+why3_vc Reverse_length by simp
+
+why3_vc reverse_append by simp
+
+why3_vc reverse_reverse by simp
 
 why3_end
 
