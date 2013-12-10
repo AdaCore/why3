e0279f0b1a4c75ba6cc02674842d8d00b095f862
François Bobot <francois.bobot@cea.fr>
2013-12-10 23:36:48 +0100

    [why3session] merge the option hist of why3session info and why3session csv --valid-by-time
                  rename options of why3session csv

--- src/why3session/why3session_csv.ml
+++ src/why3session/why3session_csv.ml
@@ -16,10 +16,13 @@ open Format
 module Hprover = Whyconf.Hprover
 module S = Session
 
-let opt_output_dir = ref ""
+let orig_dir = Sys.getcwd ()
+
+let opt_output_dir = ref "./"
 
 let opt_provers = ref []
 let opt_aggregate = ref false
+let opt_print_csv = ref false
 
 let simple_read_opt_prover s =
   try
@@ -31,49 +34,49 @@ let simple_read_opt_prover s =
 
 
 let opt_value_not_valid = ref "-1."
-let opt_gnuplot = false
+type gnuplot =
+  | PDF
+  | PNG
+  | SVG
+  | QT
+  | GP
 
-type style =
-| Normal
-| By_Time
+let opt_gnuplot = ref []
+let select_gnuplot = function
+  | "pdf" -> opt_gnuplot := PDF::!opt_gnuplot
+  | "png" -> opt_gnuplot := PNG::!opt_gnuplot
+  | "svg" -> opt_gnuplot := SVG::!opt_gnuplot
+  | "qt"  -> opt_gnuplot := QT::!opt_gnuplot
+  | "gp" -> opt_gnuplot := GP::!opt_gnuplot
+  | _ -> assert false (** absurd: Arg already filtered bad cases *)
 
-let opt_style = ref Normal
-
-let set_style s =
-  match s with
-  | "normal" -> opt_style := Normal
-  | "by_time" -> opt_style := By_Time
-  | _  -> assert false
-
-(* TODO *)
-let print_gnuplot =
-"set datafile separator \",\"
-set key autotitle columnhead
-
-set term png
-set logscale xy 10
-set object rectangle from screen 0,0 to screen 1,1 behind
-
-set output \"%s\"
-set xlabel \"%a\"
-max(x,y) = x > y ? x : y
-plot     \"aggregate.csv\" using (max($4,0.001)):(max($3,0.001))
-"
+let opt_data = ref false
+let opt_by_time = ref false
 
 let spec =
   ("-o",
    Arg.Set_string opt_output_dir,
-   "<dir> where to produce LaTeX files (default: session dir)") ::
+   "<dir> where to produce files (default: session dir)") ::
   ("--filter-prover",
    Arg.String (fun s ->
      opt_provers := (simple_read_opt_prover s)::!opt_provers),
    " select the provers")::
+    ("--data", Arg.Set opt_data,
+     "For all the goals give the time taken by each provers that proved it.")::
     ("--aggregate", Arg.Set opt_aggregate,
-     " aggregate all the input into one")::
+     " aggregate all the input into one file named \
+       aggregate_data.* and aggregate.*")::
     ("--value-not-valid", Arg.Set_string opt_value_not_valid,
-     " value used when the external proof is not valid")::
-    ("--style", Arg.Symbol (["normal";"by_time"],set_style)," set the style")
-      :: (common_options)
+     " value used when the external proof is not valid (only for --data)")::
+    ("--valid_by_time", Arg.Set opt_by_time,
+     "Give the evolution of the number of goal proved \
+      for each provers (default)")::
+    ("--gnuplot", Arg.Symbol (["pdf";"png";"svg";"qt";"gp"],select_gnuplot),
+     "Run gnuplot on the produced file (currently only with --valid_by_time)\
+      (gp write the gnuplot script used for generating the other case)")::
+    ("--output-csv", Arg.Set opt_print_csv,
+     "print the csv, set byt default when --gnuplot is not set")::
+    common_options
 
 (** Normal *)
 
@@ -117,7 +120,7 @@ let run_one_normal filter_provers fmt fname =
 
 let run_normal dir filter_provers =
   if !opt_aggregate then
-    let out = open_out (Filename.concat dir "aggregate.csv") in
+    let out = open_out (Filename.concat dir "aggregate_data.csv") in
     let fmt = formatter_of_out_channel out in
     Pp.wnl fmt;
     iter_files (run_one_normal filter_provers fmt);
@@ -126,7 +129,7 @@ let run_normal dir filter_provers =
     iter_files (fun fname ->
       let out = open_out
         (Filename.concat dir
-           ((try Filename.chop_extension fname with _ -> fname)^".csv")) in
+           ((try Filename.chop_extension fname with _ -> fname)^"_data.csv")) in
       let fmt = formatter_of_out_channel out in
       Pp.wnl fmt;
       run_one_normal filter_provers fmt fname;
@@ -197,27 +200,123 @@ let print_provers_time (provers_time : float list Whyconf.Hprover.t) fmt =
       print_line l end in
   print_line l
 
+let create_gnuplot_by_time nb_provers fmt =
+  Format.pp_print_string fmt
+"\
+set datafile separator \",\"\n\
+set key autotitle columnhead\n\
+set logscale x 10\n\
+set object rectangle from screen 0,0 to screen 1,1 behind\n\
+set xlabel \"time\"\n\
+\n\
+plot ";
+  for i=1 to nb_provers do
+    Format.fprintf fmt
+      "data_filename using ($1):($%i) with steps"
+      (i+1);
+    if i <> nb_provers then Format.pp_print_string fmt ",\\\n";
+  done
+
+let print_file out f : unit =
+    let fmt = formatter_of_out_channel out in
+    Pp.wnl fmt;
+    f fmt;
+    close_out out
+
+let print_args fmt args =
+  (Pp.print_iter1 Array.iter
+     (fun fmt () -> Format.pp_print_string fmt " ") (** no @\n *)
+     (fun fmt -> Format.fprintf fmt "%S"))
+   fmt args
+
+let call_gnuplot arg1 arg2 csv_file gp_file =
+  let args =
+    [| "gnuplot"; "-e"; arg1; "-e"; arg2;
+       "-e"; Printf.sprintf "data_filename = \"%s\"" (Opt.get csv_file);
+       Opt.get gp_file |] in
+  Debug.dprintf verbose "exec:%a@." print_args args;
+  let pid = Unix.create_process "gnuplot" args
+      Unix.stdin Unix.stdout Unix.stderr  in
+  match Unix.waitpid [] pid with
+  | _, Unix.WEXITED 0 -> ()
+  | _ -> Format.eprintf "Command failed:%a@." print_args args
+
+
+let run_by_time_gen dir canonical_name iter =
+  let to_remove = Stack.create () in
+  let canonical_name = Filename.concat dir canonical_name in
+  (** compute stats *)
+  let provers_time = Whyconf.Hprover.create 5 in
+  iter provers_time;
+  (** print .csv if necessary *)
+  let csv_file =
+     if !opt_gnuplot = [] || !opt_print_csv then
+       let fname = canonical_name ^ ".csv" in
+       print_file (open_out fname)
+         (fun fmt -> print_provers_time provers_time fmt);
+       Some fname
+     else if !opt_gnuplot <> [] then
+       let fname,ch = Filename.open_temp_file "why3session" ".csv" in
+       Stack.push fname to_remove;
+       print_file ch
+         (fun fmt -> print_provers_time provers_time fmt);
+       Some fname
+     else None
+  in
+
+  (** create .gp if necessary *)
+  let nb_provers = Whyconf.Hprover.length provers_time in
+  let gp_file =
+    if List.mem GP !opt_gnuplot
+    then let fname = canonical_name ^ ".gp" in
+      print_file (open_out fname)
+        (fun fmt -> create_gnuplot_by_time nb_provers fmt);
+      Some fname
+    else if !opt_gnuplot <> [] then
+      let fname,ch = Filename.open_temp_file "why3session" ".gp" in
+      Stack.push fname to_remove;
+      print_file ch
+        (fun fmt -> create_gnuplot_by_time nb_provers fmt);
+      Some fname
+    else None
+  in
+
+  (** output .png, .pdf, .csv and run .qt if necessary *)
+  if List.mem PNG !opt_gnuplot then
+    call_gnuplot
+      "set terminal pngcairo size 600, 400"
+      (Printf.sprintf "set output \"%s.png\"" canonical_name)
+      csv_file gp_file;
+  if List.mem PDF !opt_gnuplot then
+    call_gnuplot
+      "set terminal pdfcairo"
+      (Printf.sprintf "set output \"%s.pdf\"" canonical_name)
+      csv_file gp_file;
+  if List.mem SVG !opt_gnuplot then
+    call_gnuplot
+      "set terminal svg"
+      (Printf.sprintf "set output \"%s.svg\"" canonical_name)
+      csv_file gp_file;
+  if List.mem QT !opt_gnuplot then
+    call_gnuplot
+      "set terminal qt persist"
+      ""
+      csv_file gp_file;
+  (** Clean up temporary files *)
+  Stack.iter Sys.remove to_remove
+
 
 let run_by_time dir filter_provers =
   if !opt_aggregate then
-    let out = open_out (Filename.concat dir "aggregate.csv") in
-    let fmt = formatter_of_out_channel out in
-    Pp.wnl fmt;
-    let provers_time = Whyconf.Hprover.create 5 in
-    iter_files (run_one_by_time provers_time filter_provers);
-    print_provers_time provers_time fmt;
-    close_out out
+    run_by_time_gen dir "aggregate.csv"
+      (fun provers_time ->
+         iter_files (run_one_by_time provers_time filter_provers))
   else
     iter_files (fun fname ->
-      let out = open_out
-        (Filename.concat dir
-           ((try Filename.chop_extension fname with _ -> fname)^".csv")) in
-      let fmt = formatter_of_out_channel out in
-      Pp.wnl fmt;
-      let provers_time = Whyconf.Hprover.create 5 in
-      run_one_by_time provers_time filter_provers fname;
-      print_provers_time provers_time fmt;
-      close_out out)
+        run_by_time_gen dir (try Filename.chop_extension fname with _ -> fname)
+          (fun provers_time ->
+             run_one_by_time provers_time filter_provers fname)
+      )
 
 (* Common *)
 
@@ -226,14 +325,14 @@ let run () =
   if should_exit1 then exit 1;
   let filter_provers =
     List.map (Whyconf.filter_prover_with_shortcut whyconf) !opt_provers in
-  let dir = !opt_output_dir in
-  match !opt_style with
-  | Normal -> run_normal dir filter_provers
-  | By_Time -> run_by_time dir filter_provers
+  let dir = Sysutil.absolutize_filename orig_dir (!opt_output_dir) in
+  if !opt_data then run_normal dir filter_provers;
+  if !opt_by_time || not (!opt_data) then run_by_time dir filter_provers
 
 let cmd =
   { cmd_spec = spec;
-    cmd_desc = "output session as a table for simple processing.";
+    cmd_desc =
+      "output session as a table or graph for simple processing or viewing.";
     cmd_name = "csv";
     cmd_run  = run;
   }
