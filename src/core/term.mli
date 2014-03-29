dad2bdf3d35b092b0a68f72b2a074d3baee48bc2
Claude Marche <Claude.Marche@inria.fr>
2014-03-29 13:17:48 +0100

    add necessary comment

--- src/core/term.mli
+++ src/core/term.mli
@@ -145,6 +145,8 @@ module Mterm : Extmap.S with type key = term
 module Sterm : Extset.S with module M = Mterm
 module Hterm : Exthtbl.S with type key = term
 
+(** term equality modulo alpha-equivalence and location *)
+
 val t_compare : term -> term -> int
 val t_equal : term -> term -> bool
 val t_hash : term -> int
