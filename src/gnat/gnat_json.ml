ed23f7aeeebc2bed7a533e2f454e85fdc5438d06
Johannes Kanig <kanig@adacore.com>
2014-08-20 10:12:33 +0200

    N820-001 fix quoting of JSON strings
    Strings were unquoted in JSON output from Ocaml, doing this now
    (this JSON is only parsed by gnat2why)
    
    * gnat_json.ml
    (string) quote " and \ when outputting to JSON format

--- src/gnat/gnat_json.ml
+++ src/gnat/gnat_json.ml
@@ -1,6 +1,17 @@
 open Why3
 
-let string fmt s = Format.fprintf fmt "\"%s\"" s
+let string fmt s =
+  let b = Buffer.create (2 * String.length s) in
+  Buffer.add_char b '"';
+  for i = 0 to String.length s -1 do
+    match s.[i] with
+    | '"' -> Buffer.add_string b "\\\""
+    | '\\' -> Buffer.add_string b "\\\\"
+    | c -> Buffer.add_char b c
+  done;
+  Buffer.add_char b '"';
+  Format.fprintf fmt "%s" (Buffer.contents b)
+
 let int fmt d = Format.fprintf fmt "%d" d
 let bool fmt b = Format.fprintf fmt "%b" b
 
