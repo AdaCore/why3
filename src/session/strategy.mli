49c520cf76ed27b84cb4b08381eb1dfc56428970
Andrei Paskevich <andrei@lri.fr>
2014-08-29 13:10:49 +0200

    add headers to the new files

--- src/session/strategy.mli
+++ src/session/strategy.mli
@@ -1,5 +1,13 @@
-
-open Why3
+(********************************************************************)
+(*                                                                  *)
+(*  The Why3 Verification Platform   /   The Why3 Development Team  *)
+(*  Copyright 2010-2014   --   INRIA - CNRS - Paris-Sud University  *)
+(*                                                                  *)
+(*  This software is distributed under the terms of the GNU Lesser  *)
+(*  General Public License version 2.1, with the special exception  *)
+(*  on linking described in file LICENSE.                           *)
+(*                                                                  *)
+(********************************************************************)
 
 (** {2 User-defined strategies} *)
 
