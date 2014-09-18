1975bbfdca1003fa212ba23a71ce7f81bf27675e
Léon Gondelman <lgondelmann@gmail.com>
2014-09-18 17:09:28 +0200

    New transformation curry

---
+++ src/transform/curry.ml
@@ -0,0 +1,23 @@
+open Ident
+open Term
+open Decl
+open Theory
+open Task
+
+
+let rec curry t = match t.t_node with
+  | Tbinop (Timplies, lhs, rhs) -> expand t lhs (curry rhs)
+  | _ -> t_map curry t
+
+and expand orig l r = match l.t_node with
+  | Tbinop (Tand, a, b) -> expand orig a (expand orig b r)
+  | _  -> t_label_copy orig (t_implies (curry l) r)
+
+
+let curry = Trans.goal (fun pr t -> [create_prop_decl Pgoal pr (curry t)])
+
+
+
+let () =
+  Trans.register_transform "curry" curry
+    ~desc:"Currify."
