68385317b7ff9d14cc52ff14201a8d6f9771366f
Martin Clochard <martin.clochard@lri.fr>
2014-10-03 12:57:59 +0200

    Renamed transformation curry into prop_curry

--- src/transform/prop_curry.ml
+++ src/transform/prop_curry.ml
@@ -1,6 +1,6 @@
+
 open Term
 open Decl
-
 
 let rec curry t = match t.t_node with
   | Tbinop (Timplies, lhs, rhs) -> expand t lhs (curry rhs)
@@ -10,11 +10,10 @@ and expand orig l r = match l.t_node with
   | Tbinop (Tand, a, b) -> expand orig a (expand orig b r)
   | _  -> t_label_copy orig (t_implies (curry l) r)
 
-
 let curry = Trans.goal (fun pr t -> [create_prop_decl Pgoal pr (curry t)])
 
-
-
 let () =
-  Trans.register_transform "curry" curry
-    ~desc:"Currify."
+  Trans.register_transform "prop_curry" curry
+    ~desc:"Transform@ conjunctions@ in@ implication@ premises@ into@ \
+      sequences@ of@ premises."
+
