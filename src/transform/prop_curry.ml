dbc424b608f3112587b546309af177091182791e
Claude Marche <Claude.Marche@inria.fr>
2015-03-19 12:59:44 +0100

    updated headers for 2015, new headers in .v files

--- src/transform/prop_curry.ml
+++ src/transform/prop_curry.ml
@@ -1,3 +1,12 @@
+(********************************************************************)
+(*                                                                  *)
+(*  The Why3 Verification Platform   /   The Why3 Development Team  *)
+(*  Copyright 2010-2015   --   INRIA - CNRS - Paris-Sud University  *)
+(*                                                                  *)
+(*  This software is distributed under the terms of the GNU Lesser  *)
+(*  General Public License version 2.1, with the special exception  *)
+(*  on linking described in file LICENSE.                           *)
+(********************************************************************)
 
 open Term
 open Decl
