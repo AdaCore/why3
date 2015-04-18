4dacced6d5a5d025e05cda1ae7b78507f493bd70
Claude Marché <Claude.Marche@inria.fr>
2015-04-18 09:36:58 +0200

    update roadmap

--- src/whyml/mlw_interp.ml
+++ src/whyml/mlw_interp.ml
@@ -1284,7 +1284,7 @@ and exec_app env s ps args (*spec*) ity_result =
 (*
       Mreg.union (fun _ x _ -> Some x) subst env.regenv }
 *)
-    Mreg.set_union subst env.regenv
+    Mreg.set_union subst env.regenv }
   in
   match find_definition env ps with
     | Some d ->
