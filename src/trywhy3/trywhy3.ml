eeecc325c74fe558a28cbb1c09992c2b4a8f9459
Claude Marche <Claude.Marche@inria.fr>
2015-09-06 21:22:32 +0200

    Try Why3: new version with a more elaborate text editor
    needs 'ace' to run, which can be retrieved using
    
    git clone https://github.com/ajaxorg/ace-builds.git

--- src/trywhy3/trywhy3.ml
+++ src/trywhy3/trywhy3.ml
@@ -1,74 +1,5 @@
 
 
-
-let examples =
-  [ "Drinkers paradox","
-theory T
-
-   (** Type of all persons *)
-   type person
-
-   (** Predicate saying that some person drinks *)
-   predicate drinks person
-
-   (** Paradox: there exists a person x such that if x drinks,
-       then everybody drinks *)
-   goal drinkers_paradox: exists x:person. drinks x -> forall y:person. drinks y
-
-end
-";
-   "Simple Arithmetic","
-theory T
-
-   use import int.Int
-
-   goal g: exists x:int. x*(x+1) = 42
-
-end
-";
-"Integral square root","
-module M
-
-  use import int.Int
-  use import ref.Ref
-
-  function sqr (x:int) : int = x * x
-
-  let isqrt (x:int) : int
-    requires { x >= 0 }
-    ensures { result >= 0 }
-    ensures { result <= x < sqr (result + 1) }
-  = let count = ref 0 in
-    let sum = ref 1 in
-    while !sum <= x do
-      invariant { !count >= 0 }
-      invariant { x >= sqr !count }
-      invariant { !sum = sqr (!count+1) }
-      variant   { x - !count }
-      count := !count + 1;
-      sum := !sum + 2 * !count + 1
-    done;
-    !count
-
-  let main () ensures { result = 4 } = isqrt 17
-
-end
-";
-  ]
-
-(** registering the std lib *)
-
-(*
-let () =
-  List.iter
-    (fun (name,content) ->
-      Sys_js.register_file ~name ~content;
-    ) Theories.theories
-*)
-
-
-(** Rendering in the browser *)
-
 module D = Dom_html
 
 module Html = struct
@@ -95,6 +26,13 @@ let ul nl =
   node x
 
 end
+
+let get_opt o =
+  Js.Opt.get o (fun () -> assert false)
+
+let log s =
+  Firebug.console ## log (Js.string s)
+
 
 let temp_file_name = "/input.mlw"
 let why3_conf_file = "/why3.conf"
@@ -140,141 +78,115 @@ let run_alt_ergo_on_task t =
   Driver.print_task alt_ergo_driver str_formatter t;
   let text = flush_str_formatter () in
   (* TODO ! *)
+(* from Alt-Ergo:
+  let a = Why_parser.file Why_lexer.token lb in
+  let ltd, typ_env = Why_typing.file false Why_typing.empty_env a in
+  let declss = Why_typing.split_goals ltd in
+  SAT.start ();
+  let declss = List.map (List.map fst) declss in
+  let report = FE.print_status in
+  let pruning =
+    List.map
+      (fun d ->
+        if select () > 0 then Pruning.split_and_prune (select ()) d
+        else [List.map (fun f -> f,true) d])
+  in
+  List.iter
+    (List.iter
+       (fun dcl ->
+	 let cnf = Cnf.make dcl in
+	 ignore (Queue.fold (FE.process_decl report)
+		   (SAT.empty (), true, Explanation.empty) cnf)
+       )) (pruning declss)
+*)
   text
 
 let split_trans = Trans.lookup_transform_l "split_goal_wp" env
 
-let run textbox preview (_ : D.mouseEvent Js.t) : bool Js.t =
-  let text = Js.to_string (textbox##value) in
+let prove text =
   let ch = open_out temp_file_name in
   output_string ch text;
   close_out ch;
-  let answer =
-    try
-      (* TODO: add a function Env.read_string or Env.read_from_lexbuf ? *)
-      let theories = Env.read_file Env.base_language env temp_file_name in
-(*
-      Html.par
-        [Html.of_string
-           (Pp.sprintf "parsing and typing OK, %d theories"
-              (Stdlib.Mstr.cardinal theories))]
-*)
-      let theories =
-        Stdlib.Mstr.fold
-          (fun thname th acc ->
-            let tasks = Task.split_theory th None None in
-            let tasks = List.map
-              (fun t ->
-                let (id,expl,_) = Termcode.goal_expl_task ~root:true t in
-                let expl = match expl with
-                  | Some s -> s
-                  | None -> id.Ident.id_string
-                in
-                let tl = Trans.apply split_trans t in
-                let tasks =
-                  List.map
-                    (fun task ->
-                      let (id,expl,_) = Termcode.goal_expl_task ~root:false task in
-                      let expl = match expl with
-                        | Some s -> s
-                        | None -> id.Ident.id_string
-                      in
-                      let result = run_alt_ergo_on_task task in
-                      let result =
-                        if String.length result > 80 then
-                          "..." ^ String.sub result (String.length result - 80) 80
-                        else result
-                      in
-                      [Html.of_string (expl ^" : " ^ result)])
-                    tl
-                in
-                [Html.of_string expl; Html.ul tasks])
-              tasks
-            in
-            [ Html.of_string ("Theory " ^ thname); Html.ul tasks]
-            :: acc)
-          theories []
-      in
-      Html.ul theories
-(*
-      Stdlib.Mstr.iter
-        (fun _thname th ->
+  try
+    (* TODO: add a function Env.read_string or Env.read_from_lexbuf ? *)
+    let theories = Env.read_file Env.base_language env temp_file_name in
+    let theories =
+      Stdlib.Mstr.fold
+        (fun thname th acc ->
           let tasks = Task.split_theory th None None in
-          List.iter
-            (fun task ->
-              let (id,expl,_) = Termcode.goal_expl_task ~root:true task in
+          let tasks = List.map
+            (fun t ->
+              let (id,expl,_) = Termcode.goal_expl_task ~root:true t in
               let expl = match expl with
                 | Some s -> s
                 | None -> id.Ident.id_string
               in
-              push_answer
-                (Pp.sprintf "Goal: %s@\n" expl))
-            tasks)
-*)
-    with
-    | Loc.Located(loc,Parser.Error) ->
-      let (_,l,b,e) = Loc.get loc in
-      Html.par
-        [Html.of_string
-            (Pp.sprintf "syntax error line %d, columns %d-%d" l b e)]
-    | Loc.Located(loc,e') ->
-      let (_,l,b,e) = Loc.get loc in
-      Html.par
-        [Html.of_string
-            (Pp.sprintf
-               "error line %d, columns %d-%d: %a" l b e
-               Exn_printer.exn_printer e')]
-    | e ->
-      Html.par
-        [Html.of_string
-            (Pp.sprintf
-               "unexpected exception: %a (%s)" Exn_printer.exn_printer e
-               (Printexc.to_string e))]
+              let tl = Trans.apply split_trans t in
+              let tasks =
+                List.map
+                  (fun task ->
+                    let (id,expl,_) = Termcode.goal_expl_task ~root:false task in
+                    let expl = match expl with
+                      | Some s -> s
+                      | None -> id.Ident.id_string
+                    in
+                    let result = run_alt_ergo_on_task task in
+                    let result =
+                      if String.length result > 80 then
+                        "..." ^ String.sub result (String.length result - 80) 80
+                      else result
+                    in
+                    [Html.of_string (expl ^" : " ^ result)])
+                  tl
+              in
+              [Html.of_string expl; Html.ul tasks])
+            tasks
+          in
+          [ Html.of_string ("Theory " ^ thname); Html.ul tasks]
+          :: acc)
+        theories []
+    in
+    Html.ul theories
+  with
+  | Loc.Located(loc,Parser.Error) ->
+    let (_,l,b,e) = Loc.get loc in
+    Html.par
+      [Html.of_string
+          (Pp.sprintf "syntax error line %d, columns %d-%d" l b e)]
+  | Loc.Located(loc,e') ->
+    let (_,l,b,e) = Loc.get loc in
+    Html.par
+      [Html.of_string
+          (Pp.sprintf
+             "error line %d, columns %d-%d: %a" l b e
+             Exn_printer.exn_printer e')]
+  | e ->
+    Html.par
+      [Html.of_string
+          (Pp.sprintf
+             "unexpected exception: %a (%s)" Exn_printer.exn_printer e
+             (Printexc.to_string e))]
+let () =
+  let handler = Dom.handler
+    (fun _ev ->
+      log "why3 prove is running";
+      let global = Js.Unsafe.global in
+      let editor = Js.Unsafe.get global (Js.string "editor") in
+      let code = Js.to_string (Js.Unsafe.meth_call editor "getValue" [| |]) in
+      let answer = prove code in
+      (* remove the previous answer if any *)
+      let console =
+        get_opt (Dom_html.document ## getElementById (Js.string "console"))
+      in
+      Js.Opt.iter (console##lastChild) (Dom.removeChild console);
+      (* put the new answer *)
+      Dom.appendChild console answer;
+      Js._false)
   in
-  (* remove the previous answer if any *)
-  Js.Opt.iter (preview##lastChild) (Dom.removeChild preview);
-  (* put the new answer *)
-  Dom.appendChild preview answer;
-  Js._false
-
-let onload (_event : #Dom_html.event Js.t) : bool Js.t =
-  let input =
-    Js.Opt.get (Html.d##getElementById(Js.string "input"))
-      (fun () -> assert false) in
-  let output =
-    Js.Opt.get (Html.d##getElementById(Js.string "output"))
-      (fun () -> assert false) in
-  (* first, the textbox *)
-  let textbox = D.createTextarea Html.d in
-  textbox##rows <- 32; textbox##cols <- 64;
-  Dom.appendChild input textbox;
-  (* second, the example buttons *)
-  List.iter
-    (fun (name,text) ->
-      let b = D.createButton ~name:(Js.string name) Html.d in
-      b##textContent <- Js.some (Js.string name);
-      Dom.appendChild output b;
-      b##onclick <-
-        D.handler
-        (fun (_ : D.mouseEvent Js.t) ->
-          textbox##value <- Js.string text;
-          Js._false))
-    examples;
-  Dom.appendChild output (D.createBr Html.d);
-  (* third, the go button *)
-  let go = D.createButton ~name:(Js.string "go") Html.d in
-  go##textContent <- Js.some (Js.string "Go");
-  Dom.appendChild output go;
-  Dom.appendChild output (D.createBr Html.d);
-  (* then, the answer zone *)
-  let preview = D.createDiv Html.d in
-  go##onclick <- D.handler (run textbox preview);
-  preview##style##border <- Js.string "1px black";
-  preview##style##padding <- Js.string "5px";
-  Dom.appendChild output preview;
-  Js._false
-
-let _ = D.window##onload <- D.handler onload
+  let button =
+    get_opt (Dom_html.document ## getElementById (Js.string "prove"))
+  in
+  button ## onclick <- handler
 
 
 (*
