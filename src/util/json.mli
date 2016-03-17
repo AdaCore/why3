76fb6b30f4cb05cc4bd82acf25893cf070aed543
Johannes Kanig <kanig@adacore.com>
2016-03-17 12:31:55 +0900

    add some comments to JSON interface

--- src/util/json.mli
+++ src/util/json.mli
@@ -10,18 +10,35 @@
 (********************************************************************)
 
 val string : Format.formatter -> string -> unit
+(* print json string, that is add '"' to the front and back, and escape '"' and
+   '\' in the string *)
 val int : Format.formatter -> int -> unit
+(* print an integer *)
 val bool : Format.formatter -> bool -> unit
+(* print an boolean *)
+val float : Format.formatter -> float -> unit
+(* print an floating point number *)
 
 val list :
   (Format.formatter -> 'a -> unit) -> Format.formatter -> 'a list -> unit
+(* provided a printer for elements, print a json list of these elements. In the
+   case of the empty list, print the json empty list [] *)
 
 val map_bindings :
-  ('a -> string) -> 
-  (Format.formatter -> 'b -> unit) -> 
+  ('a -> string) ->
+  (Format.formatter -> 'b -> unit) ->
   Format.formatter ->
   ('a * 'b) list ->
   unit
+(* arguments:
+   * a mapping from keys to strings;
+   * a printer of values
+   * the formatter
+   * a list of key,value pairs
+  action: print the list of key-value pairs as a json record, if the list is
+  empty, print the empty record *)
 
 val print_json_field :
   string -> (Format.formatter -> 'a -> unit) -> Format.formatter -> 'a -> unit
+(* given a field name, a value and a printer for the value, print a json
+   mapping (field assignment). Do not print anything else. *)
