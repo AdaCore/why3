99b6e736ae1c54217586ef1d1888478284cdd4e7
Andrei Paskevich <andrei@lri.fr>
2016-04-20 20:28:56 +0200

    headers on the added files

--- src/server/server-unix.c
+++ src/server/server-unix.c
@@ -1,3 +1,14 @@
+/********************************************************************/
+/*                                                                  */
+/*  The Why3 Verification Platform   /   The Why3 Development Team  */
+/*  Copyright 2010-2016   --   INRIA - CNRS - Paris-Sud University  */
+/*                                                                  */
+/*  This software is distributed under the terms of the GNU Lesser  */
+/*  General Public License version 2.1, with the special exception  */
+/*  on linking described in file LICENSE.                           */
+/*                                                                  */
+/********************************************************************/
+
 // This is the unix implementation of the VC server. It uses the poll
 // mechanism to wait for events, plus the "self pipe trick" to handle
 // terminating child processes.
--- src/server/server-win.c
+++ src/server/server-win.c
@@ -1,3 +1,14 @@
+/********************************************************************/
+/*                                                                  */
+/*  The Why3 Verification Platform   /   The Why3 Development Team  */
+/*  Copyright 2010-2016   --   INRIA - CNRS - Paris-Sud University  */
+/*                                                                  */
+/*  This software is distributed under the terms of the GNU Lesser  */
+/*  General Public License version 2.1, with the special exception  */
+/*  on linking described in file LICENSE.                           */
+/*                                                                  */
+/********************************************************************/
+
 // This is the windows implementation of the VC server. Its main feature is
 // the use of an IO Completion port to handle all kinds of events.
 //
