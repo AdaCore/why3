404b0d187779bb80556ea9cc0e2be7955e8231b7
Yannick Moy <moy@adacore.com>
2016-05-06 16:56:44 +0200

    P405-006 New lemma in SPARK Coq file

--- lib/coq/SPARK.v
+++ lib/coq/SPARK.v
@@ -19,3 +19,14 @@ Lemma div_then_mult : forall x y z, 0 < y -> 0 <= z -> x <= z / y -> x * y <= z.
   replace (z / y * y) with (y * (z / y)) by auto with zarith.
   apply (Z.mul_div_le _ _ H).
 Qed.
+
+Lemma mul_ratio : forall x y z, 0 <= x -> 0 <= y -> 0 < z -> y <= z -> x * y / z <= x.
+  intros.
+  destruct (Z.eq_dec y 0) as [y_zero|y_not_zero].
+  * rewrite y_zero, Z.mul_0_r, Z.div_0_l by auto with zarith.
+    exact H.
+  * apply Int.Trans with (y := x * y / y).
+    apply Z.div_le_compat_l; auto with zarith.
+    rewrite Z.div_mul by auto.
+    auto with zarith.
+Qed.
