548a6eadced0f0f727bd0381bb8d4c39960bcd8e
Raphaël Rieu-Helft <raphael.rieu-helft@lri.fr>
2016-11-09 17:01:59 +0100

    Module Main now compiles

--- src/mlw/cprinter.ml
+++ src/mlw/cprinter.ml
@@ -41,7 +41,9 @@ module C = struct
     | Eindex of expr * expr (* Array access *)
     | Edot of expr * ident (* Field access with dot *)
     | Earrow of expr * ident (* Pointer access with arrow *)
-    | Esyntax of string * ty * (ty array) * (expr*ty) list
+    | Esyntax of string * ty * (ty array) * (expr*ty) list * bool
+  (* template, type and type arguments of result, typed arguments,
+     is/is not converter*)
 
   and constant =
     | Cint of string
@@ -75,8 +77,9 @@ module C = struct
 
   (* [assignify id] transforms a statement that computes a value into
      a statement that assigns that value to id *)
-  let rec assignify id = function
-    | Snop -> raise NotAValue
+  let rec assignify id s =
+    match s with
+    | Snop -> (*Format.printf "assign snop@."; Snop*) raise NotAValue (* ? *)
     | Sexpr e -> Sexpr (Ebinop (Bassign, Evar id, e))
     | Sblock (ds, s) -> Sblock (ds, assignify id s)
     | Sseq (s1, s2) -> Sseq (s1, assignify id s2)
@@ -101,8 +104,8 @@ module C = struct
                                 propagate_in_expr id v e2)
     | Edot (e,i) -> Edot (propagate_in_expr id v e, i)
     | Earrow (e,i) -> Earrow (propagate_in_expr id v e, i)
-    | Esyntax (s,t,ta,l) ->
-      Esyntax (s,t,ta,List.map (fun (e,t) -> (propagate_in_expr id v e),t) l)
+    | Esyntax (s,t,ta,l,b) ->
+      Esyntax (s,t,ta,List.map (fun (e,t) -> (propagate_in_expr id v e),t) l,b)
     | Enothing -> Enothing
     | Econst c -> Econst c
     | Esize_type ty -> Esize_type ty
@@ -154,6 +157,19 @@ module C = struct
 
   let is_empty_block s = s = Sblock([], Snop)
   let block_of_expr e = [], Sexpr e
+
+  let rec is_nop = function
+    | Snop -> true
+    | Sblock ([], s) -> is_nop s
+    | Sseq (s1,s2) -> is_nop s1 && is_nop s2
+    | _ -> false
+
+  let one_stmt = function
+    | Snop -> true
+    | Sexpr _ -> true
+    | Sblock _ -> true
+    | _ -> false
+
 end
 
 type info = Pdriver.printer_args = private {
@@ -164,6 +180,147 @@ type info = Pdriver.printer_args = private {
   syntax      : Printer.syntax_map;
   converter   : Printer.syntax_map;
 }
+
+module Print = struct
+
+  open C
+  open Format
+  open Printer
+  open Pp
+
+  exception Unprinted of string
+
+  let c_keywords = ["auto"; "break"; "case"; "char"; "const"; "continue"; "default"; "do"; "double"; "else"; "enum"; "extern"; "float"; "for"; "goto"; "if"; "int"; "long"; "register"; "return"; "short"; "signed"; "sizeof"; "static"; "struct"; "switch"; "typedef"; "union"; "unsigned"; "void"; "volatile"; "while" ]
+
+  let () = assert (List.length c_keywords = 32)
+
+  let sanitizer = sanitizer char_to_lalpha char_to_alnumus
+  let printer = create_ident_printer c_keywords ~sanitizer
+
+  let print_ident fmt id = fprintf fmt "%s" (id_unique printer id)
+
+  let protect_on x s = if x then "(" ^^ s ^^ ")" else s
+
+  let rec print_ty ?(paren=false) fmt = function
+    | Tvoid -> fprintf fmt "void"
+    | Tsyntax (s, tl) ->
+      syntax_arguments
+        (if paren then ("("^s^")") else s)
+        (print_ty ~paren:false) fmt tl
+    | Tptr ty -> fprintf fmt "(%a)*" (print_ty ~paren:true) ty
+    | Tarray (ty, expr) ->
+      fprintf fmt (protect_on paren "%a[%a]")
+        (print_ty ~paren:true) ty (print_expr ~paren:false) expr
+    | Tstruct _ -> raise (Unprinted "structs")
+    | Tunion _ -> raise (Unprinted "unions")
+    | Tnamed id -> print_ident fmt id
+    | Tnosyntax -> raise (Unprinted "type without syntax")
+
+  and print_unop fmt = function
+    | Unot -> fprintf fmt "!"
+    | Ustar -> fprintf fmt "*"
+    | Uaddr -> fprintf fmt "&"
+
+  and print_binop fmt = function
+    | Band -> fprintf fmt "&&"
+    | Bor -> fprintf fmt "||"
+    | Beq -> fprintf fmt "=="
+    | Bne -> fprintf fmt "!="
+    | Bassign -> fprintf fmt "="
+
+  and print_expr ~paren fmt = function
+    | Enothing -> Format.printf "enothing"; ()
+    | Eunop(u,e) -> fprintf fmt (protect_on paren "%a %a")
+      print_unop u (print_expr ~paren:true) e
+    | Ebinop(b,e1,e2) ->
+      fprintf fmt (protect_on paren "%a %a %a")
+        (print_expr ~paren:true) e1 print_binop b (print_expr ~paren:true) e2
+    | Ecast(ty, e) ->
+      fprintf fmt (protect_on paren "(%a)%a")
+        (print_ty ~paren:false) ty (print_expr ~paren:true) e
+    | Ecall (e,l) -> fprintf fmt (protect_on paren "%a(%a)")
+      (print_expr ~paren:true) e (print_list comma (print_expr ~paren:false)) l
+    | Econst c -> print_const fmt c
+    | Evar id -> print_ident fmt id
+    | Esize_expr e ->
+      fprintf fmt (protect_on paren "sizeof(%a)") (print_expr ~paren:false) e
+    | Esize_type ty ->
+      fprintf fmt (protect_on paren "sizeof(%a)") (print_ty ~paren:false) ty
+    | Eindex _ | Edot _ | Earrow _ -> raise (Unprinted "struct/array access")
+    | Esyntax (s, t, args, lte,_) ->
+      gen_syntax_arguments_typed snd (fun _ -> args)
+        (if paren then ("("^s^")") else s)
+        (fun fmt (e,_t) -> print_expr ~paren:false fmt e)
+        (print_ty ~paren:false) (C.Enothing,t) fmt lte
+
+  and print_const  fmt = function
+    | Cint s | Cfloat s | Cchar s | Cstring s -> fprintf fmt "%s" s
+
+  let print_id_init fmt = function
+    | id, Enothing -> print_ident fmt id
+    | id,e -> fprintf fmt "%a = %a" print_ident id (print_expr ~paren:false) e
+
+  let rec print_stmt ~braces fmt = function
+    | Snop -> Format.printf "snop"; ()
+    | Sexpr e -> fprintf fmt "%a;" (print_expr ~paren:false) e;
+    | Sblock ([] ,s) when (not braces || one_stmt s) -> (print_stmt ~braces:false) fmt s
+    | Sblock b -> fprintf fmt "@[<hov>{@\n  @[<hov>%a@]@\n}@]" print_body b
+    | Sseq (s1,s2) -> fprintf fmt "%a@\n%a"
+      (print_stmt ~braces:false) s1
+      (print_stmt ~braces:false) s2
+    | Sif(c,t,e) when is_empty_block e ->
+       fprintf fmt "if(%a)@\n%a" (print_expr ~paren:false) c
+         (print_stmt ~braces:true) t
+    | Sif (c,t,e) -> fprintf fmt "if(%a)@\n%a@\nelse@\n%a"
+      (print_expr ~paren:false) c
+      (print_stmt ~braces:true) t
+      (print_stmt ~braces:true) e
+    | Swhile (e,b) -> fprintf fmt "while (%a)@ %a"
+      (print_expr ~paren:false) e (print_stmt ~braces:true) b
+    | Sfor _ -> raise (Unprinted "for loops")
+    | Sbreak -> fprintf fmt "break;"
+    | Sreturn e -> fprintf fmt "return %a;" (print_expr ~paren:true) e
+
+  and print_def fmt = function
+    | Dfun (id,(rt,args),body) ->
+       fprintf fmt "%a %a(@[%a@])@ @[<hov>{@;<1 2>@[<hov>%a@]@\n}@\n@]"
+               (print_ty ~paren:false) rt
+               print_ident id
+               (print_list comma
+			   (print_pair_delim nothing space nothing
+					     (print_ty ~paren:false) print_ident))
+	       args
+               print_body body
+    | Dproto (id, (rt, args)) ->
+      fprintf fmt "%a %a(@[%a@]);@;"
+        (print_ty ~paren:false) rt
+        print_ident id
+        (print_list comma
+           (print_pair_delim nothing space nothing
+			     (print_ty ~paren:false) print_ident))
+	args
+    | Ddecl (ty, lie) ->
+       fprintf fmt "%a @[<hov>%a@];@;"
+	       (print_ty ~paren:false) ty
+	       (print_list comma print_id_init) lie
+    | Dinclude id ->
+       fprintf fmt "#include<%a.h>@;" print_ident id
+    | Dtypedef (ty,id) ->
+       fprintf fmt "@[<hov>typedef@ %a@;%a;@]@;"
+	       (print_ty ~paren:false) ty print_ident id
+
+  and print_body fmt body =
+    print_pair_delim nothing newline nothing
+		     (print_list newline print_def)
+		     (print_stmt ~braces:true)
+		     fmt body
+
+  let print_file fmt info ast =
+    Mid.iter (fun _ sl -> List.iter (fprintf fmt "%s\n") sl) info.thprelude;
+    newline fmt ();
+    fprintf fmt "@[<v>%a@]@." (print_list newline print_def) ast
+
+end
 
 module Translate = struct
   open Pdecl
@@ -214,17 +371,21 @@ module Translate = struct
               let params =
 		List.map (fun pv -> (C.Evar(pv_name pv), ty_of_ty info (ty_of_ity pv.pv_ity)))
 			 pvsl in
-              let rty = ty_of_ity rs.rs_cty.cty_result in
+              let rty = ty_of_ity e.e_ity in
               let rtyargs = match rty.ty_node with
 		| Tyvar _ -> [||]
 		| Tyapp (_,args) -> Array.of_list (List.map (ty_of_ty info) args)
               in
-              C.Esyntax(s,ty_of_ty info rty, rtyargs, params)
+              C.Esyntax(s,ty_of_ty info rty, rtyargs, params,
+                        Mid.mem rs.rs_name info.converter)
            | None ->
               let args = List.filter (fun pv -> not pv.pv_ghost) pvsl in
               C.(Ecall(Evar(rs.rs_name), List.map (fun pv -> Evar(pv_name pv)) args))
          in
-	 C.([], if is_return then Sreturn e else Sexpr e)
+	 C.([],
+            if is_return && not (ity_equal rs.rs_cty.cty_result ity_unit)
+            then Sreturn e
+            else Sexpr e)
       | _ -> raise (Unsupported "Cpur/Cany") (*TODO clarify*)
       end
     | Eassign _ -> raise (Unsupported "mutable field assign")
@@ -233,23 +394,36 @@ module Translate = struct
       | LDvar (pv,le) ->
         Format.printf "let %s@." pv.pv_vs.vs_name.id_string;
         if pv.pv_ghost then expr info ~is_return e
-        else if ((pv_name pv).id_string = "_" && ity_equal pv.pv_ity ity_unit)
+        else if ity_equal pv.pv_ity ity_unit
         then ([], C.Sseq (C.Sblock(expr info ~is_return:false le),
 			  C.Sblock(expr info ~is_return e)))
         else begin
           match le.e_node with
           | Econst (Number.ConstInt ic) ->
             let n = Number.compute_int ic in
-            let cexp = C.(Econst (Cint (BigInt.to_string n))) in
+            let ce = C.(Econst (Cint (BigInt.to_string n))) in
 	    Format.printf "propagate constant %s for var %s@."
 			  (BigInt.to_string n) (pv_name pv).id_string;
-            C.propagate_in_block (pv_name pv) cexp (expr info ~is_return e)
+            C.propagate_in_block (pv_name pv) ce (expr info ~is_return e)
+          | Eexec ({c_node = Capp(rs,_); _},_)
+              when Mid.mem rs.rs_name info.converter ->
+            begin match expr info ~is_return:false le with
+            | [], C.Sexpr(se) ->
+              C.propagate_in_block (pv_name pv) se (expr info ~is_return e)
+            | _ -> assert false
+            end
           | _->
             let t = ty_of_ty info (ty_of_ity pv.pv_ity) in
-            let d,s = expr info ~is_return:false le in
-	    let initblock = d, C.assignify (pv_name pv) s in
-            [ C.Ddecl (t, [pv_name pv, C.Enothing]) ],
-            C.Sseq (C.Sblock initblock, C.Sblock (expr info ~is_return e))
+            match expr info ~is_return:false le with
+            | [], C.Sexpr((C.Esyntax(_,_,_,_,b) as se))
+                when b (* converter *) ->
+              Format.printf "propagate converter for var %s@."
+                (pv_name pv).id_string;
+              C.propagate_in_block (pv_name pv) se (expr info ~is_return e)
+            | d,s ->
+              let initblock = d, C.assignify (pv_name pv) s in
+              [ C.Ddecl (t, [pv_name pv, C.Enothing]) ],
+              C.Sseq (C.Sblock initblock, C.Sblock (expr info ~is_return e))
         end
       | _ -> assert false
       end
@@ -321,10 +495,9 @@ module Translate = struct
 
     | _ -> [] (*TODO exn ? *)
 
-
   let munit info = function
     | Udecl pd -> pdecl info pd
-    | Uuse m -> [C.Dinclude m.mod_theory.Theory.th_name]
+    | Uuse _m -> (*[C.Dinclude m.mod_theory.Theory.th_name]*) []
     | Uclone _ -> raise (Unsupported "clone")
     | Umeta _ -> raise (Unsupported "meta")
     | Uscope _ -> []
@@ -339,135 +512,7 @@ module Translate = struct
 end
 (*TODO simplifications : propagate constants, collapse blocks with only a statement and no def*)
 
-module Print = struct
 
-  open C
-  open Format
-  open Printer
-  open Pp
-
-  exception Unprinted of string
-
-  let c_keywords = ["auto"; "break"; "case"; "char"; "const"; "continue"; "default"; "do"; "double"; "else"; "enum"; "extern"; "float"; "for"; "goto"; "if"; "int"; "long"; "register"; "return"; "short"; "signed"; "sizeof"; "static"; "struct"; "switch"; "typedef"; "union"; "unsigned"; "void"; "volatile"; "while" ]
-
-  let () = assert (List.length c_keywords = 32)
-
-  let sanitizer = sanitizer char_to_lalpha char_to_alnumus
-  let printer = create_ident_printer c_keywords ~sanitizer
-
-  let print_ident fmt id = fprintf fmt "%s" (id_unique printer id)
-
-  let protect_on x s = if x then "(" ^^ s ^^ ")" else s
-
-  let rec print_ty ?(paren=false) fmt = function
-    | Tvoid -> fprintf fmt "void"
-    | Tsyntax (s, tl) -> syntax_arguments s (print_ty ~paren:false) fmt tl
-    | Tptr ty -> fprintf fmt "(%a)*" (print_ty ~paren:true) ty
-    | Tarray (ty, expr) ->
-      fprintf fmt (protect_on paren "%a[%a]")
-        (print_ty ~paren:true) ty (print_expr ~paren:false) expr
-    | Tstruct _ -> raise (Unprinted "structs")
-    | Tunion _ -> raise (Unprinted "unions")
-    | Tnamed id -> print_ident fmt id
-    | Tnosyntax -> raise (Unprinted "type without syntax")
-
-  and print_unop fmt = function
-    | Unot -> fprintf fmt "!"
-    | Ustar -> fprintf fmt "*"
-    | Uaddr -> fprintf fmt "&"
-
-  and print_binop fmt = function
-    | Band -> fprintf fmt "&&"
-    | Bor -> fprintf fmt "||"
-    | Beq -> fprintf fmt "=="
-    | Bne -> fprintf fmt "!="
-    | Bassign -> fprintf fmt "="
-
-  and print_expr ?(paren=false) fmt = function
-    | Enothing -> Format.printf "enothing"; ()
-    | Eunop(u,e) -> fprintf fmt (protect_on paren "%a %a")
-      print_unop u (print_expr ~paren:true) e
-    | Ebinop(b,e1,e2) ->
-      fprintf fmt (protect_on paren "%a %a %a")
-        (print_expr ~paren:true) e1 print_binop b (print_expr ~paren:true) e2
-    | Ecast(ty, e) ->
-      fprintf fmt (protect_on paren "(%a)%a")
-        (print_ty ~paren:false) ty (print_expr ~paren:true) e
-    | Ecall (e,l) -> fprintf fmt (protect_on paren "%a(%a)")
-      (print_expr ~paren:true) e (print_list comma (print_expr ~paren:false)) l
-    | Econst c -> print_const fmt c
-    | Evar id -> print_ident fmt id
-    | Esize_expr e ->
-      fprintf fmt (protect_on paren "sizeof(%a)") (print_expr ~paren:false) e
-    | Esize_type ty ->
-      fprintf fmt (protect_on paren "sizeof(%a)") (print_ty ~paren:false) ty
-    | Eindex _ | Edot _ | Earrow _ -> raise (Unprinted "struct/array access")
-    | Esyntax (s, t, args, lte) ->
-      gen_syntax_arguments_typed snd (fun _ -> args)
-        (if paren then ("("^s^")") else s)
-        (fun fmt (e,_t) -> print_expr ~paren:false fmt e)
-        (print_ty ~paren:false) (C.Enothing,t) fmt lte
-
-  and print_const  fmt = function
-    | Cint s | Cfloat s | Cchar s | Cstring s -> fprintf fmt "%s" s
-
-  let print_id_init fmt = function
-    | id, Enothing -> print_ident fmt id
-    | id,e -> fprintf fmt "%a = %a" print_ident id (print_expr ~paren:false) e
-
-  let rec print_stmt fmt = function
-    | Snop -> Format.printf "snop"; ()
-    | Sexpr e -> fprintf fmt "%a;" (print_expr ~paren:false) e;
-    | Sblock b -> fprintf fmt "@[<hv>{@;<1 2>@[<hv>%a@]@\n}@]" print_body b
-    | Sseq (s1,s2) -> fprintf fmt "%a@\n%a" print_stmt s1 print_stmt s2
-    | Sif(c,t,e) when is_empty_block e ->
-       fprintf fmt "if(%a)@\n%a" (print_expr ~paren:false) c print_stmt t
-    | Sif (c,t,e) -> fprintf fmt "if(%a)@\n%a@\nelse@\n%a"
-      (print_expr ~paren:false) c print_stmt t print_stmt e
-    | Swhile (e,b) -> fprintf fmt "while (%a)@ %a"
-      (print_expr ~paren:false) e print_stmt b
-    | Sfor _ -> raise (Unprinted "for loops")
-    | Sbreak -> fprintf fmt "break;"
-    | Sreturn e -> fprintf fmt "return %a;" (print_expr ~paren:true) e
-
-  and print_def fmt = function
-    | Dfun (id,(rt,args),body) ->
-       fprintf fmt "%a %a(@[%a@])@ @[<hv>{@;<1 2>@[<hv>%a@]@;}@;@]"
-               (print_ty ~paren:false) rt
-               print_ident id
-               (print_list comma
-			   (print_pair_delim nothing space nothing
-					     (print_ty ~paren:false) print_ident))
-	       args
-               print_body body
-    | Dproto (id, (rt, args)) ->
-      fprintf fmt "%a %a(@[%a@]);@;"
-        (print_ty ~paren:false) rt
-        print_ident id
-        (print_list comma
-           (print_pair_delim nothing space nothing
-			     (print_ty ~paren:false) print_ident))
-	args
-    | Ddecl (ty, lie) ->
-       fprintf fmt "%a @[<hov>%a@];@;"
-	       (print_ty ~paren:false) ty
-	       (print_list comma print_id_init) lie
-    | Dinclude id ->
-       fprintf fmt "#include<%a>@;" print_ident id
-    | Dtypedef (ty,id) ->
-       fprintf fmt "@[<hov>typedef@ %a@;%a;@]@;"
-	       (print_ty ~paren:false) ty print_ident id
-
-  and print_body fmt body =
-    print_pair_delim nothing newline nothing
-		     (print_list newline print_def)
-		     print_stmt
-		     fmt body
-
-  let print_file fmt = fprintf fmt "@[<v>%a@]@." (print_list newline print_def)
-
-
-end
 
 let fg ?fname m =
   let n = m.Pmodule.mod_theory.Theory.th_name.Ident.id_string in
@@ -478,7 +523,7 @@ let fg ?fname m =
 let pr args ?old fmt m =
   ignore(old);
   let ast = Translate.translate args m in
-  try Print.print_file fmt ast
+  try Print.print_file fmt args ast
   with Print.Unprinted s -> (Format.printf "Could not print: %s@." s;
 		       Format.fprintf fmt "/* Dummy file */@.")
 
