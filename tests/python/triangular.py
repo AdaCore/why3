55c3f770c5a696a7cdb17f1169b5b71fff89dcab
Andrei Paskevich <andrei@lri.fr>
2017-03-06 16:19:32 +0100

    triangular.py: use a more natural formalisation

--- tests/python/triangular.py
+++ tests/python/triangular.py
@@ -8,9 +8,9 @@ s = 0
 k = 0
 while k <= n:
     #@ invariant 0 <= k <= n+1
-    #@ invariant 2 * s == (k - 1) * k
+    #@ invariant s == (k - 1) * k // 2
     s = s + k
     k = k + 1
 
 print(s)
-#@ assert 2 * s == n * (n+1)
+#@ assert s == n * (n+1) // 2
