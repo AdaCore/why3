4326d252351d890e71b23f0cda0bf57c83422512
Raphaël Rieu-Helft <raphael.rieu-helft@lri.fr>
2017-03-16 17:10:31 +0100

    Add gnuplot scripts

--- examples/in_progress/multiprecision/tests.c
+++ examples/in_progress/multiprecision/tests.c
@@ -10,8 +10,8 @@ uint64_t add(uint64_t * r3, uint64_t * x4, uint64_t * y3, int32_t sx, int32_t
 void mul(uint64_t * r10, uint64_t * x11, uint64_t * y10, int32_t sx2, int32_t
          sy2);
 
-void div_qr(uint64_t * q, uint64_t * r, uint64_t * x, uint64_t * y,
-            uint64_t * nx, uint64_t * ny,  int32_t sx, int32_t sy);
+void tdiv_qr(uint64_t * q, uint64_t * r, uint64_t * x, uint64_t * y,
+             int32_t sx, int32_t sy);
 
 #define TMP_ALLOC_LIMBS(n) malloc((n) * 8)
 
@@ -27,7 +27,20 @@ void mpn_dump(mp_ptr ap, mp_size_t an) {
 #define COMPARE
 #define TEST_GMP
 #define TEST_WHY3
+#define TEST_MUL
+#define TEST_DIV
 #endif
+
+
+void init_valid (mp_ptr ap, mp_ptr bp, mp_size_t an, mp_size_t bn) {
+  mpn_random2 (ap, an + 1);
+  mpn_random2 (bp, bn + 1);
+  while (bp[bn-1] == 0)
+    {
+      //printf("an = %d, bn = %d, aborted\n", (int)an, (int)bn);
+      mpn_random2 (bp, bn + 1);
+    };
+}
 
 int main () {
   mp_ptr ap, bp, rp, refp, rq, rr, refq, refr;
@@ -58,23 +71,22 @@ int main () {
   refq = TMP_ALLOC_LIMBS (max_n + 1);
   refr = TMP_ALLOC_LIMBS (max_n + 1);
 
+#ifdef TEST_MUL
+#ifdef BENCH
+  printf ("#an bn t(s)\n");
+#endif
   for (an = 2; an <= max_n; an += 1)
     {
-      for (bn = 1; bn <= an; bn += 1)
+      for (bn = 1; bn <= max_n; bn += 1)
 	{
-	  mpn_random2 (ap, an + 1);
-	  mpn_random2 (bp, bn + 1);
-
-          while (bp[bn-1] == 0)
-            {
-              //printf("an = %d, bn = %d, aborted\n", (int)an, (int)bn);
-              mpn_random2 (bp, bn + 1);
-            };
-
+	  init_valid (ap, bp, an, bn);
 #ifdef BENCH
-
-          gettimeofday(&begin, NULL);
-          for (int iter = 0; iter != 10000; ++iter) {
+          elapsed = 0;
+          for (int iter = 0; iter != 100; ++iter) {
+            init_valid (ap, bp, an, bn);
+            gettimeofday(&begin, NULL);
+            for (int i = 0; i != 100; ++i)
+              {
 #endif
 
 #ifdef TEST_GMP
@@ -85,12 +97,13 @@ int main () {
 #endif
 
 #ifdef BENCH
-          }
-          gettimeofday(&end, NULL);
-          elapsed =
+              }
+            gettimeofday(&end, NULL);
+            elapsed +=
             (end.tv_sec - begin.tv_sec)
             + ((end.tv_usec - begin.tv_usec)/1000000.0);
-          printf ("multiplication: an=%d, bn=%d, t=%f\n", (int)an, (int)bn, elapsed);
+          }
+          printf ("%d %d %f\n", an, bn, elapsed);
 #endif
 #ifdef COMPARE
 	  rn = an + bn;
@@ -107,73 +120,80 @@ int main () {
 #endif
         }
     }
+#ifdef COMPARE
+  printf ("multiplication ok\n");
+#endif
+#endif
+#ifdef TEST_DIV
+#ifdef BENCH
+  printf ("#an bn t(s)\n");
+#endif
   for (an = 2; an <= max_n; an += 1)
     {
       for (bn = 1; bn <= an; bn += 1)
-        {
-          mpn_random2 (ap, an + 1);
-	  mpn_random2 (bp, bn + 1);
+	{
+	  init_valid (ap, bp, an, bn);
 
-          while (bp[bn-1] == 0)
-            {
-              //printf("an = %d, bn = %d, aborted\n", (int)an, (int)bn);
-              mpn_random2 (bp, bn + 1);
-            };
 #ifdef BENCH
-          gettimeofday(&begin, NULL);
-          for (int iter = 0; iter != 100000; ++iter) {
+          elapsed = 0;
+          for (int iter = 0; iter != 100; ++iter) {
+            init_valid (ap, bp, an, bn);
+            gettimeofday(&begin, NULL);
+            for (int i = 0; i != 1000; ++i)
+              {
 #endif
 
 #ifdef TEST_GMP
-            mpn_tdiv_qr (refq, refr, 0, ap, an, bp, bn);
+                mpn_tdiv_qr (refq, refr, 0, ap, an, bp, bn);
 #endif
 #ifdef TEST_WHY3
-            tdiv_qr(rq, rr, ap, bp, an, bn);
+                tdiv_qr(rq, rr, ap, bp, an, bn);
 #endif
 
 #ifdef BENCH
-        }
-          gettimeofday(&end, NULL);
-          elapsed =
-            (end.tv_sec - begin.tv_sec)
-            + ((end.tv_usec - begin.tv_usec)/1000000.0);
-          printf ("division: an=%d, bn=%d, t=%f\n"
-//, am=%016lx\n
-
-                  , (int)an, (int)bn, elapsed
-                  // , ap[an]
-                  );
+              }
+            gettimeofday(&end, NULL);
+            elapsed +=
+              (end.tv_sec - begin.tv_sec)
+              + ((end.tv_usec - begin.tv_usec)/1000000.0);
+          }
+          printf ("%d %d %f\n", an, bn, elapsed);
+          if (an==bn)
+            printf ("\n"); //for gnuplot
 #endif
 #ifdef COMPARE
-          rn = bn;
+            rn = bn;
           if (mpn_cmp (refr, rr, rn))
 	    {
-	      printf ("ERROR, an = %d, bn = %d, rn = %d\n",
-		      (int) an, (int) bn, (int) rn);
-	      printf ("a: "); mpn_dump (ap, an);
-	      printf ("b: "); mpn_dump (bp, bn);
-	      printf ("q:    "); mpn_dump (rq, an-bn+2);
-	      printf ("refq: "); mpn_dump (refq, an-bn+2);
-	      printf ("r:    "); mpn_dump (rr, rn);
-	      printf ("refr: "); mpn_dump (refr, rn);
-	      abort();
-	    }
+          printf ("ERROR, an = %d, bn = %d, rn = %d\n",
+            (int) an, (int) bn, (int) rn);
+          printf ("a: "); mpn_dump (ap, an);
+          printf ("b: "); mpn_dump (bp, bn);
+          printf ("q:    "); mpn_dump (rq, an-bn+2);
+          printf ("refq: "); mpn_dump (refq, an-bn+2);
+          printf ("r:    "); mpn_dump (rr, rn);
+          printf ("refr: "); mpn_dump (refr, rn);
+          abort();
+        }
           rn = an - bn + 1;
           if (mpn_cmp (refq, rq, rn))
 	    {
-	      printf ("ERROR, an = %d, bn = %d, rn = %d\n",
-		      (int) an, (int) bn, (int) rn);
-	      printf ("a: "); mpn_dump (ap, an);
-	      printf ("b: "); mpn_dump (bp, bn);
-	      printf ("r:   "); mpn_dump (rq, rn);
-	      printf ("ref: "); mpn_dump (refq, rn);
-	      abort();
-	    }
+          printf ("ERROR, an = %d, bn = %d, rn = %d\n",
+            (int) an, (int) bn, (int) rn);
+          printf ("a: "); mpn_dump (ap, an);
+          printf ("b: "); mpn_dump (bp, bn);
+          printf ("r:   "); mpn_dump (rq, rn);
+          printf ("ref: "); mpn_dump (refq, rn);
+          abort();
+        }
 #endif
 	}
-    }
-
-  //TMP_FREE;
-  //tests_end ();
-  return 0;
-}
+        }
+#endif
+#ifdef COMPARE
+  printf ("division ok\n");
+#endif
+          //TMP_FREE;
+          //tests_end ();
+          return 0;
+        }
