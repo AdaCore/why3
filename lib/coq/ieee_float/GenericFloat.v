64034845ead1a8d4f7c5708ef740cf9a24310330
Johannes Kanig <kanig@adacore.com>
2017-06-26 09:11:11 +0900

    Q623-010 fix Coq compilation of why3 lib
    Change-Id: I36180086441b1ecc82292ab09097e76afdda3a71

--- lib/coq/ieee_float/GenericFloat.v
+++ lib/coq/ieee_float/GenericFloat.v
@@ -32,10 +32,10 @@ Require Import ZArith.
 Require Import Fourier.
 Require Import real.Truncate.
 
-Arguments B754_zero {prec} {emax}.
-Arguments B754_infinity {prec} {emax}.
-Arguments B754_nan {prec} {emax}.
-Arguments B754_finite {prec} {emax}.
+Arguments B754_zero {prec} {emax} _.
+Arguments B754_infinity {prec} {emax} _.
+Arguments B754_nan {prec} {emax} _ _.
+Arguments B754_finite {prec} {emax} _ _ _ _.
 
 (* Why3 assumption *)
 Inductive mode :=
