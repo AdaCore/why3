a465e9b2d60eb7cb04f357c57b36b7f18f021950
Claude Marche <Claude.Marche@inria.fr>
2017-06-27 07:58:40 +0200

    restored why3session html

--- src/why3session/why3session_main.ml
+++ src/why3session/why3session_main.ml
@@ -16,9 +16,9 @@ open Why3session_lib
 let cmds =
   [|
     Why3session_info.cmd;
+    Why3session_html.cmd;
 (*
     Why3session_latex.cmd;
-    Why3session_html.cmd;
     Why3session_csv.cmd;
     Why3session_copy.cmd_mod;
     Why3session_copy.cmd_copy;
