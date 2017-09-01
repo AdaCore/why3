8a7eccf6a7764de5e5de9182114ce5c282002a32
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2017-09-01 18:04:02 +0200

    Port knuth_prime_numbers.

--- examples/knuth_prime_numbers/knuth_prime_numbers_WP_PrimeNumbers_exists_prime_1.v
+++ examples/knuth_prime_numbers/knuth_prime_numbers_WP_PrimeNumbers_exists_prime_1.v
@@ -35,49 +35,22 @@ Definition contents {a:Type} {a_WT:WhyType a} (v:(ref a)): a :=
   | (mk_ref x) => x
   end.
 
-Axiom array : forall (a:Type), Type.
-Parameter array_WhyType : forall (a:Type) {a_WT:WhyType a},
-  WhyType (array a).
-Existing Instance array_WhyType.
-
-Parameter elts: forall {a:Type} {a_WT:WhyType a}, (array a) -> (Z -> a).
-
-Parameter length: forall {a:Type} {a_WT:WhyType a}, (array a) -> Z.
-
-Axiom array'invariant : forall {a:Type} {a_WT:WhyType a}, forall (self:(array
-  a)), (0%Z <= (length self))%Z.
-
-(* Why3 assumption *)
-Definition mixfix_lbrb {a:Type} {a_WT:WhyType a} (a1:(array a)) (i:Z): a :=
-  ((elts a1) i).
-
-Parameter mixfix_lblsmnrb: forall {a:Type} {a_WT:WhyType a}, (array a) ->
-  Z -> a -> (array a).
-
-Axiom mixfix_lblsmnrb_spec : forall {a:Type} {a_WT:WhyType a},
-  forall (a1:(array a)) (i:Z) (v:a), ((length (mixfix_lblsmnrb a1 i
-  v)) = (length a1)) /\ ((elts (mixfix_lblsmnrb a1 i
-  v)) = (map.Map.set (elts a1) i v)).
-
 (* Why3 assumption *)
 Definition no_prime_in (l:Z) (u:Z): Prop := forall (x:Z), ((l < x)%Z /\
   (x < u)%Z) -> ~ (number.Prime.prime x).
 
 (* Why3 assumption *)
-Definition first_primes (p:(array Z)) (u:Z): Prop := ((mixfix_lbrb p
-  0%Z) = 2%Z) /\ ((forall (i:Z) (j:Z), ((0%Z <= i)%Z /\ ((i < j)%Z /\
-  (j < u)%Z)) -> ((mixfix_lbrb p i) < (mixfix_lbrb p j))%Z) /\
-  ((forall (i:Z), ((0%Z <= i)%Z /\ (i < u)%Z) -> (number.Prime.prime
-  (mixfix_lbrb p i))) /\ forall (i:Z), ((0%Z <= i)%Z /\
-  (i < (u - 1%Z)%Z)%Z) -> (no_prime_in (mixfix_lbrb p i) (mixfix_lbrb p
-  (i + 1%Z)%Z)))).
+Definition first_primes (p:(Z -> Z)) (u:Z): Prop := ((p 0%Z) = 2%Z) /\
+  ((forall (i:Z) (j:Z), ((0%Z <= i)%Z /\ ((i < j)%Z /\ (j < u)%Z)) -> ((p
+  i) < (p j))%Z) /\ ((forall (i:Z), ((0%Z <= i)%Z /\ (i < u)%Z) ->
+  (number.Prime.prime (p i))) /\ forall (i:Z), ((0%Z <= i)%Z /\
+  (i < (u - 1%Z)%Z)%Z) -> (no_prime_in (p i) (p (i + 1%Z)%Z)))).
 
 (* Why3 goal *)
-Theorem exists_prime : forall (p:(array Z)) (u:Z), (1%Z <= u)%Z ->
-  ((first_primes p u) -> forall (d:Z), ((2%Z <= d)%Z /\ (d <= (mixfix_lbrb p
+Theorem exists_prime : forall (p:(Z -> Z)) (u:Z), (1%Z <= u)%Z ->
+  ((first_primes p u) -> forall (d:Z), ((2%Z <= d)%Z /\ (d <= (p
   (u - 1%Z)%Z))%Z) -> ((number.Prime.prime d) -> exists i:Z, ((0%Z <= i)%Z /\
-  (i < u)%Z) /\ (d = (mixfix_lbrb p i)))).
-(* Why3 intros p u h1 h2 d (h3,h4) h5. *)
+  (i < u)%Z) /\ (d = (p i)))).
 Proof.
 intros p u hu. generalize hu.
 pattern u; apply natlike_ind; intros. 3: omega.
@@ -91,7 +64,7 @@ simpl in H2.
 assert (d = 2)%Z by omega.
 subst; omega.
 ring_simplify (Zsucc x - 1)%Z in H2.
-assert (case: (d <= elts p (x-1) \/ elts p (x-1) < d)%Z) by omega. destruct case.
+assert (case: (d <= p (x-1) \/ p (x-1) < d)%Z) by omega. destruct case.
 destruct H0 with (d := d) as (i, (hi1, hi2)); intuition.
 destruct H1 as (p0, (sorted, (only_primes, all_primes))).
 red; split.
@@ -102,14 +75,12 @@ split; intros.
 apply only_primes; omega.
 apply all_primes; omega.
 exists i; intuition.
-unfold mixfix_lbrb in H2.
-assert (case: (d = elts p x \/ d < elts p x)%Z) by omega. destruct case.
+assert (case: (d = p x \/ d < p x)%Z) by omega. destruct case.
 exists x; intuition.
 apply False_ind.
 destruct H1 as (_, (_, (_, h))).
 revert H3.
 apply (h (x-1)%Z); try omega.
-unfold mixfix_lbrb.
-ring_simplify (x-1+1)%Z; omega.
+ring_simplify (x-1+1)%Z; now split.
 Qed.
 
