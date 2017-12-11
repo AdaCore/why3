190492a89ad03e34ab100ff4d9822b5d840b1618
Sylvain Dailler <dailler@adacore.com>
2017-12-11 11:05:51 +0100

    Update configure and regtests / nightly-build so that realization are generated again.
    * Makefile.in
    Realizations for isabelle are generated even if Isabelle is not installed.
    The .thy file are checked only if isabelle is installed. This allows us
    to treat it as with coq and be able to detect changes in realizations
    with a script.
    Dont erase xml generated file during clean.
    
    * configure.in
    Changed the configure to always generate a default isabelle version (set
    to 2017) for generation of the realization driver (needed even when no
    isablle support is given).
    
    * examples/regtests.sh
    Adding option --check-realization and --only-realization which add a test
    the compares the output of the new realization compared to the one
    recorded. It complains if they are different forcing the user to update the
    realization files.
    
    * lib/isabelle/*
    Adding xml files corresponding to generated realization for Isabelle. This
    allows for the abovementionned script to work even if Isabelle is not
    installed (which is the case for most people editing theories).
    
    * examples/nightly-bench.sh
    Add the option for realization check
    
    * misc/ci-bench
    Calls the test for realization
    
    Conflicts:
    	examples/regtests.sh

--- examples/regtests.sh
+++ examples/regtests.sh
@@ -2,6 +2,10 @@
 # regression tests for why3
 
 REPLAYOPT=""
+# Test realization too
+CHECK_REALIZATION=""
+# Don't test the rest of examples, only the realizations
+ONLY_REALIZATION=""
 
 while test $# != 0; do
 case "$1" in
@@ -15,6 +19,13 @@ case "$1" in
         REPLAYOPT="$REPLAYOPT --prover $2"
         shift
         ;;
+  "--check-realization")
+        CHECK_REALIZATION="true"
+        ;;
+  "--only-realization")
+        ONLY_REALIZATION="true"
+        CHECK_REALIZATION="true"
+        ;;
   *)
         echo "$0: Unknown option '$1'"
         exit 2
@@ -24,7 +35,9 @@ done
 
 TMP=$PWD/why3regtests.out
 TMPERR=$PWD/why3regtests.err
+TMPREAL=/tmp
 
+# Current directory is /examples
 cd `dirname $0`
 
 # too early to do that
@@ -36,68 +49,111 @@ export total=0
 export sessions=""
 export shapes=""
 
+test_generated () {
+  # Current directory is /why3
+  mkdir -p $TMPREAL/lib
+  echo "Testing isabelle realization"
+  # First copy current realization in a tmp directory
+  cp -r ../lib/isabelle/ $TMPREAL/lib/
+  # We want to use the makefile to be sure to check exhaustively the
+  # realizations that are built
+  make -C .. GENERATED_PREFIX_ISABELLE="$TMPREAL/lib/isabelle" update-isabelle > /dev/null 2> /dev/null
+  TMPDIFF=`diff -r -q -x '*.bak' ../lib/isabelle $TMPREAL/lib/isabelle`
+  if test "$TMPDIFF" = "" ; then
+    printf "ISABELLE realization OK\n"
+  else
+    printf "ISABELLE REALIZATION FAILED, please regenerate and prove it\n"
+    printf "$TMPDIFF\n"
+    printf "Generated realization are in /tmp. Use --only-realization to only test realization\n"
+    res=1
+  fi
+
+  echo "Testing coq realization"
+  # First copy current realization in a tmp directory
+  cp -r ../lib/coq/ $TMPREAL/lib/
+  # We want to use the makefile to be sure to check exhaustively the
+  # realizations that are built
+  make -C .. GENERATED_PREFIX_COQ="$TMPREAL/lib/coq" update-coq > /dev/null 2> /dev/null
+  TMPDIFF=`diff -r -q -x '*.bak' ../lib/coq $TMPREAL/lib/coq`
+  if test "$TMPDIFF" = "" ; then
+    printf "COQ realization OK\n"
+  else
+    printf "COQ REALIZATION FAILED, please regenerate and prove it\n"
+    printf "$TMPDIFF\n"
+    printf "Generated realization are in /tmp. Use --only-realization to only test realization\n"
+    res=1
+  fi
+}
+
 run_dir () {
     for f in `ls $1/*/why3session.xml`; do
         d=`dirname $f`
-	echo -n "Replaying $d ... "
+        printf "Replaying $d ... "
         ../bin/why3replay.opt -q $REPLAYOPT $2 $d 2> $TMPERR > $TMP
         ret=$?
-	if test "$ret" != "0"  ; then
-	    echo -n "FAILED (ret code=$ret):"
+        if test "$ret" != "0"  ; then
+            printf "FAILED (ret code=$ret):"
             out=`head -1 $TMP`
             if test -z "$out" ; then
                echo "standard error: (standard output empty)"
                cat $TMPERR
             else
-	       cat $TMP
+               cat $TMP
             fi
-	    res=1
-	else
-	    echo -n "OK"
-	    cat $TMP $TMPERR
+            res=1
+        else
+            printf "OK"
+            cat $TMP $TMPERR
             success=`expr $success + 1`
-	fi
+        fi
         total=`expr $total + 1`
     done
     sessions="$sessions $1/*/why3session.xml"
     shapes="$shapes $1/*/why3shapes.*"
 }
 
-echo "=== Standard Library ==="
-run_dir stdlib
-echo ""
+if test "$ONLY_REALIZATION" = "" ; then
+    echo "=== Standard Library ==="
+    run_dir stdlib
+    echo ""
 
-echo "=== Tests ==="
-# there's no session there...
-# run_dir tests
-run_dir tests-provers
-echo ""
+    echo "=== Tests ==="
+    # there's no session there...
+    # run_dir tests
+    run_dir tests-provers
+    echo ""
 
-echo "=== Check Builtin translation ==="
-run_dir check-builtin
-echo ""
+    echo "=== Check Builtin translation ==="
+    run_dir check-builtin
+    echo ""
 
-echo "=== BTS ==="
-run_dir bts
-echo ""
+    echo "=== BTS ==="
+    run_dir bts
+    echo ""
 
-echo "=== Logic ==="
-run_dir logic
-run_dir bitvectors "-L bitvectors"
-echo ""
+    echo "=== Logic ==="
+    run_dir logic
+    run_dir bitvectors "-L bitvectors"
+    echo ""
 
-echo "=== Programs ==="
-run_dir .
-run_dir foveoos11-cm
-run_dir WP_revisited
-run_dir vacid_0_binary_heaps "-L vacid_0_binary_heaps"
-run_dir avl "-L avl"
-run_dir double_wp "-L double_wp"
-run_dir prover "-L prover"
-echo ""
+    echo "=== Programs ==="
+    run_dir .
+    run_dir foveoos11-cm
+    run_dir WP_revisited
+    run_dir vacid_0_binary_heaps "-L vacid_0_binary_heaps"
+    run_dir avl "-L avl"
+    run_dir double_wp "-L double_wp"
+    run_dir prover "-L prover"
+    echo ""
 
-echo "Summary       : $success/$total"
-echo "Sessions size : "`wc -cl $sessions | tail -1`
-echo "Shapes   size : "`wc -cl $shapes | tail -1`
+    echo "Summary       : $success/$total"
+    echo "Sessions size : "`wc -cl $sessions | tail -1`
+    echo "Shapes   size : "`wc -cl $shapes | tail -1`
+fi
+
+if test "$CHECK_REALIZATION" = "true" ; then
+    test_generated
+fi
+
 
 exit $res
