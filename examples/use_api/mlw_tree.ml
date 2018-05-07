c542897d7fb1f745365f334b165192344377aee5
Claude Marche <Claude.Marche@inria.fr>
2018-05-07 16:58:12 +0200

    doc api tutorial: WhyML programs

--- examples/use_api/mlw_tree.ml
+++ examples/use_api/mlw_tree.ml
@@ -16,36 +16,26 @@ API calls
 
 ******************)
 
-(* opening the Why3 library *)
+(* BEGIN{buildenv} *)
 open Why3
-
-(* reads the config file *)
 let config : Whyconf.config = Whyconf.read_config None
-(* the [main] section of the config file *)
 let main : Whyconf.main = Whyconf.get_main config
-(* all the provers detected, from the config file *)
-let provers : Whyconf.config_prover Whyconf.Mprover.t =
-  Whyconf.get_provers config
-
-
-(* builds the environment from the [loadpath] *)
 let env : Env.env = Env.create_env (Whyconf.loadpath main)
-
-let () = Typing.open_file env [] (* dummy pathname *)
-
 open Ptree
-
-let mk_ident ?(label=[]) ?(loc=Loc.dummy_position) s = {
-  id_str = s; id_lab=label; id_loc = loc
-}
+(* END{buildenv} *)
 
 (* start a module *)
-
+(* BEGIN{openmodule} *)
+let () = Typing.open_file env [] (* empty pathname *)
+let mk_ident s = { id_str = s; id_lab=[]; id_loc = Loc.dummy_position }
 let m = Typing.open_module (mk_ident "Program")
+(* END{openmodule} *)
 
 
 (* use int.Int *)
 
+
+(* BEGIN{useimport} *)
 let mk_qid l =
   let rec aux l =
     match l with
@@ -55,30 +45,28 @@ let mk_qid l =
   in
   aux (List.rev l)
 
-let eq_symb = mk_qid [Ident.infix "="]
-
 let use_import (f, m) =
+  let m = mk_ident m in
   let loc = Loc.dummy_position in
-  let m = mk_ident ~loc m in
   Typing.open_scope loc m;
-  Typing.add_decl loc (Ptree.Duse (Qdot (Qident (mk_ident ~loc f), m)));
+  Typing.add_decl loc (Ptree.Duse (Qdot (Qident (mk_ident f), m)));
   Typing.close_scope loc ~import:true
 
 let use_int_Int = use_import ("int","Int")
+(* END{useimport} *)
 
-let int_type = mk_qid ["int"]
-
-let mul_int = mk_qid ["Int";Ident.infix "*"]
-
+(* BEGIN{helper1} *)
 let mk_expr e = { expr_desc = e; expr_loc = Loc.dummy_position }
 
 let mk_term t = { term_desc = t; term_loc = Loc.dummy_position }
 
 let mk_pat p = { pat_desc = p; pat_loc = Loc.dummy_position }
+let pat_var id = mk_pat (Pvar (id,false))
 
-let no_params ~loc = [loc, None, false, Some (PTtuple [])]
+let mk_var id = mk_term (Tident (Qident id))
 
-let param ~loc id ty = [loc, Some id, false, Some ty]
+let param0 = [Loc.dummy_position, None, false, Some (PTtuple [])]
+let param1 id ty = [Loc.dummy_position, Some id, false, Some ty]
 
 let mk_tconst s =
   mk_term
@@ -90,19 +78,36 @@ let mk_econst s =
     (Econst
        Number.(ConstInt { ic_negative = false ; ic_abs = int_literal_dec s }))
 
+let mk_tapp f l = mk_term (Tidapp(f,l))
 
+let mk_eapp f l = mk_expr (Eidapp(f,l))
+
+let mk_evar x = mk_expr(Eident(Qident x))
+(* END{helper1} *)
 
 (* declaration of
-     let f (_dummy:unit) : unit
-        requires { true }
-        ensures { true }
-      =
-        assert { 6*7 = 42 }
+  BEGIN{source1}
+     let f1 (x:int) : unit
+        requires { x=6 }
+        ensures { result=42 }
+      = x*7
+  END{source1}
  *)
-let d : decl =
+
+(* BEGIN{code1} *)
+let eq_symb = mk_qid [Ident.infix "="]
+let int_type_id = mk_qid ["int"]
+let int_type = PTtyapp(int_type_id,[])
+let mul_int = mk_qid ["Int";Ident.infix "*"]
+
+let d1 : decl =
+  let id_x = mk_ident "x" in
+  let pre = mk_tapp eq_symb [mk_var id_x; mk_tconst "6"] in
+  let result = mk_ident "result" in
+  let post = mk_tapp eq_symb [mk_var result; mk_tconst "42"] in
   let spec = {
-    sp_pre = [];
-    sp_post = [];
+    sp_pre = [pre];
+    sp_post = [Loc.dummy_position,[pat_var result,post]];
     sp_xpost = [];
     sp_reads = [];
     sp_writes = [];
@@ -112,44 +117,34 @@ let d : decl =
     sp_diverge = false;
   }
   in
-  let body =
-    let c6 = mk_tconst "6" in
-    let c7 = mk_tconst "7" in
-    let c42 = mk_tconst "42" in
-    let c6p7 = mk_term (Tidapp(mul_int,[c6;c7])) in
-    let p = mk_term (Tidapp(eq_symb,[c6p7;c42])) in
-    mk_expr(Eassert(Expr.Assert,p))
-  in
-  let f = Efun(no_params Loc.dummy_position,None,Ity.MaskVisible,spec,body)
-  in
-  Dlet(mk_ident "f",false,Expr.RKnone, mk_expr f)
+  let body = mk_eapp mul_int [mk_evar id_x; mk_econst "7"] in
+  let f1 = Efun(param1 id_x int_type, None, Ity.MaskVisible, spec, body) in
+  Dlet(mk_ident "f1",false,Expr.RKnone, mk_expr f1)
 
 let () =
-  try Typing.add_decl Loc.dummy_position d
+  try Typing.add_decl Loc.dummy_position d1
   with e ->
     Format.printf "Exception raised during typing of d:@ %a@."
       Exn_printer.exn_printer e
+(* END{code1} *)
 
 
 (*
 
 declaration of
-     let f (_dummy:unit) : unit
+BEGIN{source2}
+     let f2 () : int
         requires { true }
         ensures { result >= 0 }
-      =
-        let x = ref 42 in
-        !x
+      = let x = ref 42 in !x
+END{source2}
 
 *)
 
+(* BEGIN{code2} *)
 let ge_int = mk_qid ["Int";Ident.infix ">="]
 
 let use_ref_Ref = use_import ("ref","Ref")
-
-let mk_var id = mk_term (Tident (Qident id))
-
-let pat_var id = mk_pat (Pvar (id,false))
 
 let d2 =
   let result = mk_ident "result" in
@@ -167,13 +162,12 @@ let d2 =
   }
   in
   let body =
-    let e1 = mk_expr(Eidapp(mk_qid ["Ref";"ref"],[mk_econst "42"])) in
+    let e1 = mk_eapp (mk_qid ["Ref";"ref"]) [mk_econst "42"] in
     let id_x = mk_ident "x" in
-    let e2 = mk_expr(Eidapp(mk_qid ["Ref";Ident.prefix "!"],
-                            [mk_expr(Eident(Qident id_x))])) in
+    let e2 = mk_eapp (mk_qid ["Ref";Ident.prefix "!"]) [mk_evar id_x] in
     mk_expr(Elet(id_x,false,Expr.RKlocal,e1,e2))
   in
-  let f = Efun(no_params Loc.dummy_position,None,Ity.MaskVisible,spec,body)
+  let f = Efun(param0,None,Ity.MaskVisible,spec,body)
   in
   Dlet(mk_ident "f2",false,Expr.RKnone, mk_expr f)
 
@@ -182,34 +176,40 @@ let () =
   with e ->
     Format.printf "Exception raised during typing of d2:@ %a@."
       Exn_printer.exn_printer e
+(* END{code2} *)
 
-(* let f (a:array int)
+(*
+BEGIN{source3}
+let f (a:array int) : unit
       requires { a.length >= 1 }
       ensures { a[0] = 42 }
     = a[0] <- 42
- *)
+END{source3}
+*)
 
-let array_module = use_import ("array","Array")
+(* BEGIN{code3} *)
+let () = use_import ("array","Array")
 
-let ty_array = PTtyapp(mk_qid ["Array";"array"],[PTtyapp(int_type,[])])
+let array_int_type = PTtyapp(mk_qid ["Array";"array"],[int_type])
 
 let length = mk_qid ["Array";"length"]
 
-let array_get = mk_qid ["Array";"mixfix []"]
+let array_get = mk_qid ["Array"; Ident.mixfix "[]"]
 
-let array_set = mk_qid ["Array";"mixfix []<-"]
+let array_set = mk_qid ["Array"; Ident.mixfix "[]<-"]
 
 let d3 =
   let id_a = mk_ident "a" in
-  let pre = mk_term(Tidapp(ge_int,[mk_term(Tidapp(length,[mk_var id_a]));
-                                   mk_tconst "1"])) in
-  let result = mk_ident "result" in
+  let pre =
+    mk_tapp ge_int [mk_tapp length [mk_var id_a]; mk_tconst "1"]
+  in
   let post =
-    mk_term(Tidapp(eq_symb,[mk_term(Tidapp(array_get,[mk_var id_a;mk_tconst "0"]));
-                            mk_tconst "42"])) in
+    mk_tapp eq_symb [mk_tapp array_get [mk_var id_a; mk_tconst "0"];
+                     mk_tconst "42"]
+  in
   let spec = {
     sp_pre = [pre];
-    sp_post = [Loc.dummy_position,[pat_var result,post]];
+    sp_post = [Loc.dummy_position,[mk_pat Pwild,post]];
     sp_xpost = [];
     sp_reads = [];
     sp_writes = [];
@@ -220,30 +220,28 @@ let d3 =
   }
   in
   let body =
-    mk_expr(Eidapp(array_set,
-                   [mk_expr(Eident(Qident id_a));
-                    mk_econst "0";
-                    mk_econst "42"]))
-    in
-  let f = Efun(param Loc.dummy_position id_a ty_array,
+    mk_eapp array_set [mk_evar id_a; mk_econst "0"; mk_econst "42"]
+  in
+  let f = Efun(param1 id_a array_int_type,
                None,Ity.MaskVisible,spec,body)
   in
-  Dlet(mk_ident "f3",false,Expr.RKnone, mk_expr f)
+  Dlet(mk_ident "f3", false, Expr.RKnone, mk_expr f)
 
 let () =
   try Typing.add_decl Loc.dummy_position d3
   with e ->
     Format.printf "Exception raised during typing of d3:@ %a@."
       Exn_printer.exn_printer e
+(* END{code3} *)
 
-
-
+(* BEGIN{closemodule} *)
 let () = Typing.close_module Loc.dummy_position
+let mods : Pmodule.pmodule Stdlib.Mstr.t = Typing.close_file ()
+(* END{closemodule} *)
 
-let mods = Typing.close_file ()
+(* Checking the VCs *)
 
-                             (* Checking the VCs *)
-
+(* BEGIN{checkingvcs} *)
 let my_tasks : Task.task list =
   let mods =
     Stdlib.Mstr.fold
@@ -251,9 +249,6 @@ let my_tasks : Task.task list =
        List.rev_append (Task.split_theory m.Pmodule.mod_theory None None) acc)
       mods []
   in List.rev mods
-
-
-
 
 open Format
 
@@ -264,6 +259,9 @@ let () =
       (fun i t -> printf "Task %d: %a@." i Pretty.print_task t; i+1)
       1 my_tasks
   in ()
+
+let provers : Whyconf.config_prover Whyconf.Mprover.t =
+  Whyconf.get_provers config
 
 let alt_ergo : Whyconf.config_prover =
   let fp = Whyconf.parse_filter_prover "Alt-Ergo" in
@@ -299,6 +297,7 @@ let () =
       )
       1 my_tasks
   in ()
+(* END{checkingvcs} *)
 
 (*
 Local Variables:
