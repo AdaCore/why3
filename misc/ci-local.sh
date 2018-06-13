a29e5bfeed923c4b6a4159bfe4d173ab96079dbe
Raphael Rieu-Helft <raphael.rieu-helft@lri.fr>
2018-06-13 16:31:21 +0200

    Restore CI IDE test
    This reverts commit a30d8be75189266c112a3b5f82605a28e742364d.

--- misc/ci-local.sh
+++ misc/ci-local.sh
@@ -27,6 +27,9 @@ do
             bin/why3config --detect-provers
             make bench
             ;;
+        ide)
+            WHY3CONFIG="" xvfb-run bin/why3 ide --batch "" examples/logic/einstein.why
+            ;;
         doc)
             make doc
             ;;
