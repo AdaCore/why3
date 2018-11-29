1864120b13c13f75939f0333700a86c7181c447a
Sylvain Dailler <dailler@adacore.com>
2018-11-29 14:39:51 +0100

    RB28-023 Update coq/isabelle realization for Power_pos
    Coq/Isabelle realizations for Power_pos.
    
    * lib/coq/int/Power.v
    (Power_pos): Trivial lemma that already exists in Z.
    
    * lib/isabelle/Why3_Int.thy
    (Power_pos): proof by simp
    
    * lib/isabelle/realizations.*
    Update xml checksum for the isabelle theories
    
    Change-Id: Ie54b613ced26f9f681f3544e87199326084fca6b

--- lib/coq/int/Power.v
+++ lib/coq/int/Power.v
@@ -118,6 +118,13 @@ intros x y (h1,h2).
 now apply Z.pow_nonneg.
 Qed.
 
+(* Why3 goal *)
+Lemma Power_pos :
+forall (x:Z) (y:Z), ((0%Z < x)%Z /\ (0%Z <= y)%Z) -> (0%Z < (power x y))%Z.
+intros x y (h1,h2).
+eapply Z.pow_pos_nonneg; eauto.
+Qed.
+
 Open Scope Z_scope.
 
 (* Why3 goal *)
