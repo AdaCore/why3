2a734263b50d62971e1471e74fe2c8cb391c035a
Sylvain Dailler <dailler@adacore.com>
2019-01-23 11:32:43 +0100

    Avoid use of "model" in smt files because it is a keyword in ce output.

--- src/printer/smtv2.ml
+++ src/printer/smtv2.ml
@@ -117,6 +117,9 @@ let ident_printer () =
      (* From Z3 *)
       "map"; "bv"; "default";
       "difference";
+
+     (* Counterexamples specific keywords *)
+      "model";
       ]
   in
   let san = sanitizer char_to_alpha char_to_alnumus in
