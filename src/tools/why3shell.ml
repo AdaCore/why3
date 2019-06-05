b28765a753e447be4d51e62faf4a74bc5f10d6fb
Sylvain Dailler <dailler@adacore.com>
2019-06-05 18:20:18 +0200

    First step of restoring why3shell
    - add additional help for shell again
    - add proof status inside nodes instead of printing not supported (TODO
    print it correctly)
    - print on stdout instead of why3shell.out

--- src/tools/why3shell.ml
+++ src/tools/why3shell.ml
@@ -27,6 +27,8 @@ module Protocol_shell = struct
   let debug_proto = Debug.register_flag "shell_proto"
       ~desc:"Print@ debugging@ messages@ about@ Why3Ide@ protocol@"
 
+  let (_: unit) = Debug.unset_flag debug_proto
+
   let print_request_debug r =
     Debug.dprintf debug_proto "[request]";
     Debug.dprintf debug_proto "%a" print_request r
@@ -111,13 +113,6 @@ let treat_message_notification fmt msg = match msg with
   | Query_Info (_id, s)                            -> fprintf fmt "%s@." s
   | Query_Error (_id, s)                           -> fprintf fmt "%s@." s
   | File_Saved s                                   -> fprintf fmt "%s@." s
-(*
-  | Help s                                         -> fprintf fmt "%s@. Additionally for shell:\n\
-goto n -> focuse on n\n\
-ng -> next node\n\
-g -> print the current task\n\
-p -> print the session@." s
- *)
   | Information s          -> fprintf fmt "%s@." s
   | Task_Monitor (_t, _s, _r) -> () (* TODO do we want to print something for this? *)
   | Parse_Or_Type_Error (_, _, s)  -> fprintf fmt "%s@." s
@@ -139,7 +134,7 @@ type node = {
   node_parent: node_ID;
   node_name: string;
   mutable node_task: string option;
-  node_proof: Controller_itp.proof_attempt_status option;
+  mutable node_proof: Controller_itp.proof_attempt_status option;
   node_trans_args: string list option;
   node_type: shell_node_type;
   mutable node_proved: bool;
@@ -179,93 +174,8 @@ let print_goal fmt n =
   | None -> fprintf fmt "No goal@."
   | Some s -> fprintf fmt "Goal is: \n %s@." s
 
-let convert_to_shell_type t =
-  match t with
-  | NRoot -> SRoot
-  | NFile -> SFile
-  | NTheory -> STheory
-  | NTransformation -> STransformation
-  | NGoal -> SGoal
-  | NProofAttempt -> SProofAttempt
-
-let return_proof_info (t: node_type) =
-  match t with
-  | NProofAttempt ->
-    Some Controller_itp.Scheduled
-  | _ -> None
-
-let add_new_node fmt (n: node_ID) (parent: node_ID) (t: node_type) (name: string) (detached: bool) =
-  if t = NRoot then () else
-  let new_node = {
-    node_ID = n;
-    node_parent = parent;
-    node_name = name;
-    node_task = None;
-    node_proof = return_proof_info t;
-    node_trans_args = None; (* TODO *)
-    node_type = convert_to_shell_type t;
-    node_proved = false;
-    children_nodes = [];
-    node_detached = detached
-  } in
-  try
-    let parent = Hnode.find nodes parent in
-    parent.children_nodes <- parent.children_nodes @ [n];
-    Hnode.add nodes n new_node;
-    max_ID := !max_ID + 1
-  with
-    Not_found -> fprintf fmt "Could not find node %d@." parent
-
-let change_node fmt (n: node_ID) (u: update_info) =
-  try
-    let node = Hnode.find nodes n in
-    (match u with
-    | Proved b ->
-        node.node_proved <- b
-    | Proof_status_change (_pas, _b, _rl) when node.node_type = SProofAttempt ->
-        fprintf fmt "Not yet supported@." (* TODO *)
-    | Proof_status_change _ ->
-        fprintf fmt "Not yet supported@." (* TODO *)
-    | Name_change _ -> fprintf fmt "Not yet supported@.") (* TODO *)
-  with
-    Not_found -> fprintf fmt "Could not find node %d@." n
-
-let is_proof_attempt node_type =
-  match node_type with
-  | NProofAttempt -> true
-  | _ -> false
-
-let treat_notification fmt n =
-  fprintf fmt "Treat notifications@.";
-  match n with
-  | Reset_whole_tree -> () (* something to do ? *)
-  | Node_change (id, info)        ->
-    change_node fmt id info
-  | New_node (id, pid, typ, name, detached) ->
-    add_new_node fmt id pid typ name detached
-  | Remove _id                    -> (* TODO *)
-    fprintf fmt "got a Remove notification not yet supported@."
-  | Initialized _g_info           ->
-    (* TODO *)
-    fprintf fmt "Initialized@."
-  | Saved                         -> (* TODO *)
-    fprintf fmt "got a Saved notification not yet supported@."
-  | Saving_needed _b                -> (* TODO *)
-    fprintf fmt "got a Saving_needed notification not yet supported@."
-  | Message (msg)                 -> treat_message_notification fmt msg
-  | Dead _s                       -> (* TODO *)
-    fprintf fmt "got a Dead notification not yet supported@."
-  | File_contents _ -> fprintf fmt "got a File_contents notification not yet supported@." (* TODO *)
-  | Source_and_ce _ -> fprintf fmt "got a Source_and_ce notification not yet supported@." (* TODO *)
-  | Next_Unproven_Node_Id _ -> fprintf fmt "got a Next_Unproven_Node_Id notification not yet supported@." (* TODO *)
-  | Task (id, s, _list_loc, _)       ->
-    (* coloring the source is useless in shell *)
-    try
-      let node = Hnode.find nodes id in
-      node.node_task <- Some s;
-      if id = !cur_id then print_goal fmt !cur_id
-    with
-      Not_found -> fprintf fmt "Could not find node %d@." id
+(* _____________________________________________________________________ *)
+(* -------------------- printing --------------------------------------- *)
 
 let get_result pa =
   match pa with
@@ -273,9 +183,6 @@ let get_result pa =
   | Some pr -> match pr with
     | Controller_itp.Done pr -> Some pr
     | _ -> None
-
-(* _____________________________________________________________________ *)
-(* -------------------- printing --------------------------------------- *)
 
 let print_proof_attempt fmt pa_id =
   let pa = Hnode.find nodes pa_id in
@@ -346,11 +253,109 @@ let print_session fmt =
   let l = root_node.children_nodes in
   fprintf fmt "root %a@." print_s l
 
+let convert_to_shell_type t =
+  match t with
+  | NRoot           -> SRoot
+  | NFile           -> SFile
+  | NTheory         -> STheory
+  | NTransformation -> STransformation
+  | NGoal           -> SGoal
+  | NProofAttempt   -> SProofAttempt
+
+let return_proof_info (t: node_type) =
+  match t with
+  | NProofAttempt ->
+    Some Controller_itp.Scheduled
+  | _ -> None
+
+let add_new_node fmt (n: node_ID) (parent: node_ID) (t: node_type) (name: string) (detached: bool) =
+  if t = NRoot then () else
+  let new_node = {
+    node_ID = n;
+    node_parent = parent;
+    node_name = name;
+    node_task = None;
+    node_proof = return_proof_info t;
+    node_trans_args = None; (* TODO *)
+    node_type = convert_to_shell_type t;
+    node_proved = false;
+    children_nodes = [];
+    node_detached = detached
+  } in
+  try
+    let parent = Hnode.find nodes parent in
+    parent.children_nodes <- parent.children_nodes @ [n];
+    Hnode.add nodes n new_node;
+    max_ID := !max_ID + 1
+  with
+    Not_found -> fprintf fmt "Could not find node %d@." parent
+
+let change_node fmt (n: node_ID) (u: update_info) =
+  try
+    let node = Hnode.find nodes n in
+    (match u with
+    | Proved b ->
+        node.node_proved <- b
+    | Proof_status_change (pas, b, _rl) when node.node_type = SProofAttempt ->
+        node.node_proof <- Some pas;
+        node.node_detached <- b (* TODO check and print this *)
+    | Proof_status_change _ ->
+        (* TODO Probably case that cannot occur. *)
+        ()
+    | Name_change _ -> fprintf fmt "Not yet supported@.") (* TODO *)
+  with
+    Not_found -> fprintf fmt "Could not find node %d@." n
+
+let is_proof_attempt node_type =
+  match node_type with
+  | NProofAttempt -> true
+  | _ -> false
+
+let treat_notification fmt n =
+  match n with
+  | Reset_whole_tree                        -> print_session fmt
+  | Node_change (id, info)                  ->
+    change_node fmt id info
+  | New_node (id, pid, typ, name, detached) ->
+    add_new_node fmt id pid typ name detached
+  | Remove _id                              -> (* TODO *)
+    fprintf fmt "got a Remove notification not yet supported@."
+  | Initialized _g_info                     ->
+    (* TODO *)
+    fprintf fmt "Initialized@."
+  | Saved                                   -> (* TODO *)
+    fprintf fmt "got a Saved notification not yet supported@."
+  | Saving_needed _b                        -> (* TODO *)
+    fprintf fmt "got a Saving_needed notification not yet supported@."
+  | Message (msg)                           -> treat_message_notification fmt msg
+  | Dead _s                                 -> (* TODO *)
+    fprintf fmt "got a Dead notification not yet supported@."
+  | File_contents (f, s)                    ->
+      fprintf fmt "File %s is:\n%s" f s (* TODO print this correctly *)
+  | Source_and_ce _                         ->
+      fprintf fmt "got a Source_and_ce notification not yet supported@." (* TODO *)
+  | Next_Unproven_Node_Id _                 ->
+      fprintf fmt "got a Next_Unproven_Node_Id notification not yet supported@." (* TODO *)
+  | Task (id, s, _list_loc, _)              ->
+    (* coloring the source is useless in shell *)
+    try
+      let node = Hnode.find nodes id in
+      node.node_task <- Some s;
+      if id = !cur_id then print_goal fmt !cur_id
+    with
+      Not_found -> fprintf fmt "Could not find node %d@." id
+
+let additional_help = "Additionally for shell:\n\
+                       goto n -> focuse on n\n\
+                       ng -> next node\n\
+                       g -> print the current task\n\
+                       p -> print the session\n"
+
 (******************)
 (*    actions     *)
 (******************)
 
-let interp _chout fmt cmd =
+let interp fmt cmd =
   (* TODO dont use Str. *)
   let l = Str.split (Str.regexp " ") cmd in
   begin
@@ -368,6 +373,9 @@ let interp _chout fmt cmd =
              let c = false (* TODO *) in
              send_request (Get_task(!cur_id,c,false))
           | "p" -> print_session fmt
+          | "help" ->
+              Format.fprintf fmt "%s@." additional_help;
+              send_request (Command_req (!cur_id, cmd))
           | _ -> send_request (Command_req (!cur_id, cmd))
         end
   end;
@@ -376,9 +384,8 @@ let interp _chout fmt cmd =
     print_goal fmt !cur_id
 
 let () =
-  printf "Welcome to Why3 shell. Type 'help' for help.@.";
-  let chout = open_out "why3shell.out" in
-  let fmt = formatter_of_out_channel chout in
+  Format.printf "Welcome to Why3 shell. Type 'help' for help.@.";
+  let fmt = Format.std_formatter in
   let dir =
     try
       Server_utils.get_session_dir ~allow_mkdir:true files
@@ -391,4 +398,4 @@ let () =
   Unix_scheduler.timeout ~ms:100
     (fun () -> List.iter
         (fun n -> treat_notification fmt n) (get_notified ()); true);
-  Unix_scheduler.main_loop (interp chout fmt)
+  Unix_scheduler.main_loop (interp fmt)
