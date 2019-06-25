a562e0beeb2693b7d6cdf80df35d4e545ece340b
Claude Marche <Claude.Marche@inria.fr>
2019-06-25 12:32:21 +0200

    Isabelle output: fix output of float literals

---
+++ examples/tests-provers/ieee_float/ieee_float_Literals_more_1.thy
@@ -0,0 +1,15 @@
+theory ieee_float_Literals_more_1
+imports Why3.Why3
+begin
+
+why3_open "ieee_float_Literals_more_1.xml"
+
+why3_vc more
+(*  have "(15 / 10) * (5 /10) = 75 /100" *)
+  sorry
+  
+
+
+why3_end
+
+end
