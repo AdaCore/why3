62b34e297bd5d340c232f816f296cdf74edffffa
Sylvain <dailler@adacore.com>
2019-07-08 15:37:35 +0200

    Avoid forcing --detect when an error occur in why3config
    Patched the ci/nightly build that used the absence of a
    why3.conf file to change the --detect-provers in --detect.
    Now, explicitly use --detect.
    
    fix #365

--- misc/ci-local.sh
+++ misc/ci-local.sh
@@ -11,12 +11,12 @@ while test $# -gt 0
 do
     case "$1" in
         bench)
-            bin/why3config --detect-provers
+            bin/why3config --detect
             make bench
             ;;
         ide)
             WHY3CONFIG="" xvfb-run bin/why3 ide --batch "" examples/logic/einstein.why
-            bin/why3config --detect-provers
+            bin/why3config --detect
             bench/ide-bench
             ;;
         doc)
@@ -25,11 +25,11 @@ do
             make apidoc
             ;;
         ce-bench)
-            bin/why3config --detect-provers
+            bin/why3config --detect
             bench/ce-bench
             ;;
         nightly-bench-reduced)
-            bin/why3config --detect-provers
+            bin/why3config --detect
             sed -i why3.conf -e "s/running_provers_max = [0-9]*/running_provers_max = 1/"
             cat misc/bench-few-provers-why3-conf >> why3.conf
             COQVER=$(bin/why3 --list-provers | sed -n -e 's/  Coq (\?\([0-9.]\+\).*/\1/p')
