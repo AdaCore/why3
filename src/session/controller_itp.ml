11f28a8a62eb6f38c314c84cd87d2d43ca5d9fb3
Sylvain <dailler@adacore.com>
2019-08-14 17:31:52 +0200

    Remove useless recomputation of environment and drivers during reload

--- src/session/controller_itp.ml
+++ src/session/controller_itp.ml
@@ -244,9 +244,6 @@ let print_session fmt c =
 
 let reload_files (c : controller) ~shape_version =
   let old_ses = c.controller_session in
-  c.controller_env <- Env.create_env (Env.get_loadpath c.controller_env);
-  Whyconf.Hprover.reset c.controller_provers;
-  load_drivers c;
   c.controller_session <- empty_session ~shape_version ~from:old_ses (get_dir old_ses);
   merge_files ~shape_version c.controller_env c.controller_session old_ses
 
