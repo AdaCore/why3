4bbf3a686246888dd0d1b939fd29753c543aeeae
Sylvain Dailler <dailler@adacore.com>
2019-08-21 11:07:50 +0200

    S327-015 Adding a simpl tactic for SPARK generated task
    This adds a new easy to use transformation that should simplify the use of
    manual proof by providing a tactic that is a composition of introductions,
    simplifications of trivial, trivial let bindings, and classic
    simplification of task.
    
    * src/transform/gnat_trivial.ml
    (eliminate_trivial_let): Transformation that simplifies all trivial lets
    of the form "let A = B in" with B being a single variable/identifier.
    (spark_simpl): Easy to use simplification transformation
    
    
    Change-Id: I1eb32a58090a6b78770f69a5bc2803c9ede00b0f

--- src/transform/gnat_trivial.ml
+++ src/transform/gnat_trivial.ml
@@ -31,3 +31,27 @@ let is_trivial_trans =
 let () = Args_wrapper.wrap_and_register ~desc:"Prove goals whose positive part is just [true]"
     "trivial_true"
     Args_wrapper.Ttrans_l is_trivial_trans
+
+let rec elim_t t = match t.t_node with
+  | Tlet (t1,tb) ->
+      let t1 = elim_t t1 in
+      let v, tr = t_open_bound tb in
+      begin match t1.t_node with
+        | Tvar _ | Tapp (_, []) -> elim_t (t_subst (Mvs.add v t1 Mvs.empty) tr)
+        | _ -> t_let_close v t1 (t_map elim_t tr)
+      end
+  | _ ->
+      t_map elim_t t
+
+let eliminate_trivial_let = Trans.rewrite elim_t None
+
+let spark_simpl =
+  Trans.seq_l [Trans.singleton Eliminate_epsilon.eliminate_epsilon;
+               Trans.singleton Introduction.simplify_intros;
+               Trans.singleton Inlining.trivial;
+               Trans.singleton eliminate_trivial_let;
+               Simplify_formula.simplify_formula_and_task]
+
+let () = Args_wrapper.wrap_and_register ~desc:"Simplification transformation for SPARK-generated task"
+    "spark_simpl"
+    Args_wrapper.Ttrans_l spark_simpl
