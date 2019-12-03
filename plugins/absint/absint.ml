4408d894243b3694f8b434da274c518b14e1712b
Cláudio Belo Lourenço <belolourenco@gmail.com>
2019-12-03 11:30:48 +0100

    Removing abstract interpretation plugin and some cosmetics in the makefile

--- plugins/absint/absint.ml
+++
@@ -1,9 +0,0 @@
-open Why3
-
-let read_channel env path file c =
-  fst (Env.read_file Pmodule.mlw_language env file)
-
-let () =
-  Env.register_format Pmodule.mlw_language "why3 AI" ["imlw"] read_channel
-    ~desc:"mlw with abstract interpretation"
-
