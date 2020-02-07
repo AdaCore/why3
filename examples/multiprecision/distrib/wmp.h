cae488c6723648acbbd420b94fa3a374201b0e26
Raphael Rieu-Helft <raphael.rieu-helft@lri.fr>
2020-02-07 13:45:15 +0100

    Update wmp.h

--- examples/multiprecision/distrib/wmp.h
+++ examples/multiprecision/distrib/wmp.h
@@ -8,16 +8,50 @@ typedef uint64_t wmp_limb_t;
 typedef wmp_limb_t *wmp_ptr;
 typedef wmp_limb_t const *wmp_srcptr;
 
+typedef struct
+{
+int _alloc;
+int _size;
+uint64_t * _ptr;
+} __mpz_struct;
+
+typedef __mpz_struct *mpz_ptr;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+mpz_ptr wmpz_init ();
+void wmpz_clear (mpz_ptr);
+wmp_ptr wmpz_realloc(mpz_ptr, wmp_size_t);
+
+void wmpz_set_ui(mpz_ptr, uint64_t);
+void wmpz_set_si(mpz_ptr, int64_t);
+uint64_t wmpz_get_ui(mpz_ptr);
+
+int32_t wmpz_cmp(mpz_ptr, mpz_ptr);
+int32_t wmpz_cmp_ui(mpz_ptr, uint64_t);
+
+void wmpz_add(mpz_ptr, mpz_ptr, mpz_ptr);
+void wmpz_add_ui(mpz_ptr, mpz_ptr, uint64_t);
+
+void wmpz_sub(mpz_ptr, mpz_ptr, mpz_ptr);
+void wmpz_sub_ui(mpz_ptr, mpz_ptr, uint64_t);
+void wmpz_ui_sub(mpz_ptr, uint64_t, mpz_ptr);
+
+void wmpz_mul(mpz_ptr, mpz_ptr, mpz_ptr);
+void wmpz_mul_si(mpz_ptr, mpz_ptr, int64_t);
+void wmpz_mul_ui(mpz_ptr, mpz_ptr, uint64_t);
+
+void wmpz_mul2exp(mpz_ptr, mpz_ptr, uint64_t);
+void wmpz_tdiv_q_2exp(mpz_ptr, mpz_ptr, uint64_t);
+
 int32_t wmpn_cmp (wmp_srcptr, wmp_srcptr, wmp_size_t);
 
 void wmpn_copyi (wmp_ptr, wmp_srcptr, wmp_size_t);
+void wmpn_copyd (wmp_ptr, wmp_srcptr, wmp_size_t);
 
 int32_t wmpn_zero_p (wmp_srcptr, wmp_size_t);
-
 void wmpn_zero (wmp_ptr, wmp_size_t);
 
 wmp_limb_t wmpn_add (wmp_ptr, wmp_srcptr, wmp_size_t, wmp_srcptr, wmp_size_t);
@@ -32,6 +66,7 @@ wmp_limb_t wmpn_rshift (wmp_ptr, wmp_srcptr, wmp_size_t, uint64_t);
 
 void wmpn_powm (wmp_ptr, wmp_srcptr, wmp_size_t, wmp_srcptr, wmp_size_t,
                 wmp_srcptr, wmp_size_t, wmp_ptr);
+
 
 // not verified when rp and up are aliased.
 wmp_limb_t wmpn_add_1 (wmp_ptr rp, wmp_srcptr up, wmp_size_t, wmp_limb_t);
