54ce0210c735736213e03d079d8d4690812090c1
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2020-02-20 07:40:10 +0100

    Move syntax highlighter into the why3 domain.

--- doc/ext/why3.py
+++ doc/ext/why3.py
@@ -1,3 +1,65 @@
+from pygments.lexer import RegexLexer, words
+from pygments.token import Text, Comment, Operator, Keyword, Name, String, Number, Punctuation, Error
+
+class WhyMLLexer(RegexLexer):
+    name = 'WhyML'
+    aliases = 'whyml'
+
+    keywords = (
+        'abstract', 'absurd', 'alias', 'any', 'assert', 'assume', 'as', 'at', 'axiom',
+        'break', 'by', 'check', 'clone', 'coinductive', 'constant', 'continue',
+        'diverges', 'do', 'done', 'downto',
+        'else', 'end', 'ensures', 'exception', 'exists', 'export',
+        'false', 'for', 'forall', 'fun', 'function', 'ghost', 'goal',
+        'if', 'import', 'in', 'inductive', 'invariant', 'label', 'lemma', 'let', 'loop',
+        'match', 'meta', 'module', 'mutable', 'not', 'old', 'predicate', 'private', 'pure',
+        'raise', 'raises', 'reads', 'rec', 'ref', 'requires', 'return', 'returns',
+        'scope', 'so', 'then', 'theory', 'to', 'true', 'try', 'type', 'use', 'val', 'variant',
+        'while', 'with', 'writes',
+    )
+
+    tokens = {
+        'root': [
+            (r'\s+', Text),
+            (r'\(\*\)', Operator),
+            (r'\(\*', Comment, 'comment'),
+            (r'\[@[^]]*\]', Comment),
+            (words(keywords, suffix=r'\b'), Keyword),
+            (r'[-~!%^&*+=|?<>/\\]', Operator),
+            (r'[][{};:.,()]', Punctuation),
+            (r"[^\W\d][\w']*", Name),
+            (r'\bresult\b', Name.Builtin.Pseudo),
+
+            (r'-?\d\d*(.\d*)?([eE][+-]?\d\d*)', Number.Float),
+            (r'-?0[xX][\da-fA-F][\da-fA-F]*(.[\da-fA-F]*)?([pP][+-]?\d\d*)', Number.Float),
+            (r'0[xX][\da-fA-F][\da-fA-F_]*', Number.Hex),
+            (r'0[oO][0-7][0-7_]*', Number.Oct),
+            (r'0[bB][01][01_]*', Number.Bin),
+            (r'\d[\d_]*', Number.Integer),
+
+            (r"'", Keyword),
+            (r'"', String.Double, 'string'),
+        ],
+        'comment': [
+            (r'[^(*)]+', Comment),
+            (r'\(\*', Comment, '#push'),
+            (r'\*\)', Comment, '#pop'),
+            (r'[(*)]', Comment),
+        ],
+        'string': [
+            (r'[^\\"]+', String.Double),
+            (r'\\[\\"\'ntbr]', String.Escape),
+            (r'\\[0-9]{3}', String.Escape),
+            (r'\\x[0-9a-fA-F]{2}', String.Escape),
+            (r'\\\n', String.Double),
+            (r'"', String.Double, '#pop'),
+        ],
+    }
+
+from sphinx.highlighting import lexers
+
+lexers['whyml'] = WhyMLLexer(startinline=True)
+
 import re
 from docutils import nodes
 from sphinx import addnodes
