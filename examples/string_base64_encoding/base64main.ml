a1107d63016741034384a172fba0802f8fe38665
Cláudio Belo Lourenço <belolourenco@gmail.com>
2020-02-20 14:01:21 +0100

    Base64 testing example

---
+++ examples/string_base64_encoding/base64main.ml
@@ -0,0 +1,26 @@
+open Base64
+
+let test t =
+  let d = decode (encode t) in
+  if d <> t then begin
+      Format.eprintf "Failed with %s - %s@." t d; assert false
+    end
+
+let tests = [
+  "";
+  "a";
+  "ab";
+  "abc";
+  "abcd";
+  "908ujdas0";
+  "(*^HDS)Hl;ady&*(ASD|[]\\a";
+  "(*^HDS)Hl;ady&*(ASD|[]\\a'";
+  "(*^HDS)Hl;ady&*(ASD|[]\\a'}";
+  " dsahn fadslhnoiy90hjnIOG*)YHpn 17uOJASG87oT!@*&%#!ujb c";
+  "<L:<A:<adsD:Hp8y2hjp;/\tmsdag8y64830u89(*^T*&(OGdd97s^Ydsa";
+  "<L:<A:\n<adsD:Hp8y2hjp;/\tmsdag8y64830u89(*^T*&(OGdd97s^Ydsa";
+  "<L:<A:<a\123dsD:Hp8y2hjp;/\tmsda\ng8y648\x1030u89(*^T*&(OGdd97s^Ydsa"
+]
+
+let () =
+  List.iter test tests
