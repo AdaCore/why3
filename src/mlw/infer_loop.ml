618a4ea4541ff6f23d8eb7210f474aa4609271f6
Cláudio Belo Lourenço <belolourenco@gmail.com>
2020-03-10 13:51:33 +0100

    Infer attribute

---
+++ src/mlw/infer_loop.ml
@@ -0,0 +1,44 @@
+(* If --enable-infer configure flag is set to true, the infer_loop.ml
+   corresponds to this file. *)
+
+open Ident
+open Theory
+open Ity
+open Expr
+
+let infer_flag =
+  Debug.register_flag "infer-loop" ~desc:"Infer loop invariants"
+
+let attrs_has_infer attrs =
+  Sattr.exists (fun a -> Strings.has_prefix "infer" a.attr_string) attrs
+
+let infer_loops env tkn mkn e cty =
+  let module AI = Ai_cfg.Make (struct
+    let env = env
+    let th_known = tkn
+    let mod_known = mkn
+    let widening = 3
+    module D = Domain.Polyhedra end) in
+  let preconditions = cty.cty_pre in
+  let cfg = AI.start_cfg () in
+  let context = AI.empty_context () in
+  List.iter (AI.add_variable cfg context) cty.cty_args;
+  if Debug.test_flag Uf_domain.infer_debug then
+    Format.printf "%a@." Expr.print_expr e;
+  ignore (AI.put_expr_with_pre cfg context e preconditions);
+  (* will hold the different file offsets (useful when writing
+     multiple invariants) *)
+  let fixp = AI.eval_fixpoints cfg context in
+  let domain2term (e,d) =
+    let expl = "expl:infer-loop" in
+    let t    = AI.domain_to_term cfg context d in
+    let t    = Term.t_attr_add (Ident.create_attribute expl) t in
+    if Debug.test_flag Uf_domain.infer_debug then
+      Pretty.print_term Format.std_formatter t;
+    (e,t) in
+  List.map domain2term fixp
+
+let infer_loops attrs env tkn mkn e cty =
+  if Debug.test_flag infer_flag || attrs_has_infer attrs  then
+    infer_loops env tkn mkn e cty
+  else []
