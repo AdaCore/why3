f544c2e6be238d2e83b50cfc677ffa4d96174556
Raphael Rieu-Helft <raphael.rieu-helft@lri.fr>
2020-03-12 16:18:07 +0100

    Extraction of mpz_get_str

--- src/extract/c.ml
+++ src/extract/c.ml
@@ -646,7 +646,9 @@ module Print = struct
         gen_syntax_arguments_prec fmt s pr pl
 
   and print_const  fmt = function
-    | Cint s | Cfloat s | Cchar s | Cstring s -> fprintf fmt "%s" s
+    | Cint s | Cfloat s -> fprintf fmt "%s" s
+    | Cchar s -> fprintf fmt "'%s'" s
+    | Cstring s -> fprintf fmt "\"%s\"" s
 
   let print_id_init ?(size=None) ~stars fmt ie =
     (if stars > 0
@@ -1086,8 +1088,10 @@ module MLToC = struct
        let id = pv_name pv in
        let e = C.Evar id in
        ([], expr_or_return env e)
-    | Mltree.Econst (Constant.ConstStr _ | Constant.ConstReal _) ->
-       raise (Unsupported "string or real constant")
+    | Mltree.Econst (Constant.ConstStr s) ->
+       C.([], expr_or_return env (Econst (Cstring s)))
+    | Mltree.Econst (Constant.ConstReal _) ->
+       raise (Unsupported "real constant")
     | Mltree.Econst (Constant.ConstInt ic) ->
        let open Number in
        let print fmt ic =
