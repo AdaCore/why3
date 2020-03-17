0070b9408d0809ef01f1e5bfa9dfff9742558261
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2020-03-17 09:45:13 +0100

    Update headers.

--- src/ide/statmemprof.ml
+++ src/ide/statmemprof.ml
@@ -1,1 +1,12 @@
+(********************************************************************)
+(*                                                                  *)
+(*  The Why3 Verification Platform   /   The Why3 Development Team  *)
+(*  Copyright 2010-2020   --   Inria - CNRS - Paris-Sud University  *)
+(*                                                                  *)
+(*  This software is distributed under the terms of the GNU Lesser  *)
+(*  General Public License version 2.1, with the special exception  *)
+(*  on linking described in file LICENSE.                           *)
+(*                                                                  *)
+(********************************************************************)
+
 let () = Statmemprof_emacs.start 1.e-4 10000 30
--- src/util/mlmpfr_dummy.ml
+++ src/util/mlmpfr_dummy.ml
@@ -1,3 +1,14 @@
+(********************************************************************)
+(*                                                                  *)
+(*  The Why3 Verification Platform   /   The Why3 Development Team  *)
+(*  Copyright 2010-2020   --   Inria - CNRS - Paris-Sud University  *)
+(*                                                                  *)
+(*  This software is distributed under the terms of the GNU Lesser  *)
+(*  General Public License version 2.1, with the special exception  *)
+(*  on linking described in file LICENSE.                           *)
+(*                                                                  *)
+(********************************************************************)
+
 (* Dummy implementation for mpfr that always raise Not_implemented *)
 
 exception Not_Implemented
