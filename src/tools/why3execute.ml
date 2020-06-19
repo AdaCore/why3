559f52dfd99f6a6a1c71267fe63eee8a8d06b654
Cláudio Belo Lourenço <belolourenco@gmail.com>
2020-06-19 14:24:40 +0200

    Why3execute: new argument allowing use declarations
    Why3 execute can now be invoked like this:
    $ why3 execute examples/tests/interp_mach.mlw \
      "let _ = TestArgs.c := 1 in TestArgs.t5 3" --use-modules="use ref.Ref"

--- src/tools/why3execute.ml
+++ src/tools/why3execute.ml
@@ -11,6 +11,7 @@
 
 open Format
 open Why3
+open Pmodule
 
 let usage_msg = sprintf
   "Usage: %s [options] <file> <module>.<ident>...\n\
@@ -37,6 +38,8 @@ let enable_rac = ref false
 
 let dispatch = ref []
 
+let use_modules = ref []
+
 let option_list =
   let open Getopt in
   [ Key ('F', "format"), Hnd1 (AString, fun s -> opt_parser := Some s),
@@ -51,6 +54,10 @@ let option_list =
       APair ('.', AString, AString)), fun arg -> dispatch := arg :: !dispatch),
     ("<f.M> <g.N> Dispatch access to module <f.M> to module <g.N> (useful to\n\
       provide an implementation for a module with abstract types or values)");
+    KLong "use-modules",
+    Hnd1 (AString, fun l -> use_modules := String.split_on_char ';' l),
+    "<use_declaration> use modules to type given expressions separated by ';'";
+
   ]
 
 let config, _, env =
@@ -61,33 +68,81 @@ let () =
 
 let prepare_dispatch env l =
   let aux ((p1, m1), (p2, m2)) =
-    Pmodule.read_module env [p1] m1,
-    Pmodule.read_module env [p2] m2 in
+    read_module env [p1] m1,
+    read_module env [p2] m2 in
   List.fold_right (fun (source, target) -> Pinterp.add_dispatch env ~source ~target)
     (List.map aux l) Pinterp.empty_dispatch
+
+(* TODO: Functions string_list_of_qualid, find_module and the add_use
+   inside do_input are replicated from typing.ml. Can be removed if
+   the function Typing.add_decl is exported. *)
+let string_list_of_qualid q =
+  let open Ptree in
+  let rec sloq acc = function
+    | Qdot (p, id) -> sloq (id.id_str :: acc) p
+    | Qident id -> id.id_str :: acc in
+  sloq [] q
+
+let find_module env q =
+  let open Ptree in
+  match q with
+    | Qident {id_str = nm} -> read_module env [] nm
+    | Qdot (p, {id_str = nm}) -> read_module env (string_list_of_qualid p) nm
 
 let do_input f =
   let format = !opt_parser in
   let mm = match f with
     | "-" ->
-        Env.read_channel Pmodule.mlw_language ?format env "stdin" stdin
+        Env.read_channel mlw_language ?format env "stdin" stdin
     | file ->
         let (mlw_files, _) =
-          Env.read_file Pmodule.mlw_language ?format env file in
+          Env.read_file mlw_language ?format env file in
         mlw_files
   in
-  let muc = Pmodule.create_module env (Ident.id_fresh "") in
+  let muc = create_module env (Ident.id_fresh "") in
+
+  (* add all modules declared in the file to the muc *)
   let add_module muc s m =
-    let muc = Pmodule.open_scope muc s in
-    let muc = Pmodule.use_export muc m in
-    Pmodule.close_scope muc false in
-  let muc = Why3.Wstdlib.Mstr.fold_left add_module muc mm in
-  let prog_parsed = Lexer.parse_expr (Lexing.from_string !opt_exec) in
+    let muc = open_scope muc s in
+    let muc = use_export muc m in
+    close_scope muc ~import:false in
+
+  (* parse use declarations provided in the command line and add them
+     to the muc *)
+  let muc = Wstdlib.Mstr.fold_left add_module muc mm in
+  let add_use muc d =
+    let open Ptree in
+    let lb = Lexing.from_string d in
+    Loc.set_file "command line argument --use-modules" lb;
+    let decl = Lexer.parse_mlw_file lb in
+    match decl with
+    | Decls [Duseexport use] -> use_export muc (find_module env use)
+    | Decls [Duseimport (_loc,import,uses)] ->
+       let qualid_last = function Qident x | Qdot (_, x) -> x in
+       let use_as q = function Some x -> x | None -> qualid_last q in
+       let add_import muc (m, q) =
+         let import = import || q = None in
+         let muc = open_scope muc (use_as m q).id_str in
+         let m = find_module env m in
+         let muc = use_export muc m in
+         close_scope muc ~import in
+       List.fold_left add_import muc uses
+    | _ -> eprintf "only use declarations supported in command line \
+                    option '--use-modules'@.";
+           exit 1 in
+  let muc = List.fold_left add_use muc !use_modules in
+
+  (* parse and type check command line expression *)
+  let lb = Lexing.from_string !opt_exec in
+  Loc.set_file "expression to execute" lb;
+  let prog_parsed = Lexer.parse_expr lb in
   let expr = Typing.type_expr_in_muc muc prog_parsed in
   Format.eprintf "%a@." Expr.print_expr expr;
-
   let known = muc.muc_known in
+
+  (* execute expression *)
   let open Pinterp in
+  Opt.iter init_real !prec;
   try
     let dispatch = prepare_dispatch env !dispatch in
     let res = eval_global_fundef ~rac:!enable_rac env dispatch known [] expr in
@@ -96,27 +151,6 @@ let do_input f =
   with Contr (ctx, term) ->
     printf "%a@." (report_cntr expr) (ctx, term) ;
     exit 1
-
-
-  (* let do_exec (mod_name, fun_name) =
-   *   try
-   *     let open Pinterp in
-   *     (\* eprintf "Find global symbol %s.%s...@." mod_name fun_name; *\)
-   *     let {Pmodule.mod_known= known}, rs = find_global_symbol mm ~mod_name ~fun_name in
-   *     (\* eprintf "Find Global fundef %a...@." Expr.print_rs rs; *\)
-   *     let locals, body = find_global_fundef known rs in
-   *     Opt.iter init_real !prec;
-   *     ( try
-   *         let dispatch = prepare_dispatch env !dispatch in
-   *         let res = eval_global_fundef ~rac:!enable_rac env dispatch known locals body in
-   *         printf "%a@." (report_eval_result ~mod_name ~fun_name body) res;
-   *         exit (match res with Pinterp.Normal _, _ -> 0 | _ -> 1);
-   *       with Contr (ctx, term) ->
-   *         printf "%a@." (report_cntr ~mod_name ~fun_name body) (ctx, term) ;
-   *         exit 1 )
-   *   with Not_found when not (Debug.test_flag Debug.stack_trace) ->
-   *     exit 1 in
-   * Queue.iter do_exec opt_exec *)
 
 let () =
   try
