873770d3cb8a944e341868928be11eea5fa4384e
Cláudio Belo Lourenço <belolourenco@gmail.com>
2020-06-25 13:52:04 +0200

    fix trywhy3 compilation
    Create and execute expr "mod_name.func_name ()"

--- src/trywhy3/why3_worker.ml
+++ src/trywhy3/why3_worker.ml
@@ -318,17 +318,24 @@ let why3_execute modules =
         (fun _k m acc ->
            let th = m.Pmodule.mod_theory in
            let mod_name = th.Theory.th_name.Ident.id_string in
+           let fun_name = "main" in
            try
-             let fun_name = "main" in
-             let rs = Pmodule.ns_find_rs m.Pmodule.mod_export [fun_name] in
+             let open Expr in
              let open Pinterp in
-             let locals, body = find_global_fundef m.Pmodule.mod_known rs in
+             let e_unit = e_exec (c_app (rs_tuple 0) [] [] (Ity.ity_tuple [])) in
+             let (let_defn,pv) = let_var (Ident.id_fresh "o") e_unit in
+
+             let rs = Pmodule.ns_find_rs m.mod_export [mod_name;fun_name] in
+             let e_rs_unit = e_exec (c_app rs [pv] [] rs.rs_cty.cty_result) in
+             let expr = e_let let_defn e_rs_unit in
+
+             let known, dispatch = m.mod_known, Pinterp.empty_dispatch in
              let result =
                try
-                 let res = eval_global_fundef ~rac:false env Pinterp.empty_dispatch m.Pmodule.mod_known locals body in
-                 asprintf "%a@." (report_eval_result ~mod_name ~fun_name body) res
+                 let res = eval_global_fundef ~rac:false env dispatch known [] expr in
+                 asprintf "%a@." (report_eval_result expr) res
                with Contr (ctx, term) ->
-                 asprintf "%a@." (report_cntr ~mod_name ~fun_name body) (ctx, term) in
+                 asprintf "%a@." (report_cntr expr) (ctx, term) in
              let loc = Opt.get_def Loc.dummy_position th.Theory.th_name.Ident.id_loc in
              (loc, mod_name ^ ".main() returns " ^ result)
              :: acc
