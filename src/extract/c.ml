1c7cc1091cf21e8434be28324b3a75011eb76f35
Claude Marche <Claude.Marche@inria.fr>
2020-11-25 19:31:55 +0100

    revert changes in c.ml related to Invalid_argument exception
    Yet this code should be fixed, because catching Invalid_argument
    is not a good practice

--- src/extract/c.ml
+++ src/extract/c.ml
@@ -343,18 +343,16 @@ module C = struct
     | Sexpr _ -> true
     | _ -> false
 
-  exception Unsupported_stmt of stmt
-
   let rec simplify_expr (d,s) : expr =
     match (d,elim_empty_blocks(elim_nop s)) with
     | [], Sblock([],s) -> simplify_expr ([],s)
     | [], Sexpr e -> e
     | [], Sif(c,t,e) ->
        Equestion (c, simplify_expr([],t), simplify_expr([],e))
-    | [], s ->
-       raise (Unsupported_stmt s)
     | _ ->
-       raise (Unsupported "simplify_expr")
+       (* FIXME : use a dedicated exception here.
+          See also the capture of [Invalid_argument] below *)
+       raise (Invalid_argument "simplify_expr")
 
   let rec simplify_cond (cd, cs) =
     match cd,elim_empty_blocks(elim_nop cs) with
@@ -1171,7 +1169,7 @@ module MLToC = struct
          | [] -> C.([], expr_or_return env Enothing);
          | [e] -> expr info env e
          | _ ->
-            let id_struct = id_register (id_fresh "result") in
+            let id_struct = id_register (result_id ()) in
             let e_struct = C.Evar id_struct in
             let d_struct =
               C.(Ddecl(Tstruct
@@ -1197,7 +1195,10 @@ module MLToC = struct
                  try
                    acc,
                    (simplify_expr (d,s), pty)
-                 with Invalid_argument _ ->
+                 with
+                 (* FIXME: use a dedicated exception instead.
+                    see also [simplify_expr]. *)
+                 Invalid_argument "simplify_expr" ->
                    let s', e' = get_last_expr s in
                    (accd@d, Sseq(accs, s')), (e', pty))
                ([], Snop)
@@ -1835,15 +1836,6 @@ let c_printer = Pdriver.{
 
 let () =
   Pdriver.register_printer "c" c_printer
-
-let () =
-  Exn_printer.register
-    (fun fmt exn ->
-      match exn with
-      | C.Unsupported_stmt s ->
-         Format.fprintf fmt "Unsupported statement @[%a@]"
-           (Print.print_stmt ~braces:false) s
-      | e -> raise e)
 
 (*
 Local Variables:
