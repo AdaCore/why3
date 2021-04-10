b2bbc354d025b43818084c4009492948ba348192
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2021-04-10 11:34:47 +0200

    Fix bench.

--- misc/ci-local.sh
+++ misc/ci-local.sh
@@ -52,7 +52,7 @@ do
             bin/why3 config detect
             sed -i why3.conf -e "s/running_provers_max = [0-9]*/running_provers_max = 1/"
             cat misc/bench-few-provers-why3-conf >> why3.conf
-            COQVER=$(bin/why3 --list-provers | sed -n -e 's/  Coq (\?\([0-9.]\+\).*/\1/p')
+            COQVER=$(bin/why3 show provers | sed -n -e 's/  Coq (\?\([0-9.]\+\).*/\1/p')
             if test "$COQVER" != "" ; then
               sed misc/bench-coq-why3-conf -e "s/@COQVER@/$COQVER/g" >> why3.conf
             fi
