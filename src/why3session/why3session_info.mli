ebfa10f2b9a50faa11c8f65a56857cf17e0844ff
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2021-09-28 16:05:45 +0200

    Silence warning 70 "missing-mli".
    OCaml warns when there is a .ml file but no .mli file. This commit adds
    almost all the missing .mli files. The remaining ones are the *_ast.ml
    files, as the .mli files would be verbatim copies.

---
+++ src/why3session/why3session_info.mli
@@ -0,0 +1,12 @@
+(********************************************************************)
+(*                                                                  *)
+(*  The Why3 Verification Platform   /   The Why3 Development Team  *)
+(*  Copyright 2010-2021 --  Inria - CNRS - Paris-Saclay University  *)
+(*                                                                  *)
+(*  This software is distributed under the terms of the GNU Lesser  *)
+(*  General Public License version 2.1, with the special exception  *)
+(*  on linking described in file LICENSE.                           *)
+(*                                                                  *)
+(********************************************************************)
+
+val cmd : Why3session_lib.cmd
