7c940b46413cd6ff83ccb7dc428afcf4be2ae0fe
Solène Moreau <solene.moreau@inria.fr>
2022-05-19 09:47:58 +0200

    Add memlimit section in the bench
    - Update drivers with additional regexps for detecting Out of memory errors
    - Specific treatment in Call_prover.analyse_result for cases where a High Failure
    is reported by the prover, should be removed when treating issue
    https://gitlab.inria.fr/why3/why3/-/issues/648

--- src/driver/call_provers.ml
+++ src/driver/call_provers.ml
@@ -248,6 +248,16 @@ let analyse_result exit_result res_parser get_counterexmp printing_info out =
     match l with
     | [] ->
         Opt.get_def HighFailure saved_res, List.rev saved_models
+    (** FIXME (see https://gitlab.inria.fr/why3/why3/-/issues/648)
+        The following case is a specific treatment for cases when Answer HighFailure
+        is appended at the end of result_list because signaled is true in the function
+        parse_prover_run.
+        Without this hack, if a regexp matches exactly the last line of the prover output
+        and if Answer HighFailure has been appended at the end, we might end up with two
+        consecutive answers in result_list that are ignored by the more general case
+        Answer res1 :: (Answer res2 :: tl as tl1). *)
+    | Answer res :: (Answer HighFailure :: []) ->
+        Opt.get_def HighFailure (merge_answers (Some res) saved_res), List.rev saved_models
     | Answer res1 :: (Answer res2 :: tl as tl1) ->
        Debug.dprintf debug "Call_provers: two consecutive answers: %a %a@."
           print_prover_answer res1 print_prover_answer res2;
