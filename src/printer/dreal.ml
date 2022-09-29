497b0904a9272b846b7acea6658f42d6851af8b0
Paul Bonnot <paul.bonnot@inria.fr>
2022-09-29 10:55:36 +0200

    Improvements of dreal printer

--- src/printer/dreal.ml
+++ src/printer/dreal.ml
@@ -12,13 +12,11 @@
 (** Dreal printer *)
 
 open Format
-open Pp
 open Printer
 open Ident
 open Term
 open Decl
 open Theory
-open Task
 
 let syntactic_transform transf =
   Trans.on_meta meta_syntax_logic (fun metas ->
@@ -44,19 +42,15 @@ let syntactic_transform_env transf env =
       in
       transf (fun ls -> Sls.mem ls symbols) env)
 
-type info = {
-  info_env : Env.env;
-  info_syn : syntax_map;
-}
+type info = { info_syn : syntax_map }
+
+let info = ref { info_syn = Mid.empty }
 
 let find_th env file th =
   let theory = Env.read_theory env [ file ] th in
   fun id -> Theory.ns_find_ls theory.Theory.th_export [ id ]
 
-let get_info env task =
-  (* sets of known symbols *)
-  let syn = get_syntax_map task in
-  { info_env = env; info_syn = syn }
+let get_info env task = { info_syn = get_syntax_map task }
 
 let number_format =
   {
@@ -97,111 +91,143 @@ let ident_printer =
 
 let print_ident fmt id = pp_print_string fmt (id_unique ident_printer id)
 
-let rec print_fmla info fmt f =
-  let print_fmla = print_fmla info in
-  match f.t_node with
-  | Tapp ({ ls_name = id }, []) -> fprintf fmt "%a" print_ident id
+let print_decl fmt ls =
+  if Ty.ty_equal (Option.get ls.ls_value) Ty.ty_int then
+    fprintf fmt "@[(declare-fun %a () Int)@]\n" print_ident ls.ls_name
+  else
+    fprintf fmt "@[(declare-fun %a () Real)@]\n" print_ident ls.ls_name
+
+let rec print_term fmt t =
+  match t.t_node with
+  | Tapp ({ ls_name = id; ls_value = Some ty }, [])
+    when Ty.ty_equal ty Ty.ty_int || Ty.ty_equal ty Ty.ty_real ->
+    fprintf fmt "%a" print_ident id
+  | Tapp (ls, []) -> () (* We can ignore *)
   | Tapp (ls, tl) -> (
-    match query_syntax info.info_syn ls.ls_name with
-    | Some s -> syntax_arguments s print_fmla fmt tl
+    match query_syntax !info.info_syn ls.ls_name with
+    | Some s -> syntax_arguments s print_term fmt tl
     | None ->
-      unsupportedTerm f
+      unsupportedTerm t
         ("dreal: function '" ^ ls.ls_name.id_string ^ "' is not supported"))
-  | Tquant (_q, _fq) -> ()
+  | Tquant (_q, _fq) -> raise (FmlaExpected t)
   | Tbinop (Tand, f1, f2) ->
-    fprintf fmt "@[(and@ %a@ %a)@]" print_fmla f1 print_fmla f2
+    fprintf fmt "@[(and@ %a@ %a)@]" print_term f1 print_term f2
   | Tbinop (Tor, f1, f2) ->
-    fprintf fmt "@[(or@ %a@ %a)@]" print_fmla f1 print_fmla f2
+    fprintf fmt "@[(or@ %a@ %a)@]" print_term f1 print_term f2
   | Tbinop (Timplies, f1, f2) ->
-    fprintf fmt "@[(=>@ %a@ %a)@]" print_fmla f1 print_fmla f2
+    fprintf fmt "@[(=>@ %a@ %a)@]" print_term f1 print_term f2
   | Tbinop (Tiff, f1, f2) ->
-    fprintf fmt "@[(=@ %a@ %a)@]" print_fmla f1 print_fmla f2
-  | Tnot f -> fprintf fmt "@[<hv2>(not@ %a)@]" print_fmla f
+    fprintf fmt "@[(=@ %a@ %a)@]" print_term f1 print_term f2
+  | Tnot f -> fprintf fmt "@[<hv2>(not@ %a)@]" print_term f
   | Ttrue -> pp_print_string fmt "true"
   | Tfalse -> pp_print_string fmt "false"
   | Tif (f1, f2, f3) ->
-    fprintf fmt "@[<hv2>(ite %a@ %a@ %a)@]" print_fmla f1 print_fmla f2
-      print_fmla f3
+    fprintf fmt "@[<hv2>(ite %a@ %a@ %a)@]" print_term f1 print_term f2
+      print_term f3
   | Tlet (t1, tb) ->
     let v, f2 = t_open_bound tb in
-    fprintf fmt "@[<hv2>(let ((%a %a))@ %a)@]" print_ident v.vs_name print_fmla
-      t1 print_fmla f2
-  | Tcase _ -> unsupportedTerm f "dreal: you must eliminate match"
-  | Tvar _ -> raise (FmlaExpected f)
+    fprintf fmt "@[<hv2>(let ((%a %a))@ %a)@]" print_ident v.vs_name print_term
+      t1 print_term f2
+  | Tcase _ -> unsupportedTerm t "dreal: you must eliminate match"
+  | Tvar _ -> raise (FmlaExpected t)
   | Tconst c ->
     fprintf fmt "%a" Constant.(print number_format unsupported_escape) c
-  | Teps _ -> raise (FmlaExpected f)
+  | Teps _ -> raise (FmlaExpected t)
 
-exception Contradiction
+let rec print_assertion fmt assertion =
+  fprintf fmt "@[(assert %a)@]\n" print_term assertion
 
-let split_fmla info fmt f =
-  let rec split pos f =
-    match f.t_node with
-    | Tbinop (Tand, f1, f2) when pos ->
-      split true f1;
-      split true f2
-    | Tbinop (Tor, f1, f2) when not pos ->
-      split false f1;
-      split false f2
-    | Tbinop (Timplies, f1, f2) when not pos ->
-      split true f1;
-      split false f2
-    | Tapp (ls, []) -> ()
-    | Tapp (ls, [ { t_node = Tapp (t1, []) }; t2 ])
-      when ls_equal ls ps_equ && t_equal t2 t_bool_true ->
-      ()
-    | Ttrue ->
-      if pos then
-        ()
-      else
-        raise Contradiction
-    | Tfalse ->
-      if pos then
-        raise Contradiction
-      else
-        ()
-    | Tnot f -> split (not pos) f
-    | Tapp (ls, [ t1; t2 ]) when pos && ls_equal ls ps_equ ->
-      fprintf fmt "@[(assert  %a)@]\n" (print_fmla info) f
-    | _ ->
-      if pos then
-        fprintf fmt "@[(assert %a)@]\n" (print_fmla info) f
-      else
-        fprintf fmt "@[(assert %a)@]\n" (print_fmla info) (t_not f)
-  in
-  split true f
+type fmla =
+  | Unsupported
+  | Tautology
+  | Contradiction
+  | Formula of term
 
-(* let rec print_ddecl fmt (ty, e) = *)
-(*   match e with *)
-(*   | (f, h) :: b -> fprintf fmt "%a" print_ident f.ls_name *)
-(*   | [] -> () *)
+(* Filter all the formulas that are not inequalities or equalities *)
+(* Also performs some simplifications *)
+let rec get_fmla f =
+  match f.t_node with
+  | Tbinop (Tand, f1, f2) -> (
+    match get_fmla f1 with
+    | Unsupported
+    | Tautology ->
+      get_fmla f2
+    | Contradiction -> Contradiction
+    | Formula f1 -> (
+      match get_fmla f2 with
+      | Unsupported
+      | Tautology ->
+        Formula f1
+      | Contradiction -> Contradiction
+      | Formula f2 -> Formula (t_and f1 f2)))
+  | Tbinop (Tor, f1, f2) -> (
+    match get_fmla f1 with
+    | Unsupported -> Unsupported
+    | Tautology -> Tautology
+    | Contradiction -> get_fmla f2
+    | Formula f1 -> (
+      match get_fmla f2 with
+      | Unsupported -> Unsupported
+      | Tautology -> Tautology
+      | Contradiction -> Formula f1
+      | Formula f2 -> Formula (t_or f1 f2)))
+  | Tbinop (Timplies, f1, f2) -> (
+    match get_fmla f1 with
+    | Unsupported
+    | Contradiction ->
+      Unsupported
+    | Tautology -> get_fmla f2
+    | Formula f1 -> (
+      match get_fmla f2 with
+      | Unsupported -> Unsupported
+      | Tautology -> Tautology
+      | Contradiction -> Formula (t_implies f1 t_false)
+      | Formula f2 -> Formula (t_implies f1 f2)))
+  | Ttrue -> Tautology
+  | Tfalse -> Contradiction
+  | Tnot f1 -> (
+    match get_fmla f1 with
+    | Unsupported -> Unsupported
+    | Tautology -> Contradiction
+    | Contradiction -> Tautology
+    | Formula f -> Formula (t_not f))
+  | Tapp (ls, [ t1; t2 ]) -> (
+    match query_syntax !info.info_syn ls.ls_name with
+    | Some s -> Formula f
+    | None -> Unsupported)
+  | _ -> Unsupported
 
-let print_decl info fmt d =
+let get_decls_and_assertions (decls, assertions) d =
   match d.d_node with
-  | Dtype _ -> ()
-  | Ddata dl -> () (* List.iter (print_ddecl fmt) dl *)
+  | Dtype _ -> (decls, assertions)
+  | Ddata dl -> (decls, assertions)
   | Dparam ({ ls_args = []; ls_value = Some ty } as ls)
     when Ty.ty_equal ty Ty.ty_int || Ty.ty_equal ty Ty.ty_real ->
-    if Ty.ty_equal ty Ty.ty_int then
-      fprintf fmt "@[(declare-fun %a () Int)@]\n" print_ident ls.ls_name
-    else
-      fprintf fmt "@[(declare-fun %a () Real)@]\n" print_ident ls.ls_name
+    (ls :: decls, assertions)
   | Dparam _
   | Dlogic _ ->
-    ()
+    (decls, assertions)
   | Dind _ ->
     unsupportedDecl d
       "please remove inductive definitions before calling dreal printer"
-  | Dprop (Paxiom, pr, f) -> split_fmla info fmt f
-  | Dprop (Pgoal, pr, f) -> split_fmla info fmt (t_not f)
+  | Dprop (Paxiom, _, f) -> (
+    match get_fmla f with
+    | Contradiction -> (decls, t_false :: assertions)
+    | Formula f -> (decls, f :: assertions)
+    | _ -> (decls, assertions))
+  | Dprop (Pgoal, _, f) -> (decls, t_not f :: assertions)
   | Dprop (Plemma, _, _) -> unsupportedDecl d "dreal: lemmas are not supported"
 
 let print_task args ?old:_ fmt task =
   forget_all ident_printer;
   print_prelude fmt args.prelude;
   print_th_prelude task fmt args.th_prelude;
-  let info = get_info args.env task in
-  List.iter (print_decl info fmt) (Task.task_decls task);
+  info := get_info args.env task;
+  let decls, assertions =
+    List.fold_left get_decls_and_assertions ([], []) (Task.task_decls task)
+  in
+  List.iter (print_decl fmt) (List.rev decls);
+  List.iter (print_assertion fmt) (List.rev assertions);
   fprintf fmt "(check-sat)"
 
 let () =
