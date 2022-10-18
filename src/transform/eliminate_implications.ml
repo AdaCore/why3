23b7c4c8d75164e5ac58ca613f6fd62b3cc534b1
pbonnot <paul.bonnot@inria.fr>
2022-10-18 19:53:24 +0200

    Started to create another transformation eliminate_implications

--- src/transform/eliminate_implications.ml
+++ src/transform/eliminate_implications.ml
@@ -15,19 +15,49 @@ open Ty
 open Theory
 open Task
 
-(* TODO Remove Tor ? *)
-(* TODO Transform A ^ B -> C into A -> B -> C ? We could therefore get rid of
-   the conjonctions in truths *)
 (* TODO Try to instanciate quantifiers for quantified formulas with 3 or less
    variables ? This would have a worst case complexity of m*n^3 where n is the
    number of variables that can be instantiated and m the number of quantified
    formulas with 3 or less variables. Perhaps we should only do that with the
    formulas of the local context ? At least we should avoid instantiating stuff
    from the stdlib *)
-(* Remove all that is not an equality/inequality or quantified stuff. Also
-   remove Timplies, Tiff, Tand. We could still throw away quantified assertions
-   that could be used on implications if instanciated, but its probably too
-   costly to use them. *)
+
+let rec simplify_fmla f =
+  match f.t_node with
+  | Tbinop (Tand, f1, f2) -> simplify_fmla f1 @ simplify_fmla f2
+  | Tbinop (Timplies, f1, f2) ->
+    let conditions = simplify_fmla f1 in
+    let post = simplify_fmla f2 in
+    List.map
+      (fun t -> List.fold_left (fun acc f -> t_implies_simp f acc) t conditions)
+      post
+  | Tbinop (Tiff, f1, f2) ->
+    simplify_fmla (t_implies_simp f1 f2) @ simplify_fmla (t_implies_simp f2 f1)
+  | Tbinop (Tor, f1, f2) ->
+    simplify_fmla (t_implies_simp (t_not f1) f2)
+    @ simplify_fmla (t_implies_simp (t_not f2) f1)
+  | Tnot t -> (
+    match t.t_node with
+    | Tbinop (Tand, f1, f2) ->
+      simplify_fmla (t_or (t_not_simp f1) (t_not_simp f2))
+    | Tbinop (Tor, f1, f2) ->
+      simplify_fmla (t_and (t_not_simp f1) (t_not_simp f2))
+    | Tbinop (Timplies, f1, f2) -> simplify_fmla (t_and f1 (t_not_simp f2))
+    | Tnot t2 -> simplify_fmla t2
+    | _ -> [ f ])
+  | _ -> [ f ]
+
+let simplify d =
+  match d.d_node with
+  | Dprop (kind, pr, f) ->
+    List.map (fun t -> create_prop_decl kind pr t) (simplify_fmla f)
+  | _ -> [ d ]
+
+let simplify_fmla_form = Trans.decl simplify None
+
+let () =
+  Trans.register_transform "simplify_fmla_form" simplify_fmla_form
+    ~desc:"- Destructs@ all@ conjunctions@ and@ disjunctions."
 
 let is_present map term =
   try
@@ -45,26 +75,13 @@ let add_new_impl impls key impl =
 
 let rec get_fmlas f =
   match f.t_node with
-  | Tbinop (Tand, f1, f2) -> (
-    let new_truths, new_impls = ([ f1; f2 ], []) in
-    let new_truths, new_impls =
-      match f1.t_node with
-      | Tbinop (Timplies, _, _)
-      | Tbinop (Tiff, _, _) ->
-        let truths, impls = get_fmlas f1 in
-        (new_truths @ truths, new_impls @ impls)
-      | _ -> (new_truths, new_impls)
-    in
-    match f2.t_node with
-    | Tbinop (Timplies, _, _)
-    | Tbinop (Tiff, _, _) ->
-      let truths, impls = get_fmlas f2 in
-      (new_truths @ truths, new_impls @ impls)
-    | _ -> (new_truths, new_impls))
   | Tbinop (Timplies, f1, f2) -> ([], [ f ])
-  | Tbinop (Tiff, f1, f2) ->
-    ([ t_implies f1 f2; t_implies f2 f1 ], [ t_implies f1 f2; t_implies f2 f1 ])
   | _ -> ([], [])
+
+let add_impl f impls =
+  match f.t_node with
+  | Tbinop (Timplies, _, _) -> f :: impls
+  | _ -> impls
 
 let rec check_truth (truths, impls) truth =
   try
@@ -77,8 +94,7 @@ let rec check_truth (truths, impls) truth =
             if is_present truths f2 then
               (a, b)
             else
-              let new_truths, new_impls = get_fmlas f2 in
-              (new_truths @ (f2 :: a), new_impls @ b)
+              (f2 :: a, add_impl f2 b)
           | _ -> assert false)
         ([], []) formulas
     in
@@ -106,23 +122,13 @@ and check_impl (truths, impls) impl =
       let () = Mterm.find f1 truths in
       let truths = Mterm.add f2 () truths in
       let impls = Mterm.remove f1 impls in
-      let new_truths, new_impls = get_fmlas f2 in
-      let new_truths = [ f2 ] @ new_truths in
-      let truths =
-        List.fold_left (fun truths t -> Mterm.add t () truths) truths new_truths
-      in
-      let truths, impls =
-        List.fold_left check_truth (truths, impls) new_truths
-      in
-      let impls =
-        List.fold_left
-          (fun impls t ->
-            match t.t_node with
-            | Tbinop (Timplies, f1, f2) -> Mterm.add f1 t impls
-            | _ -> assert false)
-          impls new_impls
-      in
-      List.fold_left check_impl (truths, impls) new_impls
+      let truths, impls = check_truth (truths, impls) f2 in
+      match f2.t_node with
+      | Tbinop (Timplies, _, _) ->
+        let truths, impls = check_impl (truths, impls) f2 in
+        let impls = Mterm.add f1 t impls in
+        (truths, impls)
+      | _ -> (truths, impls)
     with
     | Not_found -> (truths, impls))
   | _ -> assert false
@@ -230,45 +236,23 @@ let filter_non_arith symbols truths acc t =
     | _ -> acc)
   | _ -> Task.add_tdecl acc t
 
-let remove_unused_decls symbols truths task =
+let remove_unused_decls truths task =
   let filter_non_arith = filter_non_arith symbols truths in
   task_fold_left filter_non_arith task
 
-let remove_unused_decls_ symbols (truths, impls) =
+let remove_unused_decls (truths, impls) =
   let truths, _ =
     Mterm.fold_left (fun arg t () -> check_truth arg t) (truths, impls) truths
   in
   Trans.store (remove_unused_decls symbols truths)
 
-let remove_non_arith symbols =
+let eliminate_implications =
   Trans.bind
     (Trans.fold_decl get_truths_and_impls (Mterm.empty, Mterm.empty))
-    (remove_unused_decls_ symbols)
-
-let keep_only_arithmetic env =
-  let symbol_names =
-    [
-      Ident.op_infix "<";
-      Ident.op_infix "<=";
-      Ident.op_infix ">";
-      Ident.op_infix ">=";
-    ]
-  in
-  let int = Env.read_theory env [ "int" ] "Int" in
-  let int_symbols =
-    List.map (fun name -> ns_find_ls int.th_export [ name ]) symbol_names
-  in
-  let real = Env.read_theory env [ "real" ] "Real" in
-  let real_symbols =
-    List.map (fun name -> ns_find_ls real.th_export [ name ]) symbol_names
-  in
-  let symbols = int_symbols @ real_symbols in
-  Trans.compose
-    (Trans.lookup_transform "inline_trivial" env)
-    (remove_non_arith symbols)
+    remove_unused_decls
 
 let () =
-  Trans.register_env_transform "keep_only_arithmetic" keep_only_arithmetic
+  Trans.register_transform "eliminate_implications" eliminate_implications
     ~desc:
       "Only@ keep@ declarations@ of@ integer@ and@ real@ variables,@ also@ \
        only@ keep@ assertions@ about@ inequalities@ or@ equalites@ between@ \
