6b06fb0cec246f3726e38e95ca6f08d11fc51166
Xavier Denis <xldenis@gmail.com>
2023-04-22 17:40:18 +0200

    Fix the subregion analysis

--- plugins/cfg/subregion_analysis.ml
+++ plugins/cfg/subregion_analysis.ml
@@ -17,11 +17,22 @@ open Expr
 open Term
 open Ident
 
-type domain_elt =
-  | Union of ity * domain_elt list
-  | Variable of pvsymbol
-  | Proj of ity * rsymbol * domain_elt
-  | Bot
+type domain_elt = Union of ity * domain_elt list | Variable of pvsymbol | Proj of ity * rsymbol * domain_elt | Bot
+
+let rec equal a b =
+  match (a, b) with
+  | Bot, Bot -> true
+  | Variable v, Variable w -> pv_equal v w
+  | Proj (t, r, e), Proj (t2, r2, e2) -> ity_equal t t2 && rs_equal r r2 && equal e e2
+  | Union (ty, elts), Union (ty2, elts2) -> ity_equal ty ty2 && List.for_all2 equal elts elts2
+  | _, _ -> false
+
+let rec print_elt fmt e =
+  match e with
+  | Union (_, elts) -> Format.fprintf fmt "{ %a }" (Format.pp_print_list print_elt) elts
+  | Variable p -> Format.fprintf fmt "%a" Ity.print_pv p
+  | Proj (_, r, elt) -> Format.fprintf fmt "%a -> %a" print_elt elt Expr.print_rs r
+  | Bot -> Format.fprintf fmt "bot"
 
 let rec ity_of d : ity =
   match d with
@@ -30,8 +41,7 @@ let rec ity_of d : ity =
       let inner_ty = ity_of p in
       match inner_ty.ity_node with
       | Ityvar _ -> assert false
-      | Ityapp (s, tl, rl)
-      | Ityreg { reg_its = s; reg_args = tl; reg_regs = rl } ->
+      | Ityapp (s, tl, rl) | Ityreg { reg_its = s; reg_args = tl; reg_regs = rl } ->
           let isb = its_match_regs s tl rl in
           ity_full_inst isb v.rs_cty.cty_result)
   | Bot -> assert false
@@ -51,8 +61,7 @@ let mk_proj ty elt f =
       let fld_nms = fields (ity_of elt) in
       let rec go nms vals =
         match (nms, vals) with
-        | nm :: nms, v :: vals ->
-            if pv_equal nm (fd_of_rs f) then v else go nms vals
+        | nm :: nms, v :: vals -> if pv_equal nm (fd_of_rs f) then v else go nms vals
         | _, _ -> failwith "mk_proj: field not part of type"
       in
       go fld_nms flds
@@ -61,12 +70,7 @@ let mk_proj ty elt f =
 
 let unfold known v =
   let ity = ity_of v in
-  let sym =
-    match ity.ity_node with
-    | Ityapp (sym, _, _) -> sym
-    | Ityreg r -> r.reg_its
-    | _ -> assert false
-  in
+  let sym = match ity.ity_node with Ityapp (sym, _, _) -> sym | Ityreg r -> r.reg_its | _ -> assert false in
 
   let def =
     try find_its_defn known sym
@@ -86,8 +90,7 @@ let rec merge known l r =
         Format.eprintf "cannot merge %a with %a\n" print_ity ty1 print_ity ty2;
         assert false)
   | Variable v1, Variable v2 -> if pv_equal v1 v2 then Variable v1 else Bot
-  | Proj (c, f1, p1), Proj (_, f2, p2) ->
-      if f1 = f2 then mk_proj c (merge known p1 p2) f1 else Bot
+  | Proj (c, f1, p1), Proj (_, f2, p2) -> if f1 = f2 then mk_proj c (merge known p1 p2) f1 else Bot
   | Variable _, _ -> Bot
   | _, Variable _ -> Bot
   | _, Bot -> Bot
@@ -109,12 +112,6 @@ let rec update known l fld fld_ty v =
   | Variable _ -> update known (unfold known l) fld fld_ty v
   | _ -> assert false
 
-let rec is_projection elt =
-  match elt with
-  | Variable _ -> true
-  | Proj (_, _, p) -> is_projection p
-  | _ -> false
-
 (* requires the term to be a projection *)
 let rec to_term elt : term =
   match elt with
@@ -122,41 +119,33 @@ let rec to_term elt : term =
   | Proj (_, fld, p) -> t_app_infer (ls_of_rs fld) [ to_term p ]
   | _ -> assert false
 
-let rec generate_equality known f v (k : term -> term) : term list =
-  match (f, v) with
-  | Variable f, v when is_projection v ->
-      [ ps_app ps_equ [ k (t_var f.pv_vs); k (to_term v) ] ]
-  | f, Variable v when is_projection f ->
-      [ ps_app ps_equ [ k (to_term f); k (t_var v.pv_vs) ] ]
-  | Union (_, vls), Union (_, vls') ->
-      let rec go vls vls' acc =
-        match (vls, vls') with
-        | v :: vls, v' :: vls' ->
-            go vls vls' (generate_equality known v v' k @ acc)
-        | [], [] -> acc
-        | _, _ -> failwith "unreachable"
-      in
-      go vls vls' []
-  | Proj (_, fld, p), Proj (_, fld', p') ->
-      if rs_equal fld fld' then
-        generate_equality known p p' (fun s ->
-            k (t_app_infer (ls_of_rs fld) [ s ]))
-      else []
-  | Bot, Bot -> []
-  (* others *)
-  | _, Bot -> []
-  | Bot, _ -> []
-  | Union _, _ -> generate_equality known f (unfold known v) k
-  | _, Union _ -> generate_equality known (unfold known f) v k
-  (* | Variable _, _ -> generate_equality known (unfold known f) v k
-     | Proj _, _ -> generate_equality known (unfold known f) v k
-     | _, Variable _ -> generate_equality known f (unfold known v) k
-     | _, Proj _ -> generate_equality known f (unfold known v) k
-  *)
-  | _, _ -> assert false (* unreachable *)
+let rec find_invariants known (a : domain_elt) (b : domain_elt) (k : domain_elt -> 'a) : 'a list =
+  if equal a b then [ k a ]
+  else
+    match (a, b) with
+    | Union (_, vls), Union (_, vls') ->
+        let rec go vls vls' acc =
+          match (vls, vls') with
+          | v :: vls, v' :: vls' -> go vls vls' (find_invariants known v v' k @ acc)
+          | [], [] -> acc
+          | _, _ -> failwith "unreachable"
+        in
+        go vls vls' []
+    | _, Bot | Bot, _ -> []
+    | Variable _, Variable _ -> []
+    | Proj (i, r, e), Proj (_, r2, e2) ->
+        (* assert (ity_equal i i2); *)
+        if rs_equal r r2 then find_invariants known e e2 (fun d -> k (Proj (i, r, d))) else []
+    | Union _, _ -> find_invariants known a (unfold known b) k
+    | _, Union _ -> find_invariants known (unfold known a) b k
+    | Variable _, Proj _ -> []
+    | Proj _, Variable _ -> []
 
 (* The domain maps symbols to their symbolic value *)
 type domain = domain_elt Mpv.t
+
+let[@warning "-32"] print_domain fmt dom =
+  Mpv.iter (fun k v -> Format.fprintf fmt "%a => %a" Ity.print_pv k print_elt v) dom
 
 module FreshNames = struct
   open Ity
@@ -176,15 +165,10 @@ let find m k def = match Mpv.find_opt k m with Some v -> v | None -> def
 
 let merge_domains known d1 d2 =
   Mpv.merge
-    (fun _ a b ->
-      match (a, b) with
-      | Some a, Some b -> Some (merge known a b)
-      | None, b -> b
-      | a, None -> a)
+    (fun _ a b -> match (a, b) with Some a, Some b -> Some (merge known a b) | None, b -> b | a, None -> a)
     d1 d2
 
-let rec analyze muc (st : FreshNames.t) (regions : domain) (e : expr) :
-    domain_elt * expr * domain =
+let rec analyze muc (st : FreshNames.t) (regions : domain) (e : expr) : domain_elt * expr * domain =
   let attrs = e.e_attrs in
   let loc = e.e_loc in
   let d, e, r = inner muc st regions e in
@@ -207,19 +191,13 @@ and inner muc st regions e =
   | Elet (def, e) ->
       let st, def', regions = analyze_letdefn muc st regions def in
       let dom, e, regions = analyze muc st regions e in
-      let regions =
-        match def with LDvar (p, _) -> Mpv.remove p regions | _ -> regions
-      in
+      let regions = match def with LDvar (p, _) -> Mpv.remove p regions | _ -> regions in
       (dom, e_let def' e, regions)
   | Eexec (ce, _) ->
       let dom, e = analyze_cexp muc st regions ce in
       (dom, e_exec e, regions)
   | Eassign es ->
-      let regions, es =
-        Lists.map_fold_left
-          (fun regions asgn -> analyze_assign muc st regions asgn)
-          regions es
-      in
+      let regions, es = Lists.map_fold_left (fun regions asgn -> analyze_assign muc st regions asgn) regions es in
 
       (Bot, e_assign es, regions)
   | Eif (s, i, e) ->
@@ -276,9 +254,7 @@ and inner muc st regions e =
 
       let dom, regions =
         match (dom_reg, e_dom_reg) with
-        | Some (dom1, reg1), Some (dom2, reg2) ->
-            ( merge muc.muc_known dom1 dom2,
-              merge_domains muc.muc_known reg1 reg2 )
+        | Some (dom1, reg1), Some (dom2, reg2) -> (merge muc.muc_known dom1 dom2, merge_domains muc.muc_known reg1 reg2)
         | None, Some (dom, reg) -> (dom, reg)
         | Some (dom, reg), None -> (dom, reg)
         | None, None -> assert false
@@ -287,13 +263,7 @@ and inner muc st regions e =
       (dom, e_match scrut brs exn_brs, regions)
   | Ewhile (cond, inv, var, body) ->
       let inv =
-        List.map
-          (fun i ->
-            t_v_map
-              (fun v ->
-                t_var (FreshNames.pv st (restore_pv v) (restore_pv v)).pv_vs)
-              i)
-          inv
+        List.map (fun i -> t_v_map (fun v -> t_var (FreshNames.pv st (restore_pv v) (restore_pv v)).pv_vs) i) inv
       in
       let _, cond, regions = analyze muc st regions cond in
       let _, body, body_regions = analyze muc st Mpv.empty body in
@@ -303,20 +273,23 @@ and inner muc st regions e =
         List.map
           (fun k ->
             let k = FreshNames.pv st k k in
-            let_var
-              (Ident.id_clone k.pv_vs.vs_name)
-              ~ghost:true
-              (e_pure (t_var k.pv_vs)))
+            let_var (Ident.id_clone k.pv_vs.vs_name) ~ghost:true (e_pure (t_var k.pv_vs)))
           (Mpv.keys body_regions)
       in
       let lets, vars = List.split old_vals in
 
+      (* Maps the freshened version of a variable to the name of the ghost binding holding the version before the loop *)
+      let olds =
+        List.map2 (fun a b -> ((FreshNames.pv st a a).pv_vs, t_var b.pv_vs)) (Mpv.keys body_regions) vars |> Mvs.of_list
+      in
+
       let eqs =
         List.fold_right
           (fun (k, v) acc ->
-            generate_equality muc.muc_known (Variable k) v (fun k -> k) @ acc)
-          (List.combine vars (Mpv.values body_regions))
-          []
+            let invs : term list = find_invariants muc.muc_known (Variable (FreshNames.pv st k k)) v to_term in
+            let t' = List.map (fun t -> ps_app ps_equ [ t; t_subst olds t ]) invs in
+            t' @ acc)
+          (Mpv.bindings body_regions) []
       in
       let regions = merge_domains muc.muc_known regions body_regions in
 
@@ -340,13 +313,7 @@ and inner muc st regions e =
       (Bot, loop, regions)
   | Efor (ix, (l, dir, u), iix, invs, body) ->
       let invs =
-        List.map
-          (fun i ->
-            t_v_map
-              (fun v ->
-                t_var (FreshNames.pv st (restore_pv v) (restore_pv v)).pv_vs)
-              i)
-          invs
+        List.map (fun i -> t_v_map (fun v -> t_var (FreshNames.pv st (restore_pv v) (restore_pv v)).pv_vs) i) invs
       in
       let _, body, body_regions = analyze muc st Mpv.empty body in
 
@@ -355,20 +322,22 @@ and inner muc st regions e =
         List.map
           (fun k ->
             let k = FreshNames.pv st k k in
-            let_var
-              (Ident.id_clone k.pv_vs.vs_name)
-              ~ghost:true
-              (e_pure (t_var k.pv_vs)))
+            let_var (Ident.id_clone k.pv_vs.vs_name) ~ghost:true (e_pure (t_var k.pv_vs)))
           (Mpv.keys body_regions)
       in
       let lets, vars = List.split old_vals in
 
+      let olds =
+        List.map2 (fun a b -> ((FreshNames.pv st a a).pv_vs, t_var b.pv_vs)) (Mpv.keys body_regions) vars |> Mvs.of_list
+      in
+
       let eqs =
         List.fold_right
           (fun (k, v) acc ->
-            generate_equality muc.muc_known (Variable k) v (fun k -> k) @ acc)
-          (List.combine vars (Mpv.values body_regions))
-          []
+            let invs = find_invariants muc.muc_known (Variable (FreshNames.pv st k k)) v to_term in
+            let t' = List.map (fun t -> ps_app ps_equ [ t; t_subst olds t ]) invs in
+            t' @ acc)
+          (Mpv.bindings body_regions) []
       in
       let regions = merge_domains muc.muc_known regions body_regions in
 
@@ -380,13 +349,7 @@ and inner muc st regions e =
           body eqs
       in
 
-      let loop =
-        e_for ix
-          (e_var (FreshNames.pv st l l))
-          dir
-          (e_var (FreshNames.pv st u u))
-          iix invs body
-      in
+      let loop = e_for ix (e_var (FreshNames.pv st l l)) dir (e_var (FreshNames.pv st u u)) iix invs body in
 
       let loop =
         List.fold_left
@@ -407,34 +370,23 @@ and inner muc st regions e =
       let exn = e_exn x expr in
       (dom, exn, regions)
   | Eassert (k, t) ->
-      let t =
-        t_v_map
-          (fun v ->
-            t_var (FreshNames.pv st (restore_pv v) (restore_pv v)).pv_vs)
-          t
-      in
+      let t = t_v_map (fun v -> t_var (FreshNames.pv st (restore_pv v) (restore_pv v)).pv_vs) t in
       (Bot, e_assert k t, regions)
   | Eghost e ->
       let dom, expr, regions = analyze muc st regions e in
       (dom, e_ghostify true expr, regions)
   | Epure t ->
-      let t =
-        t_v_map
-          (fun v ->
-            t_var (FreshNames.pv st (restore_pv v) (restore_pv v)).pv_vs)
-          t
-      in
+      let t = t_v_map (fun v -> t_var (FreshNames.pv st (restore_pv v) (restore_pv v)).pv_vs) t in
       (Bot, e_pure t, regions)
   | Eabsurd -> (Bot, e, regions)
 
-and analyze_letdefn muc st (regions : domain) (l : let_defn) :
-    FreshNames.t * let_defn * domain =
+and analyze_letdefn muc st (regions : domain) (l : let_defn) : FreshNames.t * let_defn * domain =
   match l with
   | LDvar (nm, exp) ->
+      (* Format.printf "BEFORE ==== \n\n %a\n\n" Expr.print_expr exp; *)
       let dom, exp, regions = analyze muc st regions exp in
-      let letdef, nm' =
-        let_var (Ident.id_clone nm.pv_vs.vs_name) ~ghost:nm.pv_ghost exp
-      in
+      (* Format.printf "AFTER ==== \n\n %a\n\n" Expr.print_expr exp; *)
+      let letdef, nm' = let_var (Ident.id_clone nm.pv_vs.vs_name) ~ghost:nm.pv_ghost exp in
 
       (FreshNames.add_pv st nm nm', letdef, Mpv.add nm dom regions)
   | LDsym (r, ce) -> (
@@ -442,14 +394,9 @@ and analyze_letdefn muc st (regions : domain) (l : let_defn) :
       | Cfun e ->
           let _, e, _ = analyze muc FreshNames.empty Mpv.empty e in
           let cty = ce.c_cty in
-          let f =
-            c_fun cty.cty_args cty.cty_pre cty.cty_post cty.cty_xpost
-              cty.cty_oldies e
-          in
+          let f = c_fun cty.cty_args cty.cty_pre cty.cty_post cty.cty_xpost cty.cty_oldies e in
           let def, sym =
-            let_sym
-              (Ident.id_clone ~attrs:r.rs_name.id_attrs r.rs_name)
-              ~ghost:(cty_ghost cty) ~kind:(rs_kind r) f
+            let_sym (Ident.id_clone ~attrs:r.rs_name.id_attrs r.rs_name) ~ghost:(cty_ghost cty) ~kind:(rs_kind r) f
           in
 
           (FreshNames.add_rs st r sym, def, regions)
@@ -463,8 +410,7 @@ and analyze_letdefn muc st (regions : domain) (l : let_defn) :
               | Cfun e ->
                   let _, e, _ = analyze muc st Mpv.empty e in
                   let cty = def.rec_fun.c_cty in
-                  c_fun cty.cty_args cty.cty_pre cty.cty_post cty.cty_xpost
-                    cty.cty_oldies e
+                  c_fun cty.cty_args cty.cty_pre cty.cty_post cty.cty_xpost cty.cty_oldies e
               | _ -> def.rec_fun
             in
             (def.rec_rsym, f, def.rec_varl, rs_kind def.rec_sym))
@@ -472,11 +418,7 @@ and analyze_letdefn muc st (regions : domain) (l : let_defn) :
       in
       let def, rdl' = let_rec defs in
 
-      let subst =
-        List.fold_left2
-          (fun sm d d' -> Mrs.add d.rec_sym d'.rec_sym sm)
-          Mrs.empty rdl rdl'
-      in
+      let subst = List.fold_left2 (fun sm d d' -> Mrs.add d.rec_sym d'.rec_sym sm) Mrs.empty rdl rdl' in
 
       (FreshNames.merge_rs st subst, def, regions)
 
@@ -494,11 +436,7 @@ and analyze_cexp muc st regions (c : cexp) : domain_elt * cexp =
         | None ->
             (* check if r is a record constructor *)
             if Strings.has_suffix "'mk" r.rs_name.id_string then
-              Union
-                ( c.c_cty.cty_result,
-                  List.map
-                    (fun x -> find regions x (Variable (FreshNames.pv st x x)))
-                    args )
+              Union (c.c_cty.cty_result, List.map (fun x -> find regions x (Variable (FreshNames.pv st x x))) args)
             else Bot
       in
       let vl = List.map (fun v -> FreshNames.pv st v v) args in
@@ -512,9 +450,7 @@ and analyze_cexp muc st regions (c : cexp) : domain_elt * cexp =
   | Cfun expr ->
       let _, e, _ = analyze muc st regions expr in
       let cty = c.c_cty in
-      ( Bot,
-        c_fun cty.cty_args cty.cty_pre cty.cty_post cty.cty_xpost cty.cty_oldies
-          e )
+      (Bot, c_fun cty.cty_args cty.cty_pre cty.cty_post cty.cty_xpost cty.cty_oldies e)
   | Cany -> (Bot, c)
 
 and analyze_br muc st regions (b : reg_branch) =
