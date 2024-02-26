9c97b66f7059ab11afa3865cef9ecf2a3edc1cf8
Andrei Paskevich <andrei@lri.fr>
2024-02-26 11:03:14 +0100

    coma_main: piggyback on top of WhyML modules, not theories
    We need modules to use Typing anyway.

--- plugins/coma/coma_main.ml
+++ plugins/coma/coma_main.ml
@@ -18,74 +18,46 @@ open Ptree
 
 let debug = Debug.register_flag "coma" ~desc:"[coma] plugin debug flag"
 
-let qualid_last = function Qident x | Qdot (_, x) -> x
-let use_as q = function Some x -> x | None -> qualid_last q
-
-let read_thy env lenv qualid = match qualid with
-  | Qdot (q, {id_str = n}) ->
-      Env.read_theory env (Typing.string_list_of_qualid q) n
-  | Qident {id_str = n} ->
-      try Mstr.find n lenv with Not_found -> Env.read_theory env [] n
-
-let parse_simpl_use env lenv tuc = function
-  | Puseexport q ->
-      Theory.use_export tuc (read_thy env lenv q)
-  | Puseimport (import, q, idas) ->
-      let import = import || idas = None in
-      let tuc = Theory.open_scope tuc (use_as q idas).id_str in
-      let tuc = Theory.use_export tuc (read_thy env lenv q) in
-      Theory.close_scope ~import tuc
-
-let add_def ctx c tuc muc flat bl =
-  let ctx, dfl = type_defn_list tuc ctx flat bl in
+let add_def ctx c muc flat bl =
+  let ctx, dfl = type_defn_list muc.Pmodule.muc_theory ctx flat bl in
   Debug.dprintf debug "\n@[%a@]@." Coma_syntax.PP.pp_def_block dfl;
   let c, gl = vc_defn c flat dfl in
-  let add tuc ({hs_name = {Ident.id_string = s}},f) =
+  let add muc ({hs_name = {Ident.id_string = s}},f) =
     let pr = Decl.create_prsymbol (Ident.id_fresh ("vc_" ^ s)) in
-    Theory.add_prop_decl tuc Decl.Pgoal pr f in
-  let tuc = List.fold_left add tuc gl in
-  let add tuc (id,vl,f) =
+    let d = Decl.create_prop_decl Decl.Pgoal pr f in
+    let d = Pdecl.create_pure_decl d in
+    Pmodule.add_pdecl ~vc:false muc d in
+  let muc = List.fold_left add muc gl in
+  let add muc (id,vl,f) =
     let ls = create_psymbol id (List.map (fun v -> v.vs_ty) vl) in
-    Theory.add_logic_decl tuc [Decl.make_ls_defn ls vl f] in
-  let add tuc (h,w,pl,_) = List.fold_left add tuc (vc_spec c h w pl) in
-  ctx, c, List.fold_left add tuc dfl, muc
+    let d = Decl.create_logic_decl [Decl.make_ls_defn ls vl f] in
+    let d = Pdecl.create_pure_decl d in
+    Pmodule.add_pdecl ~vc:false muc d in
+  let add muc (h,w,pl,_) = List.fold_left add muc (vc_spec c h w pl) in
+  ctx, c, List.fold_left add muc dfl
 
-let add_top env lenv menv (ctx,c,tuc,muc) = function
-  | Blo b -> add_def ctx c tuc muc false b
-  | Def d -> add_def ctx c tuc muc true [d]
+let add_top env menv (ctx,c,muc) = function
+  | Blo b -> add_def ctx c muc false b
+  | Def d -> add_def ctx c muc true [d]
   | Pld d ->
-      let td0 = muc.Pmodule.muc_theory.Theory.uc_decls in
-      let muc = Typing.Unsafe.add_decl muc env menv d in
-      let rec catch tuc tdl = if tdl == td0 then tuc else match tdl with
-        | {Theory.td_node = Theory.Decl d}::tdl ->
-            Theory.add_decl ~warn:false (catch tuc tdl) d
-        | {Theory.td_node = Theory.Use th}::tdl ->
-            Theory.use_export (catch tuc tdl) th
-        | {Theory.td_node = Theory.Meta (m,al)}::tdl ->
-            Theory.add_meta (catch tuc tdl) m al
-        | {Theory.td_node = Theory.Clone _}::_ | [] ->
-            assert false in
-      ctx, c, catch tuc muc.Pmodule.muc_theory.Theory.uc_decls, muc
+      ctx, c, Typing.Unsafe.add_decl muc env menv d
   | Use (loc, u) ->
       let d = match u with
         | Puseexport q -> Ptree.Duseexport q
         | Puseimport (i,q,a) -> Ptree.Duseimport (loc,i,[q,a]) in
-      let muc = Typing.Unsafe.add_decl muc env menv d in
-      ctx, c, parse_simpl_use env lenv tuc u, muc
+      ctx, c, Typing.Unsafe.add_decl muc env menv d
 
-let read_module env path (lenv,menv) (nm,dl) =
+let read_module env path menv (nm,dl) =
   let id = Typing.Unsafe.create_user_id nm in
   let muc = Pmodule.create_module env ~path id in
-  let ini = ctx0, c_empty, muc.Pmodule.muc_theory, muc in
-  let _,_,tuc,muc = List.fold_left (add_top env lenv menv) ini dl in
-  Mstr.add nm.id_str (Theory.close_theory tuc) lenv,
+  let ini = ctx0, c_empty, muc in
+  let _,_,muc = List.fold_left (add_top env menv) ini dl in
   Mstr.add nm.id_str (Pmodule.close_module muc) menv
 
 let read_channel_coma env path file c =
-  let ini = Mstr.empty, Mstr.empty in
   let ast = Coma_lexer.parse_channel file c in
   if Debug.test_flag Typing.debug_parse_only then Mstr.empty else
-  fst (List.fold_left (read_module env path) ini ast)
+  List.fold_left (read_module env path) Mstr.empty ast
 
-let () = Env.register_format Env.base_language "coma" ["coma"] read_channel_coma
+let () = Env.register_format Pmodule.mlw_language "coma" ["coma"] read_channel_coma
   ~desc:"Continuation Machine language"
