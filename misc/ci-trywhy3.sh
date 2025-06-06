e00dca0a3464dacfdf4810251d001965de56f849
Claude Marche <Claude.Marche@inria.fr>
2025-06-06 10:21:52 +0200

    update release instructions after release
    Fix compilation of the Alt-Ergo worker.

--- misc/ci-trywhy3.sh
+++ misc/ci-trywhy3.sh
@@ -10,12 +10,13 @@ unset DEBIAN_FRONTEND
 
 opam init -y --no-setup -j4 --bare --disable-sandboxing
 opam switch create system ocaml-system
-opam install -y camlzip data-encoding dolmen_loop dune-build-info dune-site js_of_ocaml-lwt js_of_ocaml-ppx lwt_ppx ocplib-simplex ppx_blob psmt2-frontend zarith_stubs_js
+opam install -y camlzip data-encoding dolmen=0.9 dolmen_loop dune-build-info dune-site js_of_ocaml=5.0.1 js_of_ocaml-lwt js_of_ocaml-ppx lwt_ppx ocplib-simplex ppx_blob psmt2-frontend zarith_stubs_js
 
 mkdir trywhy3 trywhy3/ace-builds trywhy3/ace-builds/src-min-noconflict/
 
-git clone --branch=v2.5.2 --depth=1 https://github.com/OCamlPro/alt-ergo.git
+git clone --branch=v2.5.4 --depth=1 https://github.com/OCamlPro/alt-ergo.git
 cd alt-ergo
+sed -i -e s/--no-source-map// src/bin/js/dune
 opam exec -- make -j4 js-worker
 cp alt-ergo-worker.js ../trywhy3/
 cd -
